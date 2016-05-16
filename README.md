212f8c53ba148caa8d19426df5ceb608be62a458
frankiefu <ffu@google.com>
2016-05-16 10:04:25 -0700

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# shop
+# SHOP
+
+### Setup
+
+##### Prerequisites
+
+Install [polymer-cli](https://github.com/Polymer/polymer-cli):
+
+    npm install -g polymer-cli
+
+Need at least v0.3.0
+
+##### Setup
+
+    git clone https://github.com/Polymer/shop.git
+    cd shop
+    bower install
+
+### Start the development server
+
+    polymer serve
+
+### Run web-component-tester tests
+
+    polymer test
+
+### Build
+
+    polymer build
---
+++ sw-precache-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    '/images/*'
+  ],
+  navigateFallback: '/index.html',
+  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],
+  runtimeCaching: [
+    {
+      urlPattern: /\/data\/images\/.*/,
+      handler: 'cacheFirst',
+      options: {
+        cache: {
+          maxEntries: 200,
+          name: 'items-cache'
+        }
+      }
+    },
+    {
+      urlPattern: /\/data\/.*json/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'data-cache'
+        }
+      }
+    }
+  ]
+};
