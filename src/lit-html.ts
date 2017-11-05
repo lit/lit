ea99d872d2c7900b5a463e99f7a439f857de3797
Justin Fagnani <justinfagnani@google.com>
2017-11-05 15:24:21 -0800

    Fix lint warnings

--- src/lit-html.ts
+++ src/lit-html.ts
@@ -137,7 +137,7 @@ const nonWhitespace = /[^\s]/;
  *    * (') then any non-(')
  */
 const lastAttributeNameRegex =
-    /([^\0-\x1F\x7F-\x9F \x09\x0a\x0c\x0d"'>=/]+)[ \x09\x0a\x0c\x0d]*=[ \x09\x0a\x0c\x0d]*(?:[^ \x09\x0a\x0c\x0d"'`<>=]*|"[^"]*|'[^']*)$/
+    /([^\0-\x1F\x7F-\x9F \x09\x0a\x0c\x0d"'>=/]+)[ \x09\x0a\x0c\x0d]*=[ \x09\x0a\x0c\x0d]*(?:[^ \x09\x0a\x0c\x0d"'`<>=]*|"[^"]*|'[^']*)$/;
 
 /**
  * Finds the closing index of the last closed HTML tag.
