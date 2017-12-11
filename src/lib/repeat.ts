48df241b99fc0ecce1b37ad8540d9788acd28c12
herr kaste <herr.kaste@gmail.com>
2017-12-11 14:06:20 +0100

    Better type for directives

--- src/lib/repeat.ts
+++ src/lib/repeat.ts
@@ -26,12 +26,12 @@ function cleanMap(part: NodePart, key: any, map: Map<any, NodePart>) {
 }
 
 export function repeat<T>(
-    items: T[], keyFn: KeyFn<T>, template: ItemTemplate<T>): DirectiveFn;
-export function repeat<T>(items: T[], template: ItemTemplate<T>): DirectiveFn;
+    items: T[], keyFn: KeyFn<T>, template: ItemTemplate<T>): DirectiveFn<NodePart>;
+export function repeat<T>(items: T[], template: ItemTemplate<T>): DirectiveFn<NodePart>;
 export function repeat<T>(
     items: Iterable<T>,
     keyFnOrTemplate: KeyFn<T>| ItemTemplate<T>,
-    template?: ItemTemplate<T>): DirectiveFn {
+    template?: ItemTemplate<T>): DirectiveFn<NodePart> {
   let keyFn: KeyFn<T>;
   if (arguments.length === 2) {
     template = keyFnOrTemplate;
@@ -39,7 +39,7 @@ export function repeat<T>(
     keyFn = keyFnOrTemplate as KeyFn<T>;
   }
 
-  return directive((part: Part): any => {
+  return directive((part: NodePart): void => {
     if (!(part instanceof NodePart)) {
       throw new Error('repeat can only be used on NodeParts');
     }
