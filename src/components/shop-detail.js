73fd2a5920948adda3a7938bedd8e0d7ff4cc81a
Keanu Lee <keanulee@google.com>
2018-02-07 11:05:13 -0800

    License headers

--- src/components/shop-detail.js
+++ src/components/shop-detail.js
@@ -1,3 +1,13 @@
+/**
+ * @license
+ * Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
 import { LitElement, html } from '../../node_modules/@polymer/lit-element/lit-element.js';
 import { unsafeHTML } from '../../node_modules/lit-html/lib/unsafe-html.js';
 import { shopButtonStyle } from './shop-button-style.js';
