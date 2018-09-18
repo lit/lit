3bbe9ff0eaf281189160d42d77f67879d1e0babc
Kevin Schaaf <kschaaf@google.com>
2018-09-17 18:03:52 -0700

    Fix typos and beef up comments more.

--- src/directives/repeat.ts
+++ src/directives/repeat.ts
@@ -36,7 +36,7 @@ const updatePart = (part: NodePart, result: TemplateResult) => {
   part.commit();
   return part;
 };
-const inserPartBefore =
+const insertPartBefore =
     (containerPart: NodePart, part: NodePart, ref?: NodePart) => {
       const container = containerPart.startNode.parentNode as Node;
       const beforeNode = ref ? ref.startNode : containerPart.endNode;
@@ -112,39 +112,57 @@ export function repeat<T>(
     let newTail = newResults.length - 1;
 
     // Overview of O(n) reconciliation algorithm (general approach based on
-    // ideas found in ivi, vue, snabdom, etc.):
+    // ideas found in ivi, vue, snabbdom, etc.):
     //
-    // * We start with the list of old parts and new results, head/tail pointers
-    //   into each, and we build up the new list of parts by updating (and when
-    //   needed, moving) old parts or creating new ones. The initial scenario
-    //   might look like this:
+    // * We start with the list of old parts and new results (and arrays of
+    //   their respective keys), head/tail pointers into each, and we build
+    //   up the new list of parts by updating (and when needed, moving) old
+    //   parts or creating new ones. The initial scenario might look like this
+    //   (for brevity of the diagrams, the numbers in the array reflect "keys"
+    //   associated with the old parts or new results, although they are
+    //   actually stored in parallel arrays indexed using the same head/tail
+    //   pointers):
     //
     //        oldHead v                 v oldTail
     //   oldParts:   [0, 1, 2, 3, 4, 5, 6]
     //   newParts:   [ ,  ,  ,  ,  ,  ,  ]
-    //   newResults: [0, 2, 1, 4, 3, 7, 6]
+    //   newResults: [0, 2, 1, 4, 3, 7, 6] <- reflects the user's new item order
     //        newHead ^                 ^ newTail
     //
     // * Iterate old & new lists from both sides, updating, swapping, or
     //   removing parts at the head/tail locations until neither head nor tail
-    //   can move
+    //   can move.
+    //
+    // * Example below: keys at head pointers match, so update old part 0 in-
+    //   place (no need to move it) and record part 0 in the `newParts` list.
+    //   The last thing we do is advance the `oldHead` and `newHead` pointers
+    //   (will be reflected in the next diagram).
     //
     //        oldHead v                 v oldTail
     //   oldParts:   [0, 1, 2, 3, 4, 5, 6]
-    //   newParts:   [0,  ,  ,  ,  ,  ,  ] <- heads matched: update 0
-    //   newResults: [0, 2, 1, 4, 3, 7, 6]
+    //   newParts:   [0,  ,  ,  ,  ,  ,  ] <- heads matched: update 0 and
+    //   newResults: [0, 2, 1, 4, 3, 7, 6]    advance both oldHead & newHead
     //        newHead ^                 ^ newTail
+    //
+    // * Example below: head pointers don't match, but tail pointers do, so
+    //   update part 6 in place (no need to move it), and record part 6 in the
+    //   `newParts` list. Last, advance the `oldTail` and `oldHead` pointers.
     //
     //           oldHead v              v oldTail
     //   oldParts:   [0, 1, 2, 3, 4, 5, 6]
-    //   newParts:   [0,  ,  ,  ,  ,  , 6] <- tails matched: update 6
-    //   newResults: [0, 2, 1, 4, 3, 7, 6]
+    //   newParts:   [0,  ,  ,  ,  ,  , 6] <- tails matched: update 6 and
+    //   newResults: [0, 2, 1, 4, 3, 7, 6]    advance both oldTail & newTail
     //           newHead ^              ^ newTail
+    //
+    // * Example below: neither head nor tail match; next check if one of the
+    //   old head/tail items was removed.  The key at `oldTail` was removed
+    //   (no longer in the `newKeyToIndexMap`), so remove that part from the
+    //   DOM and advance just the `oldTail` pointer.
     //
     //           oldHead v           v oldTail
     //   oldParts:   [0, 1, 2, 3, 4, 5, 6]
-    //   newParts:   [0,  ,  ,  ,  ,  , 6] <- 5 not in new map; remove 5
-    //   newResults: [0, 2, 1, 4, 3, 7, 6]
+    //   newParts:   [0,  ,  ,  ,  ,  , 6] <- 5 not in new map; remove 5 and
+    //   newResults: [0, 2, 1, 4, 3, 7, 6]    advance oldTail
     //           newHead ^           ^ newTail
     //
     // * Once head and tail cannot move, any mismatches are due to either new or
@@ -154,28 +172,51 @@ export function repeat<T>(
     //   null its position in the oldParts array if it lies between the head
     //   and tail so we know to skip it when the pointers get there.
     //
+    // * Example below: neither head nor tail match, and neither were removed;
+    //   so find the `newHead` key in the `oldKeyToIndexMap`, and move that old
+    //   part's DOM into the next head position (before `oldParts[oldHead]`).
+    //   Last, null the part in the `oldPart` array since it was somewhere in
+    //   the remaining oldParts still to be scanned (between the head and tail
+    //   pointers) so that we know to skip that old part on future iterations.
+    //
     //           oldHead v        v oldTail
     //   oldParts:   [0, 1, -, 3, 4, 5, 6]
     //   newParts:   [0, 2,  ,  ,  ,  , 6] <- stuck; update & move 2 into place
-    //   newResults: [0, 2, 1, 4, 3, 7, 6]
+    //   newResults: [0, 2, 1, 4, 3, 7, 6]    and advance newHead
     //           newHead ^           ^ newTail
     //
+    // * Note that for moves/insertions like the one above, a part inserted at
+    //   the head pointer is inserted before the current `oldParts[oldHead]`,
+    //   and a part inserted at the tail pointer is inserted before
+    //   `newParts[newHead+1]`. The seeming asymmetry lies in the fact that new
+    //   parts are moved into place outside in, so to the right of the head
+    //   pointer are old parts, and to the right of the tail pointer are new
+    //   parts.
+    //
     // * We always restart back from the top of the algorithm, allowing matching
-    //   and simple updates in place to continue:
+    //   and simple updates in place to continue...
+    //
+    // * Example below: the head pointers once again match, so simply update
+    //   part 1 and record it in the `newParts` array.  Last, advance both head
+    //   pointers. 
     //
     //           oldHead v        v oldTail
     //   oldParts:   [0, 1, -, 3, 4, 5, 6]
-    //   newParts:   [0, 2, 1,  ,  ,  , 6] <- heads matched; update 1
-    //   newResults: [0, 2, 1, 4, 3, 7, 6]
+    //   newParts:   [0, 2, 1,  ,  ,  , 6] <- heads matched; update 1 and
+    //   newResults: [0, 2, 1, 4, 3, 7, 6]    advance both oldHead & newHead
     //              newHead ^        ^ newTail
     //
-    // * Items that were moved as a result of being stuck (the final else
-    //   clause) are marked with null, so we always advance old pointers over
-    //   these so we're comparing the next actual old value on either end.
+    // * As mentioned above, items that were moved as a result of being stuck
+    //   (the final else clause in the code below) are marked with null, so we
+    //   always advance old pointers over these so we're comparing the next
+    //   actual old value on either end.
+    //
+    // * Example below: `oldHead` is null (already placed in newParts), so
+    //   advance `oldHead`.
     //
     //              oldHead v     v oldTail
     //   oldParts:   [0, 1, -, 3, 4, 5, 6] // old head already used; advance
-    //   newParts:   [0, 2, 1,  ,  ,  , 6]
+    //   newParts:   [0, 2, 1,  ,  ,  , 6] // oldHead
     //   newResults: [0, 2, 1, 4, 3, 7, 6]
     //                 newHead ^     ^ newTail
     //
@@ -183,21 +224,33 @@ export function repeat<T>(
     //   from head to tail or tail to head, since they will be outside the
     //   pointer range and never visited again.
     //
+    // * Example below: Here the old tail key matches the new head key, so
+    //   the part at the `oldTail` position and move its dom to the new 
+    //   head position (before `oldParts[oldHead]`). Last, advance `oldTail`
+    //   and `newHead` pointers.
+    //
     //                 oldHead v  v oldTail
     //   oldParts:   [0, 1, -, 3, 4, 5, 6]
     //   newParts:   [0, 2, 1, 4,  ,  , 6] <- old tail matches new head: update
-    //   newResults: [0, 2, 1, 4, 3, 7, 6]    and move 4 into place
+    //   newResults: [0, 2, 1, 4, 3, 7, 6]   & move 4, advance oldTail & newHead
     //                 newHead ^     ^ newTail
+    //
+    // * Example below: Old and new head keys match, so update the old head
+    //   part in place, and advance the `oldHead` and `newHead` pointers.
     //
     //                 oldHead v oldTail
     //   oldParts:   [0, 1, -, 3, 4, 5, 6]
-    //   newParts:   [0, 2, 1, 4, 3,   ,6] <- heads match: update 3
-    //   newResults: [0, 2, 1, 4, 3, 7, 6]
+    //   newParts:   [0, 2, 1, 4, 3,   ,6] <- heads match: update 3 and advance
+    //   newResults: [0, 2, 1, 4, 3, 7, 6]    oldHead & newHead
     //                    newHead ^  ^ newTail
     //
-    // * If either the new or old pointers move past each other then all we have
-    //   left is additions (if old list exhsusted) or removals (if new list
+    // * Once the new or old pointers move past each other then all we have
+    //   left is additions (if old list exhausted) or removals (if new list
     //   exhausted). Those are handled in the final while loops at the end.
+    //
+    // * Example below: `oldHead` exceeded `oldTail`, so we're done with the
+    //   main loop.  Creat the remaining part and insert it at the new head
+    //   position, and the update is complete.
     //
     //                   (oldHead > oldTail)
     //   oldParts:   [0, 1, -, 3, 4, 5, 6]
@@ -205,18 +258,11 @@ export function repeat<T>(
     //   newResults: [0, 2, 1, 4, 3, 7, 6]
     //                       newHead ^ newTail
     //
-    // * Note that for moves/insertions, a part inserted at the head pointer
-    //   is inserted before the current `oldParts[oldHead]`, and a part inserted
-    //   at the tail pointer is inserved before `newParts[newHead+1]`.
-    //   The seeming asymetry lies in the fact that new parts are moved into
-    //   place outside in, so to the right of the head pointer are old parts,
-    //   and to the right of the tail pointer are new parts.
-    //
     // * Note that the order of the if/else clauses is not important to the
     //   algorithm, as long as the null checks come first (to ensure we're
     //   always working on valid old parts) and that the final else clause
     //   comes last (since that's where the expensive moves occur). The
-    //   order of remaining clauses is is just a simple guess at wich cases
+    //   order of remaining clauses is is just a simple guess at which cases
     //   will be most common.
     //
     // * TODO(kschaaf) Note, we could calculate the longest increasing
@@ -227,7 +273,7 @@ export function repeat<T>(
     //   contiguous items in linear time, in the minimum number of moves. As
     //   the number of multiple moves where LIS might help approaches a random
     //   shuffle, the LIS optimization becomes less helpful, so it seems not
-    //   worth the code at this point, but could consider if a compelling case
+    //   worth the code at this point. Could reconsider if a compelling case
     //   arises.
 
     while (oldHead <= oldTail && newHead <= newTail) {
@@ -258,14 +304,14 @@ export function repeat<T>(
       } else if (oldKeys[oldHead] === newKeys[newTail]) {
         // Old head matches new tail; update and move to new tail
         newParts[newTail] = updatePart(oldParts[oldHead]!, newResults[newTail]);
-        inserPartBefore(
+        insertPartBefore(
             containerPart, oldParts[oldHead]!, newParts[newTail + 1]);
         oldHead++;
         newTail--;
       } else if (oldKeys[oldTail] === newKeys[newHead]) {
         // Old tail matches new head; update and move to new head
         newParts[newHead] = updatePart(oldParts[oldTail]!, newResults[newHead]);
-        inserPartBefore(containerPart, oldParts[oldTail]!, oldParts[oldHead]!);
+        insertPartBefore(containerPart, oldParts[oldTail]!, oldParts[oldHead]!);
         oldTail--;
         newHead++;
       } else {
@@ -281,7 +327,7 @@ export function repeat<T>(
         } else {
           // Reuse old part
           newParts[newHead] = updatePart(oldPart, newResults[newHead]);
-          inserPartBefore(containerPart, oldPart, oldParts[oldHead]!);
+          insertPartBefore(containerPart, oldPart, oldParts[oldHead]!);
           // This marks the old part as having been used, so that it will be
           // skipped in the first two checks above
           oldParts[oldIndex as number] = null;
