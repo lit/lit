1555e5923d934e351307e1e0baf841dc36ee7bd4
Keanu Lee <keanulee@google.com>
2018-10-10 11:02:42 -0700

    Don't use export * to workaround rollup bug

--- src/test/lit-html_test.ts
+++ src/test/lit-html_test.ts
@@ -12,17 +12,86 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
-import {SVGTemplateResult, TemplateResult} from '../lib/template-result.js';
-import {html, svg} from '../lit-html.js';
+import * as LibDefaultTemplateProcessor from '../lib/default-template-processor.js';
+import * as LibDirective from '../lib/directive.js';
+import * as LibDom from '../lib/dom.js';
+import * as LibPart from '../lib/part.js';
+import * as LibParts from '../lib/parts.js';
+import * as LibRender from '../lib/render.js';
+import * as LibTemplateFactory from '../lib/template-factory.js';
+import * as LibTemplateInstance from '../lib/template-instance.js';
+import * as LibTemplateResult from '../lib/template-result.js';
+import * as LibTemplate from '../lib/template.js';
+import * as LitHtml from '../lit-html.js';
 
 const assert = chai.assert;
 
 suite('index.js', () => {
   test('html tag returns a TemplateResult', () => {
-    assert.instanceOf(html``, TemplateResult);
+    assert.instanceOf(LitHtml.html``, LibTemplateResult.TemplateResult);
   });
 
   test('svg tag returns a SVGTemplateResult', () => {
-    assert.instanceOf(svg``, SVGTemplateResult);
+    assert.instanceOf(LitHtml.svg``, LibTemplateResult.SVGTemplateResult);
+  });
+
+  test('exports everything from lib/template-result.js', () => {
+    Object.keys(LibTemplateResult).forEach((key) => {
+      assert.property(LitHtml, key);
+    });
+  });
+
+  test('exports everything from lib/template.js', () => {
+    Object.keys(LibTemplate).forEach((key) => {
+      assert.property(LitHtml, key);
+    });
+  });
+
+  test('exports everything from lib/default-template-processor.js', () => {
+    Object.keys(LibDefaultTemplateProcessor).forEach((key) => {
+      assert.property(LitHtml, key);
+    });
+  });
+
+  test('exports everything from lib/template-instance.js', () => {
+    Object.keys(LibTemplateInstance).forEach((key) => {
+      assert.property(LitHtml, key);
+    });
+  });
+
+  test('exports everything from lib/part.js', () => {
+    Object.keys(LibPart).forEach((key) => {
+      assert.property(LitHtml, key);
+    });
+  });
+
+  test('exports everything from lib/parts.js', () => {
+    Object.keys(LibParts).forEach((key) => {
+      assert.property(LitHtml, key);
+    });
+  });
+
+  test('exports everything from lib/dom.js', () => {
+    Object.keys(LibDom).forEach((key) => {
+      assert.property(LitHtml, key);
+    });
+  });
+
+  test('exports everything from lib/directive.js', () => {
+    Object.keys(LibDirective).forEach((key) => {
+      assert.property(LitHtml, key);
+    });
+  });
+
+  test('exports everything from lib/render.js', () => {
+    Object.keys(LibRender).forEach((key) => {
+      assert.property(LitHtml, key);
+    });
+  });
+
+  test('exports everything from lib/template-factory.js', () => {
+    Object.keys(LibTemplateFactory).forEach((key) => {
+      assert.property(LitHtml, key);
+    });
   });
 });
