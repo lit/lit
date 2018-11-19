ce562121db5c106b69230dbf7eb369bdc3f5687f
rivron <david.rivron@kelis.fr>
2018-11-19 20:05:06 +0100

    Reexport of TemplateProcessor and RenderOptions... (#633)

--- src/lit-html.ts
+++ src/lit-html.ts
@@ -21,9 +21,11 @@ export {directive, DirectiveFn, isDirective} from './lib/directive.js';
 export {removeNodes, reparentNodes} from './lib/dom.js';
 export {noChange, Part} from './lib/part.js';
 export {AttributeCommitter, AttributePart, BooleanAttributePart, EventPart, isPrimitive, NodePart, PropertyCommitter, PropertyPart} from './lib/parts.js';
+export {RenderOptions} from './lib/render-options.js';
 export {parts, render} from './lib/render.js';
 export {templateCaches, templateFactory} from './lib/template-factory.js';
 export {TemplateInstance} from './lib/template-instance.js';
+export {TemplateProcessor} from './lib/template-processor.js';
 export {SVGTemplateResult, TemplateResult} from './lib/template-result.js';
 export {createMarker, isTemplatePartActive, Template} from './lib/template.js';
 
