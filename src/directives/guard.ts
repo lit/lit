5f32c9f04cdde971912678704041758a2a1f8b43
Justin Fagnani <justin@fagnani.com>
2018-11-29 16:23:13 -0800

    Update guard to handle multiple checked values and undefined (#666)

--- src/directives/guard.ts
+++ src/directives/guard.ts
@@ -14,34 +14,61 @@
 
 import {directive, Part} from '../lit-html.js';
 
-const previousExpressions = new WeakMap<Part, any>();
+const previousValues = new WeakMap<Part, unknown>();
 
 /**
- * Creates a guard directive. Prevents any re-render until the identity of the
- * expression changes, for example when a primitive changes value or when an
- * object reference changes.
+ * Prevents re-render of a template function until a single value or an array of
+ * values changes.
  *
- * This useful with immutable data patterns, by preventing expensive work until
- * data updates. Example:
+ * Example:
  *
+ * ```js
  * html`
  *   <div>
- *     ${guard(items, () => items.map(item => html`${item}`))}
+ *     ${guard([user.id, company.id], () => html`...`)}
  *   </div>
- * `
+ * ```
+ *
+ * In this case, the template only renders if either `user.id` or `company.id`
+ * changes.
+ *
+ * guard() is useful with immutable data patterns, by preventing expensive work
+ * until data updates.
+ *
+ * Example:
+ *
+ * ```js
+ * html`
+ *   <div>
+ *     ${guard([immutableItems], () => immutableItems.map(i => html`${i}`))}
+ *   </div>
+ * ```
  *
  * In this case, items are mapped over only when the array reference changes.
  *
- * @param expression the expression to check before re-rendering
- * @param valueFn function which returns the render value
+ * @param value the value to check before re-rendering
+ * @param f the template function
  */
 export const guard =
-    directive((expression: any, valueFn: () => any) => (part: Part): void => {
-      // Dirty check previous expression
-      if (previousExpressions.get(part) === expression) {
+    directive((value: unknown, f: () => unknown) => (part: Part): void => {
+      const previousValue = previousValues.get(part);
+      if (Array.isArray(value)) {
+        // Dirty-check arrays by item
+        if (Array.isArray(previousValue) &&
+            previousValue.length === value.length &&
+            value.every((v, i) => v === previousValue[i])) {
+          return;
+        }
+      } else if (
+          previousValue === value &&
+          (value !== undefined || previousValues.has(part))) {
+        // Dirty-check non-arrays by identity
         return;
       }
 
-      part.setValue(valueFn());
-      previousExpressions.set(part, expression);
+      part.setValue(f());
+      // Copy the value if it's an array so that if it's mutated we don't forget
+      // what the previous values were.
+      previousValues.set(
+          part, Array.isArray(value) ? Array.from(value) : value);
     });
