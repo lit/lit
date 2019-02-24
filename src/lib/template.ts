aa833eaa74e78325e233ca32454b5920f40d9ca1
Justin Ridgewell <jridgewell@google.com>
2019-02-24 01:22:36 -0500

    Cache length of non-array iterables
    We can marginally speed up for-loops over non-arrays (in this case, c++
    backed `NamedNodeMap` and `NodeList`).
    
    NamedNodeMap: https://jsbench.github.io/#e730df94c251a459f9213abdafe1c34c
    NodeList: https://jsbench.github.io/#f638cacc866a1b2d6e517e6cfa900d6b

--- src/lib/template.ts
+++ src/lib/template.ts
@@ -68,13 +68,14 @@ export class Template {
         if (node.nodeType === 1 /* Node.ELEMENT_NODE */) {
           if ((node as Element).hasAttributes()) {
             const attributes = (node as Element).attributes;
+            const {length} = attributes;
             // Per
             // https://developer.mozilla.org/en-US/docs/Web/API/NamedNodeMap,
             // attributes are not guaranteed to be returned in document order.
             // In particular, Edge/IE can return them out of order, so we cannot
             // assume a correspondance between part index and attribute index.
             let count = 0;
-            for (let i = 0; i < attributes.length; i++) {
+            for (let i = 0; i < length; i++) {
               if (attributes[i].value.indexOf(marker) >= 0) {
                 count++;
               }
