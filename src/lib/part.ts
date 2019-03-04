0aba5ed7e0d547af1cb410ded0f201e51f6b5b1f
Michael Stramel <stramel@users.noreply.github.com>
2019-03-04 08:35:18 -0600

    Remove typing from noChange

--- src/lib/part.ts
+++ src/lib/part.ts
@@ -46,7 +46,7 @@ export interface Part {
  * A sentinel value that signals that a value was handled by a directive and
  * should not be written to the DOM.
  */
-export const noChange: object = {};
+export const noChange = {};
 
 /**
  * A sentinel value that signals a NodePart to fully clear its content.
