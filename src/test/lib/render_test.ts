80de42a4ba0aee48b5e046b1c7aa535bdab51750
Russell Bicknell <bicknellr@google.com>
2019-05-06 17:52:55 -0700

    Test that `render` accepts non-TemplateResult values.

--- src/test/lib/render_test.ts
+++ src/test/lib/render_test.ts
@@ -1406,4 +1406,35 @@ suite('render()', () => {
       assert(container.innerHTML, '<div></div>');
     });
   });
+
+  // `render` directly passes the given value to `NodePart#setValue`, so these
+  // tests are really just a sanity check that they are accepted by `render`.
+  // Tests about rendering behavior for specific values should generally be
+  // grouped with those of `NodePart#setValue` and `#commit`.
+  suite('accepts types other than TemplateResult', () => {
+    test('accepts undefined', () => {
+      render(undefined, container);
+      assert.equal(stripExpressionMarkers(container.innerHTML), '');
+    });
+
+    test('accepts a string', () => {
+      render('test', container);
+      assert.equal(stripExpressionMarkers(container.innerHTML), 'test');
+    });
+
+    test('accepts an object', () => {
+      render({}, container);
+      assert.equal(stripExpressionMarkers(container.innerHTML), '[object Object]');
+    });
+
+    test('accepts an object with `toString`', () => {
+      render({toString() { return 'toString!'; }}, container);
+      assert.equal(stripExpressionMarkers(container.innerHTML), 'toString!');
+    });
+
+    test('accepts an symbol', () => {
+      render(Symbol('description!'), container);
+      assert.equal(stripExpressionMarkers(container.innerHTML), 'Symbol(description!)');
+    });
+  });
 });
