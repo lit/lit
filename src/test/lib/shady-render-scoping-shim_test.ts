6e254e1fe55732bba7d832e35c64516b450736a3
Russell Bicknell <bicknellr@google.com>
2019-05-14 17:50:24 -0700

    Test that adopted styles still apply after later renders if the first rendered value was not a TemplateInstance.

--- src/test/lib/shady-render-scoping-shim_test.ts
+++ src/test/lib/shady-render-scoping-shim_test.ts
@@ -12,30 +12,55 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
+import {html} from '../../lib/shady-render.js';
 import {renderShadowRoot} from '../test-utils/shadow-root.js';
 
 const assert = chai.assert;
 
 suite('shady-render scoping shim', () => {
-  test('scoped styles are applied for non-TemplateResult values', function() {
-    if (typeof window.ShadyDOM === 'undefined' || !window.ShadyDOM.inUse) {
-      this.skip();
-      return;
-    }
-    if (typeof window.ShadyCSS === 'undefined' ||
+  setup(function() {
+    if (typeof window.ShadyDOM === 'undefined' || !window.ShadyDOM.inUse ||
+        typeof window.ShadyCSS === 'undefined' ||
         window.ShadyCSS.nativeShadow ||
         window.ShadyCSS.ScopingShim === undefined) {
       this.skip();
       return;
     }
+  });
+
+  test('scoped styles are applied for non-TemplateResult values', function() {
     const container = document.createElement('scope-1');
-    window.ShadyCSS.ScopingShim.prepareAdoptedCssText(
+    window.ShadyCSS!.ScopingShim!.prepareAdoptedCssText(
         [':host { border-top: 2px solid black; }'], 'scope-1');
     document.body.appendChild(container);
     renderShadowRoot(undefined, container);
     assert.equal(
         getComputedStyle(container).getPropertyValue('border-top-width').trim(),
         '2px');
+    document.body.removeChild(container);
+  });
+
+  const testName = 'adopted CSS remains when rendering a TemplateResult ' +
+      'after an initial non-TemplateResult';
+  test(testName, function() {
+    const container = document.createElement('scope-2');
+    window.ShadyCSS!.ScopingShim!.prepareAdoptedCssText(
+        [':host { border-top: 2px solid black; } button { font-size: 7px; } '],
+        'scope-2');
+    document.body.appendChild(container);
+    renderShadowRoot(undefined, container);
+    assert.equal(
+        getComputedStyle(container).getPropertyValue('border-top-width').trim(),
+        '2px');
+    renderShadowRoot(html`<button>This is a button.</button>`, container);
+    assert.equal(
+        getComputedStyle(container).getPropertyValue('border-top-width').trim(),
+        '2px');
+    assert.equal(
+        getComputedStyle(container.shadowRoot!.querySelector('button')!)
+            .getPropertyValue('font-size')
+            .trim(),
+        '7px');
     document.body.removeChild(container);
   });
 });
