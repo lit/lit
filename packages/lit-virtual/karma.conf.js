dec49a4e78b811463d51e6ea940e2c4351a6f812
straversi <straversi@google.com>
2019-06-04 14:57:16 -0700

    ✨ beautify test reporting

--- packages/lit-virtual/karma.conf.js
+++ packages/lit-virtual/karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
 
     // web server port
