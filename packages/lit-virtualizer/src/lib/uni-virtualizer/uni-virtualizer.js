2bbe71c13780a1c75255effcff871ec32f35fdbc
straversi <straversi@google.com>
2019-06-12 10:54:26 -0700

    simplify uni-virtual build with a symlink

--- packages/lit-virtualizer/src/lib/uni-virtualizer/uni-virtualizer.js
+++
@@ -1,5 +0,0 @@
-export { VirtualRepeater } from './lib/VirtualRepeater.js';
-export { VirtualScroller } from './lib/VirtualScroller.js';
-
-export { Layout1d } from './lib/layouts/Layout1d.js';
-export { Layout1dGrid } from './lib/layouts/Layout1dGrid.js';
