a28ec06ce39e7573c857cac22aae002dd4f4fb08
straversi <straversi@google.com>
2019-06-11 17:15:04 -0700

    [lit-virtualizer] add useShadowDOM tests

--- packages/lit-virtualizer/test/test.js
+++ packages/lit-virtualizer/test/test.js
@@ -1,4 +1,5 @@
-import { LitVirtualizer } from '../lit-virtualizer.js'
+import { LitVirtualizer, scroll } from '../lit-virtualizer.js'
+import { html, render } from 'lit-html'
 
 describe('<lit-virtualizer>', function () {
   it('is running the tests', function () {
@@ -9,4 +10,70 @@ describe('<lit-virtualizer>', function () {
     const lvs = document.createElement('lit-virtualizer');
     assert.instanceOf(lvs, LitVirtualizer);
   });
+});
+
+describe('scroll', function () {
+  let container;
+
+  beforeEach(function() {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(function() {
+    document.body.removeChild(container);
+  })
+
+  it('uses the template to render the items', async function () {
+    const dir = scroll({
+      items: ['foo', 'bar', 'baz'],
+      template: (item) => html`<p>${item}</p>`,
+      useShadowDOM: false
+    });
+    const templateResult = html`<div>
+      ${dir}
+    </div>`
+
+    render(templateResult, container);
+
+    // must wait an animation frame to let render finish.
+    await (new Promise(resolve => requestAnimationFrame(resolve)));
+    assert.include(container.innerHTML, 'foo');
+    assert.include(container.innerHTML, 'bar');
+    assert.include(container.innerHTML, 'baz');
+    // must return in async test per Mocha specs.
+    return
+  });
+
+  describe('useShadowDOM', function() {
+    it('renders to shadow DOM when useShadowDOM is true', async function() {
+      const dir = scroll({
+        items: ['foo', 'bar', 'baz'],
+        template: (item) => html`<p>${item}</p>`,
+        useShadowDOM: true,
+      });
+      const templateResult = html`${dir}`
+  
+      render(templateResult, container);
+  
+      await (new Promise(resolve => requestAnimationFrame(resolve)));
+      assert.exists(container.shadowRoot);
+      return
+    });
+
+    it('does not render to shadow DOM when useShadowDOM is false', async function() {
+      const dir = scroll({
+        items: ['foo', 'bar', 'baz'],
+        template: (item) => html`<p>${item}</p>`,
+        useShadowDOM: false,
+      });
+      const templateResult = html`${dir}`
+  
+      render(templateResult, container);
+  
+      await (new Promise(resolve => requestAnimationFrame(resolve)));
+      assert.notExists(container.shadowRoot);
+      return
+    });
+  })
 });
