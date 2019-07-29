a97a16b31d4ab94d36cc33d9c2acfd1c5e8d708d
straversi <straversi@google.com>
2019-07-29 14:50:36 -0700

    Add useShadowDOM query parameter to basic lit html example

--- packages/uni-virtualizer-examples/public/basic-lit-html/index.js
+++ packages/uni-virtualizer-examples/public/basic-lit-html/index.js
@@ -1,13 +1,18 @@
 import { render, html } from 'lit-html';
 import { scroll } from 'lit-virtualizer/lib/scroll.js';
 
+const urlParams = new URLSearchParams(window.location.search);
+urlParams.set('useShadowDOM', urlParams.get('useShadowDOM') === 'true');
+const useShadowDOM = urlParams.get('useShadowDOM') === 'true';
+window.history.replaceState({}, '', `${window.location.pathname}?${urlParams}`);
+
 const example = (contacts) => html`
     <section>
         ${scroll({
             items: contacts,
             renderItem: ({ mediumText }) => html`<p>${mediumText}</p>`,
             scrollTarget: window,
-            useShadowDOM: false
+            useShadowDOM: useShadowDOM
         })}
     </section>
 `;
