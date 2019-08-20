eac6734b670e1f46348273a54b9a703d2995d6f5
web-padawan <iamkulykov@gmail.com>
2019-08-20 16:38:11 +0300

    Add HTML element tag name declaration

--- packages/lit-virtualizer/src/lib/lit-virtualizer.ts
+++ packages/lit-virtualizer/src/lib/lit-virtualizer.ts
@@ -61,3 +61,9 @@ export class LitVirtualizer<T> extends LitElement {
         })}`;
     }
 }
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'lit-virtualizer': LitVirtualizer<unknown>;
+    }
+}
