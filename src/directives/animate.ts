c5507b9889af52520ab657552b10598f66b23072
Lars den Bakker <larsdenbakker@gmail.com>
2019-10-21 23:04:28 +0200

    Dynamically detect animations in animate directive

--- src/directives/animate.ts
+++ src/directives/animate.ts
@@ -18,19 +18,20 @@ import { NodePart } from "../lib/parts.js";
 import { TemplateResult } from "../lib/template-result.js";
 
 export interface AnimateData {
-  element?: Element;
   value?: unknown;
+  element?: HTMLElement;
   activePart: NodePart;
   inactivePart: NodePart;
 }
 
 export interface AnimateOptions {
-  prefix: string;
+  entry?: string;
+  exit?: string;
 }
 
 const previousData = new WeakMap<NodePart, AnimateData>();
 
-function shouldAnimate(a: any, b: any) {
+function renderValueChanged(a: any, b: any) {
   if (a instanceof TemplateResult && b instanceof TemplateResult) {
     return a.strings !== b.strings;
   } else {
@@ -39,12 +40,57 @@ function shouldAnimate(a: any, b: any) {
 }
 
 /** Finds element in-between the two given nodes. */
-function findRenderedElement(start: Node, end: Node): Element | undefined {
+function findRenderedElement(start: Node, end: Node): HTMLElement | undefined {
   let current: Node | null = start;
   while (current && current.nodeType !== 1 /* Node.ELEMENT_NODE */ && current !== end) {
     current = current.nextSibling;
   }
-  return current && current.nodeType === 1 /* Node.ELEMENT_NODE */ ? current as Element : undefined;
+  return current && current.nodeType === 1 /* Node.ELEMENT_NODE */ ? current as HTMLElement : undefined;
+}
+
+/**
+ * Resolves when all animations on the element have finished, or when another animation was triggered.
+ * @param element the element to animate on
+ * @param animation the class which triggers the animation
+ */
+function runAnimation(el: HTMLElement, animation: string) {
+  return new Promise<void>((resolve) => {
+    const animations = new Set<string>();
+
+    const done = () => {
+      el.classList.remove(animation);
+      el.removeEventListener('animationstart', onStart);
+      el.removeEventListener('animationcancel', onEndOrCancel);
+      el.removeEventListener('animationend', onEndOrCancel);
+      resolve();
+    };
+
+    const onStart = (e: AnimationEvent) => {
+      if (e.target === el) {
+        animations.add(e.animationName)
+      }
+    };
+
+    const onEndOrCancel = (e: AnimationEvent) => {
+      if (!el.classList.contains(animation)) {
+        // class is gone, which means this animation was raced by another
+        done();
+        return;
+      }
+
+      if (e.target === el) {
+        animations.delete(e.animationName);
+        if (animations.size === 0) {
+          done();
+        }
+      }
+    };
+
+    el.addEventListener('animationstart', onStart);
+    el.addEventListener('animationcancel', onEndOrCancel);
+    el.addEventListener('animationend', onEndOrCancel);
+    el.classList.add(animation);
+  });
 }
 
 /**
@@ -52,11 +98,11 @@ function findRenderedElement(start: Node, end: Node): Element | undefined {
  *
  * When the template rendered by this directive changes, a class is added to the first rendered
  * element of the previous template and to the first rendered element of the new template. After
- * an animation of the same name is finished, the class is removed and the previous template is
+ * the animations on those elements are finished, the class is removed and the previous template is
  * removed from the DOM.
  *
- * In the second parameter, a prefix must be given which is used to create the class and animation
- * name listened for.
+ * In the options parameter you specify which class should be set to play the entry or exit animation.
+ * If a value is left empty, an animation is not played for that event.
  *
  * @param value the value to render, must be a TemplateResult if it should be animated
  * @param options animation options
@@ -84,26 +130,21 @@ function findRenderedElement(start: Node, end: Node): Element | undefined {
  *  import { html, nothing } from 'lit-html';
  *  import { animate } from 'lit-html/directives/animate.js';
  *
- *  ${animate(showFoo ? html`<div>Foo</div>` : nothing, { prefix: 'foo' })}
+ *  ${animate(showFoo ? html`<div>Foo</div>` : nothing, { entry: 'foo-entry', exit: 'foo-exit' })}
  * `
  * ```
  */
-export const animate = directive((value: unknown, options: AnimateOptions) => (parentPart: Part) => {
+export const animate = directive((value: unknown, options: AnimateOptions = {}) => (parentPart: Part) => {
   if (!(parentPart instanceof NodePart)) {
     throw new Error('animate can only be used in a NodePart');
   }
-
-  if (!options) {
-    throw new Error('Missing animate options');
-  }
-
-  const entryName = `${options.prefix}-entry`;
-  const exitName = `${options.prefix}-exit`;
+  const { entry, exit } = options;
 
   let data = previousData.get(parentPart);
   const initialRender = !data;
 
   if (!data) {
+    // first render, create child parts
     data = {
       activePart: new NodePart(parentPart.options),
       inactivePart: new NodePart(parentPart.options),
@@ -113,53 +154,56 @@ export const animate = directive((value: unknown, options: AnimateOptions) => (p
     data.activePart.appendIntoPart(parentPart);
     data.inactivePart.appendIntoPart(parentPart);
   } else {
-    // if we should not animate, only update the rendered value
-    if (!shouldAnimate(data.value, value)) {
+    if (!renderValueChanged(data.value, value)) {
+      // value or template did not change, no need to run any animations
       data.activePart.setValue(value);
       data.activePart.commit();
       return;
     }
 
-    // animate the current element out
+    // animate or remove previously rendered value
     const { element, activePart } = data;
     if (element) {
-      const handleEvent = (e: Event) => {
-        if ((e as AnimationEvent).animationName === exitName) {
+      if (exit) {
+        // cancel any existing animation
+        if (entry) {
+          element.classList.remove(entry);
+        }
+
+        // animate the current element out, then remove it
+        runAnimation(element, exit).then(() => {
           activePart.setValue(nothing);
           activePart.commit();
-          element.removeEventListener('animationend', handleEvent);
-        }
-      };
-      element.addEventListener('animationend', handleEvent);
-
-      element.classList.remove(entryName);
-      element.classList.add(exitName);
+        });
+      } else {
+        // no need to animate, just remove it
+        activePart.setValue(nothing);
+        activePart.commit();
+      }
     }
   }
 
-  // render into the inactive part, so that the previous element can animate out
+  // swap the active/inactive parts
   const previousPart = data.activePart;
   data.activePart = data.inactivePart;
   data.inactivePart = previousPart;
 
+  // render the new template
   data.activePart.setValue(value);
   data.activePart.commit();
 
+  // look for the rendered element
   const element = findRenderedElement(data.activePart.startNode, data.activePart.endNode);
   data.element = element;
   data.value = value;
 
-  // animate rendered element if this isn't the initial render
-  if (!initialRender && element) {
-    const handleEvent = (e: Event) => {
-      if ((e as AnimationEvent).animationName === entryName) {
-        element.classList.remove(entryName);
-        element.removeEventListener('animationend', handleEvent);
-      }
-    };
-    element.addEventListener('animationend', handleEvent);
+  // cancel an existing exit animation, this can occur when quickly toggling templates
+  if (exit && element) {
+    element.classList.remove(exit);
+  }
 
-    element.classList.remove(exitName);
-    element.classList.add(entryName);
+  // animate rendered element if this isn't the initial render
+  if (!initialRender && element && entry) {
+    runAnimation(element, entry);
   }
 });
