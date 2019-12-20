817f502dfae56bcf1e6c25de45f03c812506c2e7
Justin Fagnani <justinfagnani@google.com>
2019-12-20 11:54:58 -0800

    Fix URLs a bit

--- docs-src/_includes/nav.11ty.cjs
+++ docs-src/_includes/nav.11ty.cjs
@@ -1,9 +1,9 @@
 module.exports = function(data) {
   return `
 <nav>
-  <a href="/">Home</a>
-  <a href="/examples/">Examples</a>
-  <a href="/api/">API</a>
-  <a href="/install/">Install</a>
+  <a href="../">Home</a>
+  <a href="../examples/">Examples</a>
+  <a href="../api/">API</a>
+  <a href="../install/">Install</a>
 </nav>`;
 };
--- docs-src/_includes/page.11ty.cjs
+++ docs-src/_includes/page.11ty.cjs
@@ -11,10 +11,10 @@ module.exports = function(data) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${data.title}</title>
-    <link rel="stylesheet" href="/docs.css">
+    <link rel="stylesheet" href="./docs.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600|Roboto+Mono">
-    <link href="/prism-okaidia.css" rel="stylesheet" />
-    <script type="module" src="/my-element.bundled.js"></script>
+    <link href="./prism-okaidia.css" rel="stylesheet" />
+    <script type="module" src="./my-element.bundled.js"></script>
   </head>
   <body>
     ${header()}
