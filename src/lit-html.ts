a451e73664f4a5e2253c83958784a55c5ef99f91
Justin Fagnani <justinfagnani@google.com>
2020-03-16 12:50:01 -0700

    Prepare 1.2.0-pre.1 release (#1117)

--- src/lit-html.ts
+++ src/lit-html.ts
@@ -57,7 +57,8 @@ declare global {
 // This line will be used in regexes to search for lit-html usage.
 // TODO(justinfagnani): inject version number at build time
 if (typeof window !== 'undefined') {
-  (window['litHtmlVersions'] || (window['litHtmlVersions'] = [])).push('1.1.2');
+  (window['litHtmlVersions'] || (window['litHtmlVersions'] = []))
+      .push('1.2.0-pre.1');
 }
 
 /**
