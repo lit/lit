9b360fb6b86cf6b88522af5a8c0dfdbd92b41e9e
Gray Norton <graynorton@google.com>
2020-04-21 15:31:30 -0700

    remove temp & experimental examples

--- packages/uni-virtualizer-examples/rollup.config.js
+++ packages/uni-virtualizer-examples/rollup.config.js
@@ -2,26 +2,6 @@ import resolve from 'rollup-plugin-node-resolve';
 
 export default [
   {
-    input: 'public/temp/index.js',
-    output: {
-      dir: 'public/temp/build',
-      format: 'esm'
-    },
-    plugins: [
-      resolve(),
-    ]
-  },
-  {
-    input: 'public/experimental/index.js',
-    output: {
-      dir: 'public/experimental/build',
-      format: 'esm'
-    },
-    plugins: [
-      resolve(),
-    ]
-  },
-  {
     input: 'public/basic-lit-html/index.js',
     output: {
       dir: 'public/basic-lit-html/build',
