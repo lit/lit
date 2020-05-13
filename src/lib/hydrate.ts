0caac982160f463b5c6938afa3282b208ddf75ef
Kevin Schaaf <kschaaf@google.com>
2020-05-12 18:12:28 -0700

    [hydration] Add noChange handling & align w/NodePart:commit (#1162)
    * Add noChange handling & align w/NodePart:commit
    
    * Factor the open, attr, and close operations for better readability

--- src/lib/hydrate.ts
+++ src/lib/hydrate.ts
@@ -12,7 +12,8 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
-import {AttributePart, BooleanAttributePart, isIterable, NodePart, PropertyPart} from './parts.js';
+import {noChange} from './part.js';
+import {AttributePart, BooleanAttributePart, isIterable, isPrimitive, NodePart, PropertyPart} from './parts.js';
 import {RenderOptions} from './render-options.js';
 import {parts} from './render.js';
 import {templateFactory} from './template-factory.js';
@@ -134,107 +135,151 @@ export const hydrate =
 
       const walker = document.createTreeWalker(
           container, NodeFilter.SHOW_COMMENT, null, false);
-      let node: Comment|null;
+      let marker: Comment|null;
 
       // Walk the DOM looking for part marker comments
-      while ((node = walker.nextNode() as Comment | null) !== null) {
-        const data = node.data;
-
-        if (data.startsWith('lit-part')) {
-          // This is an NodePart opening marker
-          let value: unknown;
-
-          if (stack.length === 0) {
-            // We don't have a parent part, so this must be the root part
-            if (rootPart !== undefined) {
-              throw new Error('there must be only one root part per container');
-            }
-            rootPart = currentNodePart = new NodePart({
-              templateFactory,
-              ...options,
-            });
-            value = rootValue;
-          } else {
-            const state = stack[stack.length - 1];
-
-            if (state.type === 'template-instance') {
-              // We have a current template instace, so use its template
-              // processor to create parts
-              currentNodePart =
-                  state.instance.processor.handleTextExpression(options);
-              state.instance.__parts.push(currentNodePart);
-              value = state.result.values[state.instancePartIndex++];
-              state.templatePartIndex++;
-            } else if (state.type === 'iterable') {
-              const result = state.iterator.next();
-              if (result.done) {
-                value = undefined;
-                state.done = true;
-                throw new Error('Unhandled shorter than expected iterable');
-              } else {
-                value = result.value;
-              }
-              currentNodePart = new NodePart(options);
-              (state.part.value as Array<NodePart>).push(currentNodePart);
-            } else {
-              // TODO: use the closest TemplateInstance's template processor
-              currentNodePart = new NodePart(options);
-            }
+      while ((marker = walker.nextNode() as Comment | null) !== null) {
+        const markerText = marker.data;
+        if (markerText.startsWith('lit-part')) {
+          if (stack.length === 0 && rootPart !== undefined) {
+            throw new Error('there must be only one root part per container');
           }
-
-          // We know the startNode now. We'll know the endNode when we get to
-          // the matching marker.
-          currentNodePart.startNode = node;
-
-          // Check for a template result digest
-          if (value instanceof TemplateResult) {
-            const markerWithDigest = `lit-part ${value.digest}`;
-            if (data === markerWithDigest) {
-              const template = options.templateFactory(value);
-              const instance =
-                  new TemplateInstance(template, value.processor, options);
-              stack.push({
-                type: 'template-instance',
-                instance,
-                part: currentNodePart,
-                templatePartIndex: 0,
-                instancePartIndex: 0,
-                result: value,
-              });
-              // For TemplateResult values, we set the part value to the
-              // generated TemplateInstance
-              currentNodePart.value = instance;
-            } else {
-              // TODO: if this isn't the server-rendered template, do we
-              // need to stop hydrating this subtree? Clear it? Add tests.
-              throw new Error('unimplemented');
-            }
-          } else {
-            if (typeof value === 'string') {
-              // TODO: implement the rest of the NodePart commit() cascade. Can
-              // we reuse the code?
-              stack.push({part: currentNodePart, type: 'leaf'});
-              currentNodePart.value = value;
-            } else if (isIterable(value)) {
-              // currentNodePart.value will contain an array of NodeParts
-              stack.push({
-                part: currentNodePart,
-                type: 'iterable',
-                value,
-                iterator: value[Symbol.iterator](),
-                done: false,
-              });
-              currentNodePart.value = [];
-            } else {
-              stack.push({part: currentNodePart, type: 'leaf'});
-            }
+          // Create a new NodePart and push it onto the stack
+          currentNodePart = openNodePart(rootValue, marker, stack, options);
+          if (rootPart === undefined) {
+            rootPart = currentNodePart;
           }
-        } else if (data.startsWith('/lit-part')) {
-          // This is an NodePart closing marker
-          if (currentNodePart === undefined) {
+        } else if (markerText.startsWith('lit-bindings')) {
+          // Create and hydrate attribute parts into the current NodePart on the
+          // stack
+          createAttributeParts(marker, stack, options);
+        } else if (markerText.startsWith('/lit-part')) {
+          // Close the current NodePart, and pop the previous one off the stack
+          if (stack.length === 1 && currentNodePart !== rootPart) {
+            throw new Error('internal error');
+          }
+          currentNodePart = closeNodePart(marker, currentNodePart, stack);
+        }
+      }
+      console.assert(
+          rootPart !== undefined,
+          'there should be exactly one root part in a render container');
+      parts.set(container, rootPart!);
+    };
+
+const openNodePart =
+    (rootValue: unknown,
+     marker: Comment,
+     stack: Array<NodePartState>,
+     options: RenderOptions) => {
+      let value: unknown;
+      let part;
+
+      // Create the node part
+      if (stack.length === 0) {
+        part = new NodePart({
+          templateFactory,
+          ...options,
+        });
+        value = rootValue;
+      } else {
+        const state = stack[stack.length - 1];
+        if (state.type === 'template-instance') {
+          // We have a current template instance, so use its template
+          // processor to create parts
+          part = state.instance.processor.handleTextExpression(options);
+          state.instance.__parts.push(part);
+          value = state.result.values[state.instancePartIndex++];
+          state.templatePartIndex++;
+        } else if (state.type === 'iterable') {
+          const result = state.iterator.next();
+          if (result.done) {
+            value = undefined;
+            state.done = true;
+            throw new Error('Unhandled shorter than expected iterable');
+          } else {
+            value = result.value;
+          }
+          part = new NodePart(options);
+          (state.part.value as Array<NodePart>).push(part);
+        } else {
+          // TODO: use the closest TemplateInstance's template processor
+          part = new NodePart(options);
+        }
+      }
+
+      // We know the startNode now. We'll know the endNode when we get to
+      // the matching marker and set it in closeNodePart()
+      part.startNode = marker;
+
+      // Initialize the NodePart state depending on the type of value and push
+      // it onto the stack. This logic closely follows the NodePart commit()
+      // cascade order:
+      // 1. directive (not yet implemented)
+      // 2. noChange
+      // 3. primitive (note strings must be handled before iterables, since they
+      //    are iterable)
+      // 4. TemplateResult
+      // 5. Node (not yet implemented, but fallback handling is fine)
+      // 6. Iterable
+      // 7. nothing (handled in fallback)
+      // 8. Fallback for everything else
+      if (value === noChange) {
+        stack.push({part, type: 'leaf'});
+      } else if (isPrimitive(value)) {
+        stack.push({part, type: 'leaf'});
+        part.value = value;
+      } else if (value instanceof TemplateResult) {
+        // Check for a template result digest
+        const markerWithDigest = `lit-part ${value.digest}`;
+        if (marker.data === markerWithDigest) {
+          const template = options.templateFactory(value);
+          const instance =
+              new TemplateInstance(template, value.processor, options);
+          stack.push({
+            type: 'template-instance',
+            instance,
+            part,
+            templatePartIndex: 0,
+            instancePartIndex: 0,
+            result: value,
+          });
+          // For TemplateResult values, we set the part value to the
+          // generated TemplateInstance
+          part.value = instance;
+        } else {
+          // TODO: if this isn't the server-rendered template, do we
+          // need to stop hydrating this subtree? Clear it? Add tests.
+          throw new Error('unimplemented');
+        }
+      } else if (isIterable(value)) {
+        // currentNodePart.value will contain an array of NodeParts
+        stack.push({
+          part: part,
+          type: 'iterable',
+          value,
+          iterator: value[Symbol.iterator](),
+          done: false,
+        });
+        part.value = [];
+      } else {
+        // Fallback for everything else (nothing, Objects, Functions,
+        // etc.): we just initialize the part's value
+        // Note that `Node` value types are not currently supported during
+        // SSR, so that part of the cascade is missing.
+        stack.push({part: part, type: 'leaf'});
+        part.value = value == null ? '' : value;
+      }
+      return part;
+    };
+
+const closeNodePart =
+    (marker: Comment, part: NodePart|undefined, stack: Array<NodePartState>):
+        NodePart|undefined => {
+          if (part === undefined) {
             throw new Error('unbalanced part marker');
           }
-          currentNodePart.endNode = node;
+          part.endNode = marker;
 
           const currentState = stack.pop()!;
 
@@ -244,85 +289,75 @@ export const hydrate =
             }
           }
 
-          // If we empty the stack, make sure the last state is for the root
-          // part
-          if (stack.length === 0) {
-            if (currentState.part !== rootPart) {
-              throw new Error('internal error');
-            }
-          } else {
+          if (stack.length > 0) {
             const state = stack[stack.length - 1];
-            currentNodePart = state.part;
-          }
-        } else if (data.startsWith('lit-bindings')) {
-          // The parent node has attribute bindings
-
-          // Get the nodeIndex from DOM. We're only using this for an integrity
-          // check right now, we might not need it.
-          const match = /lit-bindings (\d+)/.exec(data)!;
-          const nodeIndex = parseInt(match[1]);
-
-          const state = stack[stack.length - 1];
-          if (state.type === 'template-instance') {
-            const instance = state.instance;
-            let foundOnePart = false;
-            // eslint-disable-next-line no-constant-condition
-            while (true) {
-              const part = instance.template.parts[state.templatePartIndex];
-              if (part === undefined || part.type !== 'attribute' ||
-                  part.index !== nodeIndex) {
-                break;
-              }
-              foundOnePart = true;
-
-              const parent = node.parentElement!;
-              const attributeParts =
-                  instance.processor.handleAttributeExpressions(
-                      parent, part.name, part.strings, options);
-
-              // Prime the part values so subsequent dirty-checks work
-              for (const attributePart of attributeParts) {
-                // Set the part's current value, but only for AttributeParts,
-                // not PropertyParts. This is because properties are not
-                // represented in DOM so we do need to set them on initial
-                // render.
-
-                // TODO: only do this if we definitely have the same data as on
-                // the server. We need a flag like `dataChanged` or `sameData`
-                // for this.
-                if (attributePart instanceof AttributePart &&
-                    !(attributePart instanceof PropertyPart)) {
-                  // TODO: don't use a readonly field
-                  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                  (attributePart as any).value =
-                      state.result.values[state.instancePartIndex++];
-                  attributePart.committer.dirty = false;
-                } else if (attributePart instanceof BooleanAttributePart) {
-                  // TODO: this is ugly
-                  // TODO: tests
-                  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                  (attributePart as any).value =
-                      !!state.result.values[state.instancePartIndex++];
-                }
-                // Do nothing for EventPart... we need to run EventPart.commit()
-                // to actually add the event listener, so we require a commit
-                // Just like properties.
-              }
-              state.templatePartIndex++;
-              instance.__parts.push(...attributeParts);
-            }
-            if (!foundOnePart) {
-              // For a <!--lit-bindings--> marker there should be at least
-              // one attribute part.
-              throw new Error('internal error');
-            }
+            return state.part;
           } else {
-            throw new Error('internal error');
+            return undefined;
           }
+        };
+
+const createAttributeParts =
+    (node: Comment, stack: Array<NodePartState>, options: RenderOptions) => {
+      // Get the nodeIndex from DOM. We're only using this for an integrity
+      // check right now, we might not need it.
+      const match = /lit-bindings (\d+)/.exec(node.data)!;
+      const nodeIndex = parseInt(match[1]);
+
+      const state = stack[stack.length - 1];
+      if (state.type === 'template-instance') {
+        const instance = state.instance;
+        let foundOnePart = false;
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+          const part = instance.template.parts[state.templatePartIndex];
+          if (part === undefined || part.type !== 'attribute' ||
+              part.index !== nodeIndex) {
+            break;
+          }
+          foundOnePart = true;
+
+          const parent = node.parentElement!;
+          const attributeParts = instance.processor.handleAttributeExpressions(
+              parent, part.name, part.strings, options);
+
+          // Prime the part values so subsequent dirty-checks work
+          for (const attributePart of attributeParts) {
+            // Set the part's current value, but only for AttributeParts,
+            // not PropertyParts. This is because properties are not
+            // represented in DOM so we do need to set them on initial
+            // render.
+
+            // TODO: only do this if we definitely have the same data as on
+            // the server. We need a flag like `dataChanged` or `sameData`
+            // for this.
+            if (attributePart instanceof AttributePart &&
+                !(attributePart instanceof PropertyPart)) {
+              // TODO: don't use a readonly field
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              (attributePart as any).value =
+                  state.result.values[state.instancePartIndex++];
+              attributePart.committer.dirty = false;
+            } else if (attributePart instanceof BooleanAttributePart) {
+              // TODO: this is ugly
+              // TODO: tests
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              (attributePart as any).value =
+                  !!state.result.values[state.instancePartIndex++];
+            }
+            // Do nothing for EventPart... we need to run EventPart.commit()
+            // to actually add the event listener, so we require a commit
+            // Just like properties.
+          }
+          state.templatePartIndex++;
+          instance.__parts.push(...attributeParts);
         }
+        if (!foundOnePart) {
+          // For a <!--lit-bindings--> marker there should be at least
+          // one attribute part.
+          throw new Error('internal error');
+        }
+      } else {
+        throw new Error('internal error');
       }
-      console.assert(
-          rootPart !== undefined,
-          'there should be exactly one root part in a render container');
-      parts.set(container, rootPart!);
     };
