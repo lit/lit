bd10a6e3183ea060e7f59752b2af71bdeb07d906
Peter Burns <rictic@google.com>
2020-07-23 11:49:07 -0700

    Handle IE separately in unsafe-svg

--- src/directives/unsafe-svg.ts
+++ src/directives/unsafe-svg.ts
@@ -28,6 +28,8 @@ interface PreviousValue {
 // value passed to unsafeSVG.
 const previousValues = new WeakMap<NodePart, PreviousValue>();
 
+const isIe = window.navigator.userAgent.indexOf('Trident/') > 0;
+
 /**
  * Renders the result as SVG, rather than text.
  *
@@ -49,10 +51,18 @@ export const unsafeSVG = directive((value: unknown) => (part: Part): void => {
 
   const template = document.createElement('template');
   const content = template.content;
-  const svgElement =
-      document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  content.appendChild(svgElement);
-  svgElement.innerHTML = value as string;
+  let svgElement;
+  if (isIe) {
+    // IE can't set innerHTML of an svg element. However, it also doesn't
+    // support Trusted Types, so it's ok for us to use a string when setting
+    // innerHTML.
+    template.innerHTML = `<svg>${value}</svg>`;
+    svgElement = content.firstChild!;
+  } else {
+    svgElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    content.appendChild(svgElement);
+    svgElement.innerHTML = value as string;
+  }
   content.removeChild(svgElement);
   reparentNodes(content, svgElement.firstChild);
   const fragment = document.importNode(content, true);
