372f9c286cab95c3fb50ae9495158d5a8d9976d4
Alexander Marks <aomarks@google.com>
2020-08-06 11:51:57 -0700

    Expand lit-localize runtime module

--- README.md
+++ README.md
@@ -3,3 +3,109 @@
 [![Published on npm](https://img.shields.io/npm/v/lit-localize.svg)](https://www.npmjs.com/package/lit-localize) [![Test Status](https://github.com/PolymerLabs/lit-localize/workflows/tests/badge.svg?branch=master)](https://github.com/PolymerLabs/lit-localize/actions?query=workflow%3Atests+branch%3Amaster+event%3Apush)
 
 WIP
+
+## API
+
+The `lit-localize` module exports the following functions:
+
+### `configureLocalization(configuration)`
+
+Set runtime localization configuration.
+
+In runtime mode, this function must be called once, before any calls to `msg()`.
+
+The `configuration` object must have the following properties:
+
+- `sourceLocale: string`: Required locale code in which source templates in this
+  project are written, and the initial active locale.
+
+- `targetLocales: Iterable<string>`: Required locale codes that are supported by
+  this project. Should not include the `sourceLocale` code.
+
+- `loadLocale: (locale: string) => Promise<LocaleModule>`: Required function
+  that returns a promise of the localized templates for the given locale code.
+  For security, this function will only ever be called with a `locale` that is
+  contained by `targetLocales`.
+
+Example:
+
+```typescript
+configureLocalization({
+  sourceLocale: 'en',
+  targetLocales: ['es-419', 'zh_CN'],
+  loadLocale: (locale) => import(`/${locale}.js`),
+});
+```
+
+In transform mode, this function is not required, and calls to it will be
+replaced with `undefined`.
+
+### `getLocale(): string`
+
+Return the active locale code.
+
+In transform mode, calls to this function are transformed into the static locale
+code string for each emitted locale.
+
+### `setLocale(locale: string)`
+
+Set the active locale code, and begin loading templates for that locale using
+the `loadLocale` function that was passed to `configureLocalization`.
+
+In transform mode, calls to this function are replaced with `undefined`.
+
+### `localeReady(): Promise`
+
+Return a promise that is resolved when the next set of templates are loaded and
+available for rendering. Applications in runtime mode should always `await localeReady()` before rendering.
+
+In transform mode, calls to this function are replaced with `undefined`.
+
+### `msg(id: string, template, ...args): string|TemplateResult`
+
+Make a string or lit-html template localizable.
+
+The `id` parameter is a project-wide unique identifier for this template.
+
+The `template` parameter can have any of these types:
+
+- A plain string with no placeholders:
+
+  ```typescript
+  msg('greeting', 'Hello World!');
+  ```
+
+- A lit-html
+  [`TemplateResult`](https://lit-html.polymer-project.org/api/classes/_lit_html_.templateresult.html)
+  that may contain embedded HTML:
+
+  ```typescript
+  msg('greeting', html`Hello <b>World</b>!`);
+  ```
+
+- A function that returns a [template
+  literal](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals)
+  string that may contain placeholders. Placeholders may only reference
+  parameters of the function, which will be called with the 3rd and onwards
+  parameters to `msg`.
+
+  ```typescript
+  msg('greeting', (name) => `Hello ${name}!`, getUsername());
+  ```
+
+- A function that returns a lit-html
+  [`TemplateResult`](https://lit-html.polymer-project.org/api/classes/_lit_html_.templateresult.html)
+  that may contain embedded HTML, and may contain placeholders. Placeholders may
+  only reference parameters of the function, which will be called with the 3rd
+  and onwards parameters to `msg`:
+
+  ```typescript
+  msg('greeting', (name) => html`Hello <b>${name}</b>!`, getUsername());
+  ```
+
+In transform mode, calls to this function are replaced with the static localized
+template for each emitted locale. For example:
+
+```typescript
+html`Hola <b>${getUsername()}!</b>`;
+```
--- src_client/index.ts
+++ src_client/index.ts
@@ -13,6 +13,159 @@ import {TemplateResult} from 'lit-html';
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+/**
+ * Runtime configuration parameters for lit-localize.
+ */
+export interface Configuration {
+  /**
+   * Required locale code in which source templates in this project are written,
+   * and the initial active locale.
+   */
+  sourceLocale: string;
+
+  /**
+   * Required locale codes that are supported by this project. Should not
+   * include the `sourceLocale` code.
+   */
+  targetLocales: Iterable<string>;
+
+  /**
+   * Required function that returns a promise of the localized templates for the
+   * given locale code. For security, this function will only ever be called
+   * with a `locale` that is contained by `targetLocales`.
+   */
+  loadLocale: (locale: string) => Promise<LocaleModule>;
+}
+
+/**
+ * The template-like types that can be passed to `msg`.
+ */
+export type TemplateLike =
+  | string
+  | TemplateResult
+  | ((...args: any[]) => string)
+  | ((...args: any[]) => TemplateResult);
+
+/**
+ * A mapping from template ID to template.
+ */
+export type TemplateMap = {[id: string]: TemplateLike};
+
+/**
+ * The expected exports of a locale module.
+ */
+export interface LocaleModule {
+  templates: TemplateMap;
+}
+
+class Deferred<T> {
+  readonly promise: Promise<T>;
+  private _resolve!: (value: T) => void;
+  private _reject!: (error: Error) => void;
+  settled = false;
+
+  constructor() {
+    this.promise = new Promise<T>((resolve, reject) => {
+      this._resolve = resolve;
+      this._reject = reject;
+    });
+  }
+
+  resolve(value: T) {
+    this.settled = true;
+    this._resolve(value);
+  }
+
+  reject(error: Error) {
+    this.settled = true;
+    this._reject(error);
+  }
+}
+
+let activeLocale = '';
+let sourceLocale: string | undefined;
+let validLocales: Set<string> | undefined;
+let loadLocale: ((locale: string) => Promise<LocaleModule>) | undefined;
+let templates: TemplateMap | undefined;
+let loading = new Deferred<void>();
+
+/**
+ * Set runtime configuration parameters for lit-localize. This function must be
+ * called before using any other lit-localize function.
+ */
+export function configureLocalization(config: Configuration) {
+  activeLocale = sourceLocale = config.sourceLocale;
+  validLocales = new Set(config.targetLocales);
+  validLocales.add(config.sourceLocale);
+  loadLocale = config.loadLocale;
+}
+
+/**
+ * Return the active locale code. Returns empty string if lit-localize has not
+ * yet been configured.
+ */
+export function getLocale(): string {
+  return activeLocale;
+}
+
+/**
+ * Set the active locale code, and begin loading templates for that locale using
+ * the `loadLocale` function that was passed to `configureLocalization`.
+ */
+export function setLocale(newLocale: string): void {
+  if (
+    newLocale === activeLocale ||
+    !validLocales ||
+    !validLocales.has(newLocale) ||
+    !loadLocale
+  ) {
+    return;
+  }
+  activeLocale = newLocale;
+  templates = undefined;
+  if (loading.settled) {
+    loading = new Deferred();
+  }
+  if (newLocale === sourceLocale) {
+    loading.resolve();
+  } else {
+    loadLocale(newLocale).then(
+      (mod) => {
+        if (newLocale === activeLocale) {
+          templates = mod.templates;
+          loading.resolve();
+        }
+        // Else another locale was requested in the meantime. Don't resolve or
+        // reject, because the newer load call is going to use the same promise.
+        // Note the user can call getLocale() after the promise resolves if they
+        // need to check if the locale is still the one they expected to load.
+      },
+      (err) => {
+        if (newLocale === activeLocale) {
+          loading.reject(err);
+        }
+      }
+    );
+  }
+}
+
+/**
+ * Return a promise that is resolved when the next set of templates are loaded
+ * and available for rendering.
+ */
+export function localeReady(): Promise<void> {
+  return loading.promise;
+}
+
+/**
+ * Make a string or lit-html template localizable.
+ *
+ * @param id A project-wide unique identifier for this template.
+ * @param template A string, a lit-html template, or a function that returns
+ * either a string or lit-html template.
+ * @param args In the case that `template` is a function, it is invoked with
+ * the 3rd and onwards arguments to `msg`.
+ */
 export function msg(id: string, template: string): string;
 
 export function msg(id: string, template: TemplateResult): TemplateResult;
@@ -29,24 +182,19 @@ export function msg<F extends (...args: any[]) => TemplateResult>(
   ...params: Parameters<F>
 ): TemplateResult;
 
-/**
- * TODO(aomarks) This is a temporary stub implementation of the msg function. It
- * does not yet support actual localization, and is only used by the "transform"
- * output mode, since the user needs something to import.
- *
- * It may actually make more sense to move most of the generated code from
- * "runtime" mode into this library, so that users can
- * `import {msg} from 'lit-localize'` and tell it where to fetch translation
- * (this will make more sense after the planned revamp to support runtime async
- * locale loading and re-rendering).
- */
 export function msg(
-  _id: string,
-  template: string | TemplateResult | (() => string | TemplateResult),
+  id: string,
+  template: TemplateLike,
   ...params: any[]
 ): string | TemplateResult {
-  if (typeof template === 'function') {
-    return (template as any)(...params);
+  if (activeLocale !== sourceLocale && templates) {
+    const localized = templates[id];
+    if (localized) {
+      template = localized;
+    }
+  }
+  if (template instanceof Function) {
+    return template(...params);
   }
   return template;
 }
