ead386fdfb0d9a84d9308f01e2a4754254c1ed2c
Steve Orvell <sorvell@google.com>
2020-09-08 16:58:39 -0700

    Update packages/lit-element/src/lib/updating-controller.ts
    Co-authored-by: Arthur Evans <arthure@google.com>

--- packages/lit-element/src/lib/updating-controller.ts
+++ packages/lit-element/src/lib/updating-controller.ts
@@ -21,7 +21,7 @@ import {UpdatingElement, PropertyValues, connectCallback, updateCallback} from '
  */
 
 /**
- * Base controller class which can interact with an UpdatingElemrnt by hooking
+ * Base controller class which can interact with an UpdatingElement by hooking
  * into its lifecycle. The controller can perform tasks when it associated
  * element is connected or disconnected by implementing the `onConnected` and
  * `onDisconnected` methods. It can prompt the element to update by calling
