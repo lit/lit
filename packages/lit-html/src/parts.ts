d594c38a016a902dac85e488e3c1fab7cfde5315
Justin Fagnani <justinfagnani@google.com>
2020-09-09 12:23:41 -0700

    WIP

--- packages/lit-html/src/parts.ts
+++ packages/lit-html/src/parts.ts
@@ -35,3 +35,101 @@ export const restoreNodePart = (part: NodePart, state: NodePartState) => {
   (part as any).__commitNode((state as NodePartStateInternal).__fragment);
   part.__value = (state as NodePartStateInternal).__value;
 };
+
+const createMarker = () => document.createComment('');
+
+export const createAndInsertPart = (
+  containerPart: NodePart,
+  refPart?: NodePart
+): NodePart => {
+  const container = containerPart.__startNode.parentNode as Node;
+
+  const endNode =
+    refPart === undefined ? containerPart.__endNode : refPart.__startNode;
+
+  const startNode = container.insertBefore(createMarker(), endNode);
+
+  container.insertBefore(createMarker(), endNode);
+  const newPart = new NodePart(startNode, endNode, containerPart.options);
+  return newPart;
+};
+
+export const updatePart = (part: NodePart, value: unknown) => {
+  part.__setValue(value);
+  return part;
+};
+
+export const assertNodeMakers = (part: NodePart) => {
+  let start = part.__startNode;
+  let end = part.__endNode;
+
+  if (start === null) {
+    throw new Error('invalid startNode');
+  }
+  if (end === null) {
+    return; // fine?
+  }
+  while (start !== end) {
+    if (start.nextSibling === null) {
+      throw new Error('endNode not a sucessor sibling of startNode');
+    }
+    start = start.nextSibling
+  }
+};
+
+export const insertPartBefore = (
+  containerPart: NodePart,
+  part: NodePart,
+  refPart?: NodePart
+) => {
+  console.log('insertPartBefore');
+  const container = containerPart.__startNode.parentNode!;
+
+  const refNode = refPart ? refPart.__startNode : containerPart.__endNode;
+
+  const endNode = part.__endNode!.nextSibling;
+
+  if (endNode !== refNode) {
+    assertNodeMakers(part);
+    // console.log({startNode: part.__startNode, endNode, refNode});
+    reparentNodes(container, part.__startNode, endNode, refNode);
+  }
+};
+
+export const removePart = (part: NodePart) => {
+  removeNodes(
+    part.__startNode.parentNode!,
+    part.__startNode,
+    part.__endNode!.nextSibling
+  );
+};
+
+/**
+ * Reparents nodes, starting from `start` (inclusive) to `end` (exclusive),
+ * into another container (could be the same container), before `before`. If
+ * `before` is null, it appends the nodes to the container.
+ */
+export const reparentNodes =
+    (container: Node,
+     start: Node|null,
+     end: Node|null = null,
+     before: Node|null = null): void => {
+      while (start !== end) {
+        const n = start!.nextSibling;
+        container.insertBefore(start!, before);
+        start = n;
+      }
+    };
+
+/**
+ * Removes nodes, starting from `start` (inclusive) to `end` (exclusive), from
+ * `container`.
+ */
+export const removeNodes =
+  (container: Node, start: Node|null, end: Node|null = null): void => {
+    while (start !== end) {
+      const n = start!.nextSibling;
+      container.removeChild(start!);
+      start = n;
+    }
+  };
