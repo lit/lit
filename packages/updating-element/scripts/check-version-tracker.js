0a3170b72bae128b0b23e570dfd7c208ebb1e3c7
Steven Orvell <sorvell@google.com>
2020-10-09 11:10:11 -0700

    Address review feedback.

--- packages/updating-element/scripts/check-version-tracker.js
+++ packages/updating-element/scripts/check-version-tracker.js
@@ -16,12 +16,14 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const packageFile = fs.readFileSync(path.resolve('package.json'));
-const {version} = JSON.parse(packageFile);
-const ts = fs.readFileSync(path.resolve('src/lit-element.ts'));
+const {
+  version
+} = JSON.parse(packageFile);
+const ts = fs.readFileSync(path.resolve('src/updating-element.ts'));
 
 if (!ts.includes(`'${version}'`)) {
   console.log(
-    `\nExpected lit-element.ts to contain current version "${version}"`
+    `\nExpected updating-element.ts to contain current version "${version}"`
   );
   console.log(
     `Don't forget to update the version tracker string before release!`
