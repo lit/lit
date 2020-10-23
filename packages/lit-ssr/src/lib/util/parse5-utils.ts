ca7c666a1aa91865d38b0c68c605620ec28c9670
Kevin Schaaf <kschaaf@google.com>
2020-10-23 16:03:51 -0700

    Add licenses

--- packages/lit-ssr/src/lib/util/parse5-utils.ts
+++ packages/lit-ssr/src/lib/util/parse5-utils.ts
@@ -1,4 +1,18 @@
 /**
+ * @license
+ * Copyright (c) 2019 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+/**
  * TODO(usergenic): The following set of helper functions are more-or-less
  * copied from the npm package dom5 which could not be brought in at this
  * time because it is bound to `parse5@4` where this package uses `parse5@5`.
