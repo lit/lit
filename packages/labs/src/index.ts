dbc8918cd3e2ce8af8a157466be8483882c3c3f2
Steven Orvell <sorvell@google.com>
2020-10-26 11:26:07 -0700

    Adds empty entry point to build

--- packages/labs/src/index.ts
+++ packages/labs/src/index.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
+ * Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
  * This code may only be used under the BSD style license found at
  * http://polymer.github.io/LICENSE.txt
  * The complete set of authors may be found at
@@ -11,10 +11,3 @@
  * subject to an additional IP rights grant found at
  * http://polymer.github.io/PATENTS.txt
  */
-
-import {litProdConfig} from '../../rollup-common.js';
-
-export default litProdConfig({
-  entryPoints: ['index'],
-  external: ['updating-element', 'lit-html', 'lit-element'],
-});
