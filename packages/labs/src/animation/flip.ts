4da742e30974f0252fd83f5021a69236a085c3fd
Steven Orvell <sorvell@google.com>
2021-01-05 15:43:39 -0800

    Adds more examples and a position directive

--- packages/labs/src/animation/flip.ts
+++ packages/labs/src/animation/flip.ts
@@ -11,18 +11,25 @@ export type CSSProperties = {
 export type CSSPropertiesList = string[];
 
 export type FlipOptions = {
-  onComplete?: () => void;
+  onComplete?: (element: HTMLElement, flip: Flip) => void;
   animationOptions?: KeyframeAnimationOptions;
   properties?: CSSPropertiesList;
   in?: Keyframe[];
   out?: Keyframe[];
   fromElement?: Element;
   toElement?: Element;
+  stabilizeOut?: boolean;
+  guard?: () => unknown;
 };
 
-export const flyBelow = [
-  {transform: 'translateY(250%) scale(0.25)', opacity: 0},
-];
+export const flyBelow = [{transform: 'translateY(100%) scale(0)', opacity: 0}];
+
+export const flyAbove = [{transform: 'translateY(-100%) scale(0)', opacity: 0}];
+
+export const fadeOut = [{opacity: 0}];
+export const fade = fadeOut;
+
+export const fadeIn = [{opacity: 0}, {opacity: 1}];
 
 const diffOp = (a: number, b: number) => {
   const v = a - b;
@@ -52,32 +59,52 @@ const transformProps = {
   },
 };
 
-export class Flip extends DisconnectableDirective {
-  host?: ReactiveElement;
+export const defaultAnimationOptions: KeyframeAnimationOptions = {
+  duration: 333,
+  easing: `ease-in-out`,
+};
 
-  private _animation?: Animation;
-  private _reversing = false;
-  private _inFrames?: Keyframe[];
-  private _outFrames?: Keyframe[];
+export const defaultCssProperties: CSSPropertiesList = [
+  'left',
+  'top',
+  'width',
+  'height',
+  'opacity',
+];
+
+const isDirty = (value: unknown, previous: unknown) => {
+  if (value === undefined && previous === undefined) {
+    return true;
+  }
+  if (Array.isArray(value)) {
+    // Dirty-check arrays by item
+    if (
+      Array.isArray(previous) &&
+      previous.length === value.length &&
+      value.every((v, i) => v !== (previous as Array<unknown>)[i])
+    ) {
+      return true;
+    }
+  } else if (previous !== value) {
+    // Dirty-check non-arrays by identity
+    return true;
+  }
+  return false;
+};
+
+export class Flip extends DisconnectableDirective {
+  private _host?: ReactiveElement;
   private _from!: CSSProperties;
   private _to!: CSSProperties;
-  private _onComplete?: () => void;
   private _animatingElement!: HTMLElement;
-  private _fromElement!: Element;
-  private _toElement!: Element;
   private _parentNode: Element | null = null;
   private _nextSibling: Node | null = null;
-  private _properties: CSSPropertiesList = [
-    'left',
-    'top',
-    'width',
-    'height',
-    'opacity',
-  ];
-  private _animationOptions: KeyframeAnimationOptions = {
-    duration: 333,
-    easing: `ease-in-out`,
-  };
+  private _shouldAnimate = true;
+  private _previousValue: unknown;
+
+  reversing = false;
+  animation?: Animation;
+  options!: FlipOptions;
 
   constructor(part: PartInfo) {
     super(part);
@@ -93,44 +120,56 @@ export class Flip extends DisconnectableDirective {
   }
 
   update(part: AttributePart, [options]: Parameters<this['render']>) {
-    if (this.host === undefined) {
-      this.host = part.options?.host as ReactiveElement;
-      this.host.addController({
-        willUpdate: () => this._beforeUpdate(),
-        updated: () => this._afterUpdate(),
+    if (this._host === undefined) {
+      this._host = part.options?.host as ReactiveElement;
+      this._host.addController({
+        willUpdate: () => this._willUpdate(),
+        updated: () => this._updated(),
       });
     }
-    this._onComplete = options?.onComplete;
-    this._properties = options?.properties ?? this._properties;
-    this._animationOptions =
-      options?.animationOptions ?? this._animationOptions;
+    this.options = options || {};
+    this.options.animationOptions ??= defaultAnimationOptions;
+    this.options.properties ??= defaultCssProperties;
     this._animatingElement = part.element;
-    this._fromElement = options?.fromElement ?? this._animatingElement;
-    this._toElement = options?.toElement ?? this._animatingElement;
-    this._inFrames = options?.in;
-    this._outFrames = options?.out;
     return this.render(options);
   }
 
   private _record(element: Element, props: CSSProperties) {
     const bounds = element.getBoundingClientRect();
     const computedStyle = getComputedStyle(element);
-    this._properties.forEach((p) => {
+    this.options.properties!.forEach((p) => {
       const v =
         bounds[p as keyof typeof bounds] ??
         computedStyle[p as keyof CSSStyleDeclaration];
       const asNum = Number(v);
       props[p] = isNaN(asNum) ? String(v) : asNum;
     });
+    // console.log('measuring', element, props);
   }
 
-  private _beforeUpdate() {
-    if (this._isAnimating()) {
+  getMeasuredElement() {
+    const el = this.reversing
+      ? this.options.fromElement
+      : this.options.toElement;
+    return el ?? this._animatingElement;
+  }
+
+  private _willUpdate() {
+    const value = this.options.guard?.();
+    this._shouldAnimate =
+      !this._isAnimating() && isDirty(value, this._previousValue);
+    if (!this._shouldAnimate) {
       // TODO(sorvell): what should this do?
       //this.animation.cancel();
       return;
     }
-    const element = this._reversing ? this._toElement : this._fromElement;
+    // Copy the value if it's an array so that if it's mutated we don't forget
+    // what the previous values were.
+    if (value !== undefined) {
+      this._previousValue = Array.isArray(value) ? Array.from(value) : value;
+    }
+    //
+    const element = this.getMeasuredElement();
     if (element.isConnected) {
       this._record(element, (this._from = {}));
     }
@@ -138,19 +177,19 @@ export class Flip extends DisconnectableDirective {
     this._nextSibling = this._animatingElement.nextSibling;
   }
 
-  private _afterUpdate() {
-    if (this._isAnimating() || !this._animatingElement.isConnected) {
+  private _updated() {
+    if (!this._shouldAnimate || !this._animatingElement.isConnected) {
       return;
     }
-    const element = this._reversing ? this._fromElement : this._toElement;
-    this._reversing = !this._reversing;
+    const element = this.getMeasuredElement();
     this._record(element, (this._to = {}));
     const frames =
       this._from !== undefined
         ? this._calculateFrames(this._from, this._to)
-        : this._inFrames
-        ? [...this._inFrames, {}]
+        : this.options.in
+        ? [...this.options.in, {}]
         : undefined;
+    console.log('animation frames', frames);
     if (frames !== undefined) {
       this._animate(frames);
     }
@@ -160,29 +199,38 @@ export class Flip extends DisconnectableDirective {
 
   // Experimental animate out functionality.
   disconnectedCallback() {
+    if (!this._shouldAnimate) {
+      return;
+    }
     requestAnimationFrame(async () => {
-      if (this._parentNode?.isConnected && this._outFrames !== undefined) {
-        this._parentNode.insertBefore(
-          this._animatingElement,
-          this._nextSibling
-        );
+      if (this._parentNode?.isConnected && this.options.out !== undefined) {
+        const ref =
+          this._nextSibling && this._nextSibling.parentNode === this._parentNode
+            ? this._nextSibling
+            : null;
+        this._parentNode.insertBefore(this._animatingElement, ref);
         // Move to position before removal before animating
         const shifted: CSSProperties = {};
         this._record(this._animatingElement, shifted);
-        const left = diffOp(this._from.left as number, shifted.left as number);
-        // TODO(sorvell): these nudges could conflict with existing styling
-        // or animation but setting left/top should be rare, especially via
-        // animation.
-        if (left !== 0) {
-          this._animatingElement.style.position = 'relative';
-          this._animatingElement.style.left = left + 'px';
+        if (this.options.stabilizeOut) {
+          const left = diffOp(
+            this._from.left as number,
+            shifted.left as number
+          );
+          // TODO(sorvell): these nudges could conflict with existing styling
+          // or animation but setting left/top should be rare, especially via
+          // animation.
+          if (left !== 0) {
+            this._animatingElement.style.position = 'relative';
+            this._animatingElement.style.left = left + 'px';
+          }
+          const top = diffOp(this._from.top as number, shifted.top as number);
+          if (top !== 0) {
+            this._animatingElement.style.position = 'relative';
+            this._animatingElement.style.top = top + 'px';
+          }
         }
-        const top = diffOp(this._from.top as number, shifted.top as number);
-        if (top !== 0) {
-          this._animatingElement.style.position = 'relative';
-          this._animatingElement.style.top = top + 'px';
-        }
-        await this._animate(this._outFrames);
+        await this._animate(this.options.out);
         this._animatingElement.remove();
       }
     });
@@ -213,7 +261,7 @@ export class Flip extends DisconnectableDirective {
   }
 
   private _isAnimating() {
-    return this._animation?.playState === 'running';
+    return this.animation?.playState === 'running';
   }
 
   private async _animate(frames: Keyframe[]) {
@@ -221,14 +269,12 @@ export class Flip extends DisconnectableDirective {
       return;
     }
     //console.log('animate', frames);
-    this._animation = this._animatingElement.animate(
+    this.animation = this._animatingElement.animate(
       frames,
-      this._animationOptions
+      this.options.animationOptions
     );
-    await this._animation.finished;
-    if (this._onComplete) {
-      this._onComplete();
-    }
+    await this.animation.finished;
+    this.options.onComplete?.(this._animatingElement, this);
   }
 }
 
---
+++ packages/labs/src/animation/position.ts
@@ -0,0 +1,74 @@
+import {ReactiveElement} from 'reactive-element';
+import {nothing, AttributePart} from 'lit-html';
+import {directive, PartInfo, PartType} from 'lit-html/directive.js';
+import {DisconnectableDirective} from 'lit-html/disconnectable-directive.js';
+
+interface Positionables {
+  readonly bottom: number;
+  readonly height: number;
+  readonly left: number;
+  readonly right: number;
+  readonly top: number;
+  readonly width: number;
+}
+
+export type Positions = Array<keyof Positionables>;
+
+const positionedPoints = ['top', 'right', 'bottom', 'left'];
+
+export class Position extends DisconnectableDirective {
+  private _host?: ReactiveElement;
+  private _element?: Element;
+  private _targetCb?: () => HTMLElement;
+  private _positions?: Positions;
+  private _hasUpdated = false;
+
+  constructor(part: PartInfo) {
+    super(part);
+    if (part.type === PartType.CHILD) {
+      throw new Error(
+        'The `flip` directive must be used in attribute position.'
+      );
+    }
+  }
+
+  render(_targetCb: () => HTMLElement, _positions: Positions) {
+    return nothing;
+  }
+
+  update(
+    part: AttributePart,
+    [targetCb, positions]: Parameters<this['render']>
+  ) {
+    if (this._host === undefined) {
+      this._host = part.options?.host as ReactiveElement;
+      this._host.addController({
+        updated: () => this._updated(),
+      });
+    }
+    this._element = part.element;
+    this._targetCb = targetCb;
+    this._positions = positions ?? ['left', 'top', 'width', 'height'];
+    return this.render(targetCb, positions);
+  }
+
+  private _updated() {
+    this._position();
+  }
+
+  private _position() {
+    const target = this._targetCb?.();
+    const parent = target?.offsetParent;
+    if (target === undefined || !parent) {
+      return;
+    }
+    const tr = target.getBoundingClientRect();
+    const pr = parent.getBoundingClientRect();
+    this._positions?.forEach((p) => {
+      const x = positionedPoints.includes(p) ? tr[p] - pr[p] : tr[p];
+      (this._element as HTMLElement).style[p] = `${x}px`;
+    });
+  }
+}
+
+export const position = directive(Position);
