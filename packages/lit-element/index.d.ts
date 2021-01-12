4d77c9ac3527096bc756871002a1216205e7fbdb
Justin Fagnani <justinfagnani@google.com>
2021-01-12 08:54:41 -0800

    Remove/gitignore index.* files

--- packages/lit-element/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from '@lit/reactive-element';
-export * from 'lit-html';
-export * from './lit-element.js';
-export {_$private} from './lit-element.js';
-//# sourceMappingURL=index.d.ts.map
