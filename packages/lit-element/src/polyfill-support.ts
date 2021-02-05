6bff6fa2b79c976288a644123fa45cae0bf0f2ae
Kevin Schaaf <kschaaf@google.com>
2021-02-05 00:41:22 -0800

    [lit-next] Add tsconfig for dowleveling polyfill-support to ES5 (#1579)
    * Add tsconfig for dowleveling polyfill-support to ES5
    
    * Feature detect when patching as opposed to load
    Makes the polyfill-support agnostic to wcjs load ordering
    
    * Inject polyfill-support into tests when needed; add styling test
    
    * Add sauce and browserstack examples
    
    * Make super call ES5 downlevel-safe
    
    * Pass super args and tear off Reflect.construct
    
    * Avoid Reflect.construct
    
    * Fix ENABLE_SHADYDOM_NOPATCH replacement when downleveling
    
    * Warn on unconfigured browser
    
    * Replace var to var for ES5

--- packages/lit-element/src/polyfill-support.ts
+++ packages/lit-element/src/polyfill-support.ts
@@ -33,11 +33,6 @@
 import '@lit/reactive-element/polyfill-support.js';
 import 'lit-html/polyfill-support.js';
 
-const needsPlatformSupport = !!(
-  window.ShadyCSS !== undefined &&
-  (!window.ShadyCSS.nativeShadow || window.ShadyCSS.ApplyShim)
-);
-
 interface RenderOptions {
   readonly renderBefore?: ChildNode | null;
   scope?: string;
@@ -61,7 +56,13 @@ interface PatchableLitElement extends HTMLElement {
 }: {
   LitElement: PatchableLitElement;
 }) => {
-  if (!needsPlatformSupport) {
+  // polyfill-support is only needed if ShadyCSS or the ApplyShim is in use
+  // We test at the point of patching, which makes it safe to load
+  // webcomponentsjs and polyfill-support in either order
+  if (
+    window.ShadyCSS === undefined ||
+    (window.ShadyCSS.nativeShadow && !window.ShadyCSS.ApplyShim)
+  ) {
     return;
   }
 
