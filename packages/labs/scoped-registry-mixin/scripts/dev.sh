cc869d7f68deba734defff9004df076446739840
Kevin Schaaf <kschaaf@google.com>
2021-02-12 10:19:10 -0800

    Update README & licenses

--- packages/labs/scoped-registry-mixin/scripts/dev.sh
+++ packages/labs/scoped-registry-mixin/scripts/dev.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2020 The Polymer Project Authors. All rights reserved.
+# Copyright (c) 2021 The Polymer Project Authors. All rights reserved.
 # This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
 # The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 # The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
--- packages/labs/scoped-registry-mixin/src/test/scoped-registry-mixin_test.ts
+++ packages/labs/scoped-registry-mixin/src/test/scoped-registry-mixin_test.ts
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
+ * Copyright (c) 2021 The Polymer Project Authors. All rights reserved.
  * This code may only be used under the BSD style license found at
  * http://polymer.github.io/LICENSE.txt
  * The complete set of authors may be found at
