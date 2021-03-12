e8c67f9e7e053cc6f3872d46a91682cc14a17332
Elliott Marquez <emarquez@google.com>
2021-03-12 10:14:04 -0800

    feat(directives): add forward compat with lit-next

---
+++ src/async-directive.ts
@@ -0,0 +1,81 @@
+/**
+ * @license
+ * Copyright (c) 2021 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+import { Directive, Part, PartInfo } from "./directive.js";
+import * as legacyLit from "./lit-html.js";
+
+/**
+ * A superclass for directives that need to asynchronously update.
+ */
+export abstract class AsyncDirective extends Directive {
+  private readonly ddPart: legacyLit.Part;
+  private renderedYet = false;
+
+  constructor(partInfo: PartInfo) {
+    super(partInfo);
+    this.ddPart = (partInfo as Part).legacyPart;
+  }
+
+  private ddGetNode(): Node | undefined {
+    if (this.ddPart instanceof legacyLit.NodePart) {
+      return this.ddPart.startNode;
+    } else if (this.ddPart instanceof legacyLit.EventPart) {
+      return this.ddPart.element;
+    } else if (this.ddPart instanceof legacyLit.BooleanAttributePart) {
+      return this.ddPart.element;
+    } else if (
+      this.ddPart instanceof legacyLit.PropertyPart ||
+      this.ddPart instanceof legacyLit.AttributePart
+    ) {
+      return this.ddPart.committer.element;
+    }
+    return undefined;
+  }
+
+  private shouldRender() {
+    if (!this.renderedYet) {
+      this.renderedYet = true;
+      return true;
+    }
+    const node = this.ddGetNode();
+    if (node != null && !node.isConnected) {
+      // node is disconnected, do not render
+      return false;
+    }
+    return true;
+  }
+
+  setValue(value: unknown) {
+    if (!this.shouldRender()) {
+      // node is disconnected, do nothing.
+      return;
+    }
+
+    this.ddPart.setValue(value);
+    this.ddPart.commit();
+  }
+
+  /**
+   * User callbacks for implementing logic to release any
+   * resources/subscriptions that may have been retained by this directive.
+   * Since directives may also be re-connected, `reconnected` should also be
+   * implemented to restore the working state of the directive prior to the next
+   * render.
+   *
+   * In the v1 version of these APIs, we don't monitor for disconnection and
+   * reconnection, we only call these methods when setValue is called.
+   */
+  protected disconnected() {}
+  protected reconnected() {}
+}
---
+++ src/directive.ts
@@ -0,0 +1,284 @@
+/**
+ * @license
+ * Copyright (c) 2021 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+import { directive as legacyDirective } from "./lib/directive.js";
+import * as legacyLit from "./lit-html.js";
+
+export const PartType = {
+  ATTRIBUTE: 1,
+  CHILD: 2,
+  PROPERTY: 3,
+  BOOLEAN_ATTRIBUTE: 4,
+  EVENT: 5,
+  ELEMENT: 6,
+} as const;
+
+export type PartType = typeof PartType[keyof typeof PartType];
+
+export type ChildPartInfo = {
+  readonly type: typeof PartType.CHILD;
+};
+
+export type AttributePartInfo = {
+  readonly type:
+    | typeof PartType.ATTRIBUTE
+    | typeof PartType.PROPERTY
+    | typeof PartType.BOOLEAN_ATTRIBUTE
+    | typeof PartType.EVENT;
+  readonly strings?: ReadonlyArray<string>;
+  readonly name: string;
+  readonly tagName: string;
+};
+
+export type Part = ChildPart | AttributePart | BooleanAttributePart | EventPart;
+
+type Interface<T> = {
+  [P in keyof T]: T[P];
+};
+
+export type ChildPart = Interface<ChildPartImpl>;
+class ChildPartImpl {
+  readonly type = PartType.CHILD;
+  readonly options: legacyLit.RenderOptions | undefined;
+  constructor(readonly legacyPart: legacyLit.NodePart) {
+    this.options = legacyPart.options;
+  }
+
+  get parentNode(): Node {
+    return this.legacyPart.startNode.parentNode!;
+  }
+}
+
+/**
+ * Yields all toplevel nodes inside the dom range managed by the ChildPart.
+ *
+ * So if the ChildPart has rendered:
+ *     html`<label><input></label> <div></div>`
+ *
+ * This function will yield the label and the div, but not the input.
+ */
+export function getRenderedNodes(childPart: ChildPart) {
+  const results = [];
+  const impl = childPart as ChildPartImpl;
+  const part = impl.legacyPart;
+  for (
+    let node: Node | null = part.startNode;
+    node && node !== part.endNode;
+    node = node.nextSibling
+  ) {
+    const n: Node = node;
+    results.push(n);
+  }
+  return results;
+}
+
+export type AttributePart = Interface<AttributePartImpl>;
+class AttributePartImpl {
+  readonly type: typeof PartType.ATTRIBUTE | typeof PartType.PROPERTY;
+
+  get options(): legacyLit.RenderOptions | undefined {
+    return undefined;
+  }
+  get name(): string {
+    return this.legacyPart.committer.name;
+  }
+
+  get element(): Element {
+    return this.legacyPart.committer.element;
+  }
+
+  /**
+   * If this attribute part represents an interpolation, this contains the
+   * static strings of the interpolation. For single-value, complete bindings,
+   * this is undefined.
+   */
+  get strings() {
+    return this.legacyPart.committer.strings;
+  }
+  get tagName() {
+    return this.element.tagName;
+  }
+
+  constructor(
+    readonly legacyPart: legacyLit.AttributePart | legacyLit.PropertyPart
+  ) {
+    if (legacyPart instanceof legacyLit.PropertyPart) {
+      this.type = PartType.PROPERTY;
+    } else {
+      this.type = PartType.ATTRIBUTE;
+    }
+  }
+}
+
+export type BooleanAttributePart = Interface<BooleanAttributePartImpl>;
+class BooleanAttributePartImpl {
+  readonly type = PartType.BOOLEAN_ATTRIBUTE;
+
+  get options(): legacyLit.RenderOptions | undefined {
+    return undefined;
+  }
+  get name(): string {
+    return this.legacyPart.name;
+  }
+
+  get element(): Element {
+    return this.legacyPart.element;
+  }
+
+  /**
+   * If this attribute part represents an interpolation, this contains the
+   * static strings of the interpolation. For single-value, complete bindings,
+   * this is undefined.
+   */
+  get strings() {
+    return this.legacyPart.strings;
+  }
+  get tagName() {
+    return this.element.tagName;
+  }
+
+  constructor(readonly legacyPart: legacyLit.BooleanAttributePart) {}
+}
+
+/**
+ * An AttributePart that manages an event listener via add/removeEventListener.
+ *
+ * This part works by adding itself as the event listener on an element, then
+ * delegating to the value passed to it. This reduces the number of calls to
+ * add/removeEventListener if the listener changes frequently, such as when an
+ * inline function is used as a listener.
+ *
+ * Because event options are passed when adding listeners, we must take case
+ * to add and remove the part as a listener when the event options change.
+ */
+export type EventPart = Interface<EventPartImpl>;
+class EventPartImpl {
+  readonly type = PartType.EVENT;
+  constructor(readonly legacyPart: legacyLit.EventPart) {}
+
+  get options(): legacyLit.RenderOptions | undefined {
+    return undefined;
+  }
+  get name(): string {
+    return this.legacyPart.eventName;
+  }
+
+  get element(): Element {
+    return this.legacyPart.element;
+  }
+
+  /**
+   * If this attribute part represents an interpolation, this contains the
+   * static strings of the interpolation. For single-value, complete bindings,
+   * this is undefined.
+   */
+  get strings() {
+    return undefined;
+  }
+  get tagName() {
+    return this.element.tagName;
+  }
+
+  handleEvent(event: Event) {
+    this.legacyPart.handleEvent(event);
+  }
+}
+
+// no equivalent for ElementPart in v1
+
+function legacyPartToPart(part: legacyLit.Part): Part {
+  if (part instanceof legacyLit.NodePart) {
+    return new ChildPartImpl(part);
+  } else if (part instanceof legacyLit.EventPart) {
+    return new EventPartImpl(part);
+  } else if (part instanceof legacyLit.BooleanAttributePart) {
+    return new BooleanAttributePartImpl(part);
+  } else if (
+    part instanceof legacyLit.PropertyPart ||
+    part instanceof legacyLit.AttributePart
+  ) {
+    return new AttributePartImpl(part);
+  }
+  // ElementPartInfo doesn't exist in lit-html v1
+  throw new Error(`Unknown part type`);
+}
+
+/**
+ * Information about the part a directive is bound to.
+ *
+ * This is useful for checking that a directive is attached to a valid part,
+ * such as with directive that can only be used on attribute bindings.
+ */
+export type PartInfo = ChildPartInfo | AttributePartInfo;
+
+export type DirectiveClass = {
+  new (part: PartInfo): Directive;
+};
+
+/**
+ * This utility type extracts the signature of a directive class's render()
+ * method so we can use it for the type of the generated directive function.
+ */
+export type DirectiveParameters<C extends Directive> = Parameters<C["render"]>;
+
+/**
+ * A generated directive function doesn't evaluate the directive, but just
+ * returns a DirectiveResult object that captures the arguments.
+ */
+export type DirectiveResult<C extends DirectiveClass = DirectiveClass> = {
+  /** @internal */
+  _$litDirective$: C;
+  /** @internal */
+  values: DirectiveParameters<InstanceType<C>>;
+};
+
+/**
+ * Base class for creating custom directives. Users should extend this class,
+ * implement `render` and/or `update`, and then pass their subclass to
+ * `directive`.
+ */
+export abstract class Directive {
+  constructor(partInfo: PartInfo) {}
+  abstract render(...props: Array<unknown>): unknown;
+  update(part: Part, props: Array<unknown>): unknown {
+    return this.render(...props);
+  }
+}
+
+export function directive<C extends DirectiveClass>(directiveClass: C) {
+  const partToInstance = new WeakMap<
+    legacyLit.Part,
+    readonly [Part, InstanceType<C>]
+  >();
+  const result = legacyDirective((...props: unknown[]) => {
+    return (part: legacyLit.Part) => {
+      const cached = partToInstance.get(part);
+      let modernPart, instance;
+      if (cached === undefined) {
+        modernPart = legacyPartToPart(part);
+        instance = new directiveClass(modernPart) as InstanceType<C>;
+        partToInstance.set(part, [modernPart, instance] as const);
+      } else {
+        modernPart = cached[0];
+        instance = cached[1];
+      }
+      part.setValue(instance.update(modernPart, props));
+      part.commit();
+    };
+  });
+
+  return result as (
+    ...props: DirectiveParameters<InstanceType<C>>
+  ) => (part: legacyLit.Part) => void;
+}
