3ba0c1ef17b93f483ae17b60706ef7cfdadc3ecb
Elliott Marquez <emarquez@google.com>
2021-03-12 11:16:00 -0800

    chore: format

--- src/directive.ts
+++ src/directive.ts
@@ -22,7 +22,7 @@ export const PartType = {
   BOOLEAN_ATTRIBUTE: 4,
   EVENT: 5,
   ELEMENT: 6,
-} as const;
+} as const ;
 
 export type PartType = typeof PartType[keyof typeof PartType];
 
@@ -32,10 +32,11 @@ export interface ChildPartInfo {
 
 export interface AttributePartInfo {
   readonly type:|typeof PartType.ATTRIBUTE|
-               typeof PartType.PROPERTY|
-               typeof PartType.BOOLEAN_ATTRIBUTE|typeof PartType.EVENT;
-  readonly strings?: ReadonlyArray<string>; readonly name: string; readonly tagName:
-                                                                                string;
+      typeof PartType.PROPERTY|typeof PartType.BOOLEAN_ATTRIBUTE|
+      typeof PartType.EVENT;
+  readonly strings?: ReadonlyArray<string>;
+  readonly name: string;
+  readonly tagName: string;
 }
 
 export type Part = ChildPart|AttributePart|BooleanAttributePart|EventPart;
@@ -217,7 +218,7 @@ function legacyPartToPart(part: legacyLit.Part): Part {
 export type PartInfo = ChildPartInfo|AttributePartInfo;
 
 export interface DirectiveClass {
-  new (part: PartInfo): Directive;
+  new(part: PartInfo): Directive;
 }
 
 /**
@@ -261,7 +262,7 @@ export function directive<C extends DirectiveClass>(directiveClass: C) {
       if (cached === undefined) {
         modernPart = legacyPartToPart(part);
         instance = new directiveClass(modernPart) as InstanceType<C>;
-        partToInstance.set(part, [modernPart, instance] as const);
+        partToInstance.set(part, [modernPart, instance] as const );
       } else {
         modernPart = cached[0];
         instance = cached[1];
