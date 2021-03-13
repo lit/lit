581d98ba854ad756239bbd9dcc24dd49b3dec9e1
Alexander Marks <aomarks@google.com>
2021-03-13 08:54:48 -0800

    Factor out @lit/localize-tools package
    Previously, @lit/localize included both the client library and the CLI. Now,
    it only includes the client library, and the CLI is distributed in this new
    package.

---
+++ packages/localize-tools/testdata/extract-xlb/goldens/foo.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {html} from 'lit-html';
+import {msg} from '@lit/localize';
+
+// msgdesg: Description of Hello World
+msg('Hello World!');
+
+const name = 'friend';
+// msgdesc: Description of Hello $(name)
+msg(html`Hello <b>${name}!</b>`);
---
+++ packages/localize-tools/testdata/xliff-init/input/foo.ts
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {msg} from '@lit/localize';
+
+msg('Hello World!');
