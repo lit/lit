3929e66292ef032eaf179b00f859465a195a08fd
Arthur Evans <arthure@google.com>
2021-03-17 10:36:07 -0700

    Move doc comment & add brief example.

--- packages/lit-html/src/directives/repeat.ts
+++ packages/lit-html/src/directives/repeat.ts
@@ -28,26 +28,6 @@ const generateMap = (list: unknown[], start: number, end: number) => {
   return map;
 };
 
-/**
- * A directive that repeats a series of values (usually `TemplateResults`)
- * generated from an iterable, and updates those items efficiently when the
- * iterable changes based on user-provided `keys` associated with each item.
- *
- * Note that if a `keyFn` is provided, strict key-to-DOM mapping is maintained,
- * meaning previous DOM for a given key is moved into the new position if
- * needed, and DOM will never be reused with values for different keys (new DOM
- * will always be created for new keys). This is generally the most efficient
- * way to use `repeat` since it performs minimum unnecessary work for insertions
- * and removals.
- *
- * IMPORTANT: If providing a `keyFn`, keys *must* be unique for all items in a
- * given call to `repeat`. The behavior when two or more items have the same key
- * is undefined.
- *
- * If no `keyFn` is provided, this directive will perform similar to mapping
- * items to values, and DOM will be reused against potentially different items.
- */
-
 class RepeatDirective extends Directive {
   itemKeys?: unknown[];
 
@@ -443,4 +423,30 @@ export type RepeatDirectiveFn = <T>(
   template?: ItemTemplate<T>
 ) => unknown;
 
+/**
+ * A directive that repeats a series of values (usually `TemplateResults`)
+ * generated from an iterable, and updates those items efficiently when the
+ * iterable changes based on user-provided `keys` associated with each item.
+ *
+ * Note that if a `keyFn` is provided, strict key-to-DOM mapping is maintained,
+ * meaning previous DOM for a given key is moved into the new position if
+ * needed, and DOM will never be reused with values for different keys (new DOM
+ * will always be created for new keys). This is generally the most efficient
+ * way to use `repeat` since it performs minimum unnecessary work for insertions
+ * and removals.
+ * 
+ * The `keyFn` takes two parameters, the item and its index, and returns a unique key value.
+ *
+ * ```js
+ * ${repeat(this.items, (item) => item.id, (item, index) =>
+     html`<li>${index}: ${item.name}</li>`)}
+ * ```
+ *
+ * **Important**: If providing a `keyFn`, keys *must* be unique for all items in a
+ * given call to `repeat`. The behavior when two or more items have the same key
+ * is undefined.
+ *
+ * If no `keyFn` is provided, this directive will perform similar to mapping
+ * items to values, and DOM will be reused against potentially different items.
+ */
 export const repeat = directive(RepeatDirective) as RepeatDirectiveFn;
