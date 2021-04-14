toddpress/lit-html

Initial implementation of serverUntil & ServerController

rollup-common.js
@@ -94,6 +94,8 @@ const stableProperties = {
   _$clear: 'U',
   // lit-html: Directive (used by private-ssr-support)
   _$resolve: 'V',
+  // reactive-element: ReactiveElement (used by private-ssr-support)
+  _$controllers: 'W',
 };
 
 // Validate stableProperties list, just to be safe; catches dupes and

lit/lit

rollup-common.js
@@ -92,6 +92,8 @@ const stableProperties = {
   _$resolve: 'S',
   // lit-html: Directive (used by lit-html)
   _$initialize: 'T',
+  // reactive-element: ReactiveElement (used by private-ssr-support)
+  _$controllers: 'U',
 };
 
 const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');