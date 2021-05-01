0df812ccfcbdd6d9ad5da53262501e8feb112887
Rong Sen Ng <wes.ngrongsen@gmail.com>
2021-05-01 17:47:20 +0800

    feat: add directives.js to fix #1736

---
+++ packages/lit/src/directives.ts
@@ -0,0 +1,20 @@
+/**
+ * @license
+ * Copyright 2017 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+ export * from 'lit-html/directives/async-append.js';
+ export * from 'lit-html/directives/async-replace.js';
+ export * from 'lit-html/directives/cache.js';
+ export * from 'lit-html/directives/class-map.js';
+ export * from 'lit-html/directives/guard.js';
+ export * from 'lit-html/directives/if-defined.js';
+ export * from 'lit-html/directives/live.js';
+ export * from 'lit-html/directives/ref.js';
+ export * from 'lit-html/directives/repeat.js';
+ export * from 'lit-html/directives/style-map.js';
+ export * from 'lit-html/directives/template-content.js';
+ export * from 'lit-html/directives/unsafe-html.js';
+ export * from 'lit-html/directives/unsafe-svg.js';
+ export * from 'lit-html/directives/until.js';
