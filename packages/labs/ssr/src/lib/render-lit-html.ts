4d650dc41a4a6dcf293afc912ddc56a35c910c7d
Kevin Schaaf <kschaaf@google.com>
2021-08-10 18:45:58 -0700

    Fix SSR build

--- packages/labs/ssr/src/lib/render-lit-html.ts
+++ packages/labs/ssr/src/lib/render-lit-html.ts
@@ -6,7 +6,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import type {TemplateResult, ChildPart, RenderOptions} from 'lit';
+import type {TemplateResult, ChildPart} from 'lit';
 import type {
   Directive,
   DirectiveClass,
