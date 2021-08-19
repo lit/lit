38dd7189c8350aeaef797c979478eb0c9ad789ac
Steven Orvell <sorvell@google.com>
2021-08-19 16:17:56 -0700

    Fxes #2063
    Issues a DEV_MODE warning if a ChildPart cannot accept values because its marker nodes have been ejected from the part's `parentNode`.

--- packages/lit-html/src/lit-html.ts
+++ packages/lit-html/src/lit-html.ts
@@ -1041,6 +1041,11 @@ class ChildPart implements Disconnectable {
   }
 
   _$setValue(value: unknown, directiveParent: DirectiveParent = this): void {
+    if (DEV_MODE && this.parentNode === null) {
+      throw new Error(
+        `This \`ChildPart\` has no \`parentNode\` and therefore cannot accept the value of ${value}. This likely means the element containing the part was manipulated in an unsupported way outside of Lit's control such that the part's marker nodes were ejected from DOM. For example, setting the element's \`innerHTML\` or \`textContent\` can do this.`
+      );
+    }
     value = resolveDirective(this, value, directiveParent);
     if (isPrimitive(value)) {
       // Non-rendering child values. It's important that these do not render
