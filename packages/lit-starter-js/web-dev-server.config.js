630c4a28210d8037a23da52fcffddb8c018a69f9
Kevin Schaaf <kschaaf@google.com>
2021-08-31 17:30:36 -0700

    Add config for running tests in dev mode

--- packages/lit-starter-js/web-dev-server.config.js
+++ packages/lit-starter-js/web-dev-server.config.js
@@ -6,8 +6,13 @@
 
 import {legacyPlugin} from '@web/dev-server-legacy';
 
+const mode = process.env.MODE || 'dev';
+if (!['dev', 'prod'].includes(mode)) {
+  throw new Error(`MODE must be "dev" or "prod", was "${mode}"`);
+}
+
 export default {
-  nodeResolve: true,
+  nodeResolve: {exportConditions: mode === 'dev' ? ['development'] : []},
   preserveSymlinks: true,
   plugins: [
     legacyPlugin({
