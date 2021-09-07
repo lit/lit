6945b41985ba073105fe7e7d4c7cc226783e2479
Andrew Jakubowicz <ajakubowicz@google.com>
2021-09-07 10:17:53 -0700

    1000 item count, 1 update count - 10 classes

--- packages/benchmarks/lit-element/class-map/index.ts
+++ packages/benchmarks/lit-element/class-map/index.ts
@@ -57,7 +57,7 @@ function generateData(number = 1000): IData[] {
   const { state } = decorators;
   // Settings
   const itemCount = 1000;
-  const updateCount = 6;
+  const updateCount = 1;
 
   const data = generateData(itemCount);
 
