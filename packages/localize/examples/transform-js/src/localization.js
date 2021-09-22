982f9b8e16325d82a7dbcba9ef6ad8c3a616670e
Alexander Marks <aomarks@google.com>
2021-09-22 16:56:16 -0700

    Add transform-js localize example

---
+++ packages/localize/examples/transform-js/src/localization.js
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {configureTransformLocalization} from '@lit/localize';
+
+export const {getLocale} = configureTransformLocalization({
+  sourceLocale: 'en',
+});
