2be79486c8a547a1fece302b4be8bdbf477616db
Steven Orvell <sorvell@google.com>
2021-12-07 14:55:49 -0800

    Update builds

--- packages/labs/observers/src/index.ts
+++ packages/labs/observers/src/index.ts
@@ -4,3 +4,6 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 export * from './mutation_controller.js';
+export * from './performance_controller.js';
+export * from './resize_controller.js';
+export * from './intersection_controller.js';
