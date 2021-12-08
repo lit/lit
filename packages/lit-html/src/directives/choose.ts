7545a5d3425a31bdb83a0f5859ed17aa519cb000
Justin Fagnani <justinfagnani@google.com>
2021-12-08 11:05:22 -0800

    Address feedback

--- packages/lit-html/src/directives/choose.ts
+++ packages/lit-html/src/directives/choose.ts
@@ -9,10 +9,25 @@
  * the given `value` to a case.
  *
  * Cases are structured as `[caseValue, func]`. `value` is matched to
- * `caseValue` by strict equality. The first match is selected.
+ * `caseValue` by strict equality. The first match is selected. Case values
+ * can be of any type including primitives, objects, and symbols.
  *
  * This is similar to a switch statement, but as an expression and without
  * fallthrough.
+ *
+ * @example
+ *
+ * ```ts
+ * render() {
+ *   return html`
+ *     ${choose(this.section, [
+ *       ['home', () => <h1>Home</h1>]
+ *       ['about', () => <h1>About</h1>]
+ *     ],
+ *     () => html`<h1>Error</h1>)}
+ *   `;
+ * }
+ * ```
  */
 export const choose = <T, V>(
   value: T,
@@ -21,8 +36,8 @@ export const choose = <T, V>(
 ) => {
   for (const c of cases) {
     const caseValue = c[0];
-    const fn = c[1];
     if (caseValue === value) {
+      const fn = c[1];
       return fn();
     }
   }
