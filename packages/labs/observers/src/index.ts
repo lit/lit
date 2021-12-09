e81e4fda512d50574fe89eaaf1a804ba616ff128
Steven Orvell <sorvell@google.com>
2021-12-09 12:07:31 -0800

    Address review feedback: avoid exporting everything from index.ts
    Instead users should explicitly import the controller they want to use.

--- packages/labs/observers/src/index.ts
+++ packages/labs/observers/src/index.ts
@@ -3,7 +3,3 @@
  * Copyright 2021 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-export * from './mutation_controller.js';
-export * from './performance_controller.js';
-export * from './resize_controller.js';
-export * from './intersection_controller.js';
