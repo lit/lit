1d6bbbad145214b4131332b1d5a5b62ccb4a6e36
Andrew Jakubowicz <ajakubowicz@google.com>
2021-12-14 10:36:21 -0800

    Remove lowered concurrency to reflect local sauce tests.

--- packages/tests/web-test-runner.config.js
+++ packages/tests/web-test-runner.config.js
@@ -209,7 +209,7 @@ export default {
   // For ie11 where we run tests sequentially this timeout needs to be long
   // enough so that blocked tests have time to wait for all previous test files
   // to run to completion.
-  testsStartTimeout: 60000 * 20, // default 120000
+  testsStartTimeout: 60000 * 6, // default 120000
   testsFinishTimeout: 120000, // default 20000
   testFramework: {
     // https://mochajs.org/api/mocha
