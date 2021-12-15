481c98859b2a3955cba7bcd573fadb791ec4a382
Russell Bicknell <bicknellr@google.com>
2021-12-14 17:06:38 -0800

    Add everything else to the 'max' bundles.

--- packages/lit/src/index.max.ts
+++ packages/lit/src/index.max.ts
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+import './polyfill-support.js';
+
 // Although these are re-exported from lit-element.js, we add
 // them here to effectively pre-fetch them and avoid the extra
 // waterfall when loading the lit package unbundled
@@ -11,3 +13,35 @@ import '@lit/reactive-element';
 import 'lit-html';
 
 export * from 'lit-element/lit-element.js';
+
+export * from './async-directive.js';
+export * from './decorators.js';
+export * from './directive-helpers.js';
+export * from './directive.js';
+export * from './directives/async-append.js';
+export * from './directives/async-replace.js';
+export * from './directives/cache.js';
+export * from './directives/choose.js';
+export * from './directives/class-map.js';
+export * from './directives/guard.js';
+export * from './directives/if-defined.js';
+export * from './directives/join.js';
+export * from './directives/keyed.js';
+export * from './directives/live.js';
+export * from './directives/map.js';
+export * from './directives/range.js';
+export * from './directives/ref.js';
+export * from './directives/repeat.js';
+export * from './directives/style-map.js';
+export * from './directives/template-content.js';
+export * from './directives/unsafe-html.js';
+export * from './directives/unsafe-svg.js';
+export * from './directives/until.js';
+export * from './directives/when.js';
+export {
+  html as staticHtml,
+  literal,
+  svg as staticSVG,
+  unsafeStatic,
+  withStatic,
+} from './static-html.js';
