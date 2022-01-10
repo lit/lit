34ab874d75f2f45390f20ffcca55b6b27b5b723d
Russell Bicknell <bicknellr@google.com>
2022-01-10 11:22:40 -0800

    Revert "Remove everything from `packages/lit/src/index.ts`."
    This reverts commit d846d9703c3a7a0f63fb804a5a481a3e872a167e.

--- packages/lit/src/index.ts
+++ packages/lit/src/index.ts
@@ -4,4 +4,10 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-// OOPS!
+// Although these are re-exported from lit-element.js, we add
+// them here to effectively pre-fetch them and avoid the extra
+// waterfall when loading the lit package unbundled
+import '@lit/reactive-element';
+import 'lit-html';
+
+export * from 'lit-element/lit-element.js';
