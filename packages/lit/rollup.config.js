b0fc163b36cc9d8961930e898769c728aec8e6e7
Russell Bicknell <bicknellr@google.com>
2022-01-20 17:20:23 -0800

    Provide better paths for sources in the bundles' source maps.

--- packages/lit/rollup.config.js
+++ packages/lit/rollup.config.js
@@ -6,6 +6,27 @@
 
 import {litProdConfig} from '../../rollup-common.js';
 import {createRequire} from 'module';
+import * as path from 'path';
+
+/**
+ * Takes a `relativeSourcePath` and `sourcemapPath` - with the same semantics as
+ * those provided to the function given to Rollup's
+ * [`output.sourcemapPathTransform` config option][1] - and produces a relative
+ * path to the source file from the `packages` directory of this repo.
+ *
+ * [1]: https://rollupjs.org/guide/en/#outputsourcemappathtransform
+ */
+const makeRelativeToPackagesDir = (relativeSourcePath, sourcemapPath) => {
+  const absoluteSourcePath = path.resolve(
+    path.join(path.dirname(sourcemapPath), relativeSourcePath)
+  );
+  const absolutePackagesDirPath = path.resolve(path.join(__dirname, '..'));
+  const relativePackagesDirToSourcePath = path.relative(
+    absolutePackagesDirPath,
+    absoluteSourcePath
+  );
+  return relativePackagesDirToSourcePath;
+};
 
 export default litProdConfig({
   packageName: createRequire(import.meta.url)('./package.json').name,
@@ -58,11 +79,30 @@ export default litProdConfig({
       file: 'index',
       output: 'lit.min',
       format: 'es',
+      sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+        // Convert the paths of the sources to appear as descendants of a
+        // directory with the same name as the bundle. (By default, the paths in
+        // the source map are relative paths from the bundle to the included
+        // sources as they were on disk at the time the bundle was generated.
+        // This causes the developer tools to display the original sources at
+        // seemingly unrelated locations.)
+        return path.join(
+          'lit.min.js',
+          makeRelativeToPackagesDir(relativeSourcePath, sourcemapPath)
+        );
+      },
     },
     {
       file: 'index.all',
       output: 'lit.all.min',
       format: 'es',
+      sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+        // See the comment in the core bundle above.
+        return path.join(
+          'lit.all.min.js',
+          makeRelativeToPackagesDir(relativeSourcePath, sourcemapPath)
+        );
+      },
     },
   ],
 });
