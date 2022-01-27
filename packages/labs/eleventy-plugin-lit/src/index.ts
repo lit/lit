d26f1097d9167813b9aaf51652a93e79e692b892
Kevin Schaaf <kschaaf@google.com>
2022-01-26 17:08:23 -0800

    [labs] Initial commit of eleventy plugin (#1755)
    A plugin for Eleventy for pre-rendering pages that include Lit web components. The components can then be (optionally) hydrated on the client for interactivity.
    
    Note, this plugin is primarily focused on rendering standalone widgets which can be configured with attributes in HTML into an 11ty site (there currently no facility for passing 11ty data into components via Javascript).

---
+++ packages/labs/eleventy-plugin-lit/src/index.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2021 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+type LitPluginOptions = {
+  componentModules?: string[];
+};
+
+module.exports = {
+  configFunction: function (
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    eleventyConfig: any,
+    options: LitPluginOptions = {}
+  ) {
+    eleventyConfig.addTransform(
+      'render-lit',
+      async (content: string, outputPath: string) => {
+        if (outputPath.endsWith('.html')) {
+          const render = (
+            await import('@lit-labs/ssr/lib/render-with-global-dom-shim.js')
+          ).render;
+          const html = (await import('lit')).html;
+          const unsafeHTML = (await import('lit/directives/unsafe-html.js'))
+            .unsafeHTML;
+          for (const module of options.componentModules ?? []) {
+            require(path.join(process.cwd(), module));
+          }
+          let head, body, tail;
+          const page = content.match(/(.*)(<body.*<\/body>)(.*)/);
+          if (page) {
+            [head, body, tail] = page;
+          } else {
+            head = `<html><head></head>`;
+            body = `<body>${content}</body>`;
+            tail = `</html>`;
+          }
+          return (
+            head + iterableToString(render(html`${unsafeHTML(body)}`)) + tail
+          );
+        }
+        return content;
+      }
+    );
+  },
+};
+
+// Assuming this is faster than Array.from(iter).join();
+// TODO: perf test
+const iterableToString = (iterable: Iterable<string>) => {
+  let s = '';
+  for (const i of iterable) {
+    s += i;
+  }
+  return s;
+};
