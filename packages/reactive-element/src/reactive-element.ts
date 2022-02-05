6ea3d6c4b85664be96cdb5f5bd62c6e6263aeb28
Justin Fagnani <justinfagnani@google.com>
2022-02-05 11:13:54 -0800

    Better types for PropertyValues<T> (#2482)
    * Better types for PropertyValues<T>
    
    * Address review feedback
    
    * Address feedback

--- packages/reactive-element/src/reactive-element.ts
+++ packages/reactive-element/src/reactive-element.ts
@@ -223,9 +223,18 @@ type AttributeMap = Map<string, PropertyKey>;
  * interface corresponding to the declared element properties.
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type PropertyValues<T = any> = keyof T extends PropertyKey
-  ? Map<keyof T, unknown>
-  : never;
+export interface PropertyValues<T = any> extends Map<keyof T, T[keyof T]> {
+  get<K extends keyof T>(k: K): T[K];
+  set<K extends keyof T>(key: K, value: T[K]): this;
+  forEach(
+    callbackfn: <K extends keyof T>(
+      value: T[K],
+      key: K,
+      map: Map<K, T[keyof T]>
+    ) => void,
+    thisArg?: unknown
+  ): void;
+}
 
 export const defaultConverter: ComplexAttributeConverter = {
   toAttribute(value: unknown, type?: unknown): unknown {
