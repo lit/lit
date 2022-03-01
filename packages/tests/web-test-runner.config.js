c420eb666400116aae92ff91a6a39c53e8fadd6d
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-03-01 11:24:07 -0800

    double times to rule out timeouts

--- packages/tests/web-test-runner.config.js
+++ packages/tests/web-test-runner.config.js
@@ -208,13 +208,13 @@ export default {
   // For ie11 where tests run more slowly, this timeout needs to be long
   // enough so that blocked tests have time to wait for all previous test files
   // to run to completion.
-  testsStartTimeout: 600000, // default 120000
-  testsFinishTimeout: 120000, // default 20000
+  testsStartTimeout: 1200000, // default 120000
+  testsFinishTimeout: 1200000, // default 20000
   testFramework: {
     // https://mochajs.org/api/mocha
     config: {
       ui: 'tdd',
-      timeout: '60000', // default 2000
+      timeout: '1200000', // default 2000
     },
   },
 };
