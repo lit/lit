fde08dff06468b489272307a28ba3af478726989
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-03-14 16:48:07 -0700

    isolate timeouts for windows tests

--- packages/tests/web-test-runner.config.js
+++ packages/tests/web-test-runner.config.js
@@ -202,18 +202,17 @@ export default {
       },
     }),
   ],
-  // Only actually log errors and warnings. This helps make test output less spammy.
-  browserStartTimeout: 1200000, // default 30000
+  browserStartTimeout: 12000, // default 30000
   // For ie11 where tests run more slowly, this timeout needs to be long
   // enough so that blocked tests have time to wait for all previous test files
   // to run to completion.
-  testsStartTimeout: 1200000, // default 120000
-  testsFinishTimeout: 1200000, // default 20000
+  testsStartTimeout: 120000, // default 120000
+  testsFinishTimeout: 1800000, // 30 min vs default 20000
   testFramework: {
     // https://mochajs.org/api/mocha
     config: {
       ui: 'tdd',
-      timeout: '1200000', // default 2000
+      timeout: '12000', // default 2000
     },
   },
 };
