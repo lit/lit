9555b393f9b40148d00eea79e7a9d46d2aed3285
Marco Amato <linediconsine@gmail.com>
2022-03-14 12:54:58 +0100

    Test are located in ./src/test/ and externsion is .ts

--- packages/lit-starter-ts/web-test-runner.config.js
+++ packages/lit-starter-ts/web-test-runner.config.js
@@ -91,7 +91,7 @@ try {
 // https://modern-web.dev/docs/test-runner/cli-and-configuration/
 export default {
   rootDir: '.',
-  files: ['./test/**/*_test.js'],
+  files: ['./src/**/*_test.ts'],
   nodeResolve: {exportConditions: mode === 'dev' ? ['development'] : []},
   preserveSymlinks: true,
   browsers: commandLineBrowsers ?? Object.values(browsers),
