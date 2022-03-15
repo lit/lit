f344706306334a8de42ddabb53aca776bb9b6589
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-03-14 20:55:26 -0700

    extend test finish timeout

--- packages/tests/web-test-runner.config.js
+++ packages/tests/web-test-runner.config.js
@@ -206,8 +206,8 @@ export default {
   // For ie11 where tests run more slowly, this timeout needs to be long
   // enough so that blocked tests have time to wait for all previous test files
   // to run to completion.
-  testsStartTimeout: 600000, // default 120000
-  testsFinishTimeout: 600000, // default 20000
+  testsStartTimeout: 1800000, // default 120000
+  testsFinishTimeout: 1800000, // default 20000
   testFramework: {
     // https://mochajs.org/api/mocha
     config: {
