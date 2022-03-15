b56ab416eb94d41ebf24f2718d11b082e2b55fd6
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-03-15 01:03:00 -0700

    add time to browerStartTimeout, 1 browser in ie

--- packages/tests/web-test-runner.config.js
+++ packages/tests/web-test-runner.config.js
@@ -202,12 +202,12 @@ export default {
       },
     }),
   ],
-  browserStartTimeout: 60000, // default 30000
+  browserStartTimeout: 120000, // default 30000
   // For ie11 where tests run more slowly, this timeout needs to be long
   // enough so that blocked tests have time to wait for all previous test files
   // to run to completion.
   testsStartTimeout: 1800000, // default 120000
-  testsFinishTimeout: 1800000, // default 20000
+  testsFinishTimeout: 120000, // default 20000
   testFramework: {
     // https://mochajs.org/api/mocha
     config: {
