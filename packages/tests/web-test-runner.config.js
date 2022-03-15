bda48cb3488fdd5f1caee34d251fc3ada7a4b174
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-03-14 19:10:18 -0700

    add IE workflow back into fold

--- packages/tests/web-test-runner.config.js
+++ packages/tests/web-test-runner.config.js
@@ -45,10 +45,9 @@ const browserPresets = {
     'sauce:Windows 10/Firefox@78', // Current ESR. See: https://wiki.mozilla.org/Release_Management/Calendar
     'sauce:Windows 10/Chrome@latest-3',
     'sauce:macOS 10.15/Safari@latest',
-    // 'sauce:Windows 7/Internet Explorer@11',
     // 'sauce:Windows 10/MicrosoftEdge@18', // needs globalThis polyfill
   ],
-  // 'sauce-ie11': ['sauce:Windows 7/Internet Explorer@11'],
+  'sauce-ie11': ['sauce:Windows 7/Internet Explorer@11'],
 };
 
 /*
