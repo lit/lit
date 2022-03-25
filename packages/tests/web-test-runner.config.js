9aaa1c5a48dc5ec61fd84b976545ede5239e0092
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-03-25 16:32:37 -0700

    add more time to browser_start and timeouts

--- packages/tests/web-test-runner.config.js
+++ packages/tests/web-test-runner.config.js
@@ -217,17 +217,17 @@ export default {
       },
     }),
   ],
-  browserStartTimeout: 60000, // default 30000
+  browserStartTimeout: 600000, // default 30000
   // For ie11 where tests run more slowly, this timeout needs to be long
   // enough so that blocked tests have time to wait for all previous test files
   // to run to completion.
-  testsStartTimeout: 60000 * 10, // default 120000
-  testsFinishTimeout: 120000, // default 20000
+  testsStartTimeout: 120000 * 10, // default 120000
+  testsFinishTimeout: 180000, // default 20000
   testFramework: {
     // https://mochajs.org/api/mocha
     config: {
       ui: 'tdd',
-      timeout: '60000', // default 2000
+      timeout: '120000', // default 2000
     },
   },
 };
