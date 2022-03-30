38bb0640c78737e9c5ac28db30505e27328adab2
Justin Fagnani <justinfagnani@google.com>
2022-03-30 08:04:49 -0700

    Update packages/labs/analyzer/test-files/basic-elements/src/element-a.ts
    Co-authored-by: Peter Burns <rictic@google.com>

--- packages/labs/analyzer/test-files/basic-elements/src/element-a.ts
+++ packages/labs/analyzer/test-files/basic-elements/src/element-a.ts
@@ -1,6 +1,7 @@
 import {LitElement, html, css} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
+@customElement('element-a')
 export class ElementA extends LitElement {
   static styles = css`
     :host {
