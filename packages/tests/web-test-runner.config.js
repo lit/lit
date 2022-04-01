86eada7333ca0ee6d298db0c117a2a534cad5f0c
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-04-01 10:47:59 -0700

    non-unique tunnel, extended timeouts

--- packages/tests/web-test-runner.config.js
+++ packages/tests/web-test-runner.config.js
@@ -79,8 +79,6 @@ To test on Sauce, set the environment variables:
       },
       {
         tunnelIdentifier,
-        noRemoveCollidingTunnels: true,
-        sharedTunnel: true,
       }
     );
   }
@@ -221,17 +219,17 @@ export default {
   ],
   // Only actually log errors and warnings. This helps make test output less spammy.
   filterBrowserLogs: (type) => type === 'warn' || type === 'error',
-  browserStartTimeout: 60000, // default 30000
+  browserStartTimeout: 120000, // default 30000
   // For ie11 where tests run more slowly, this timeout needs to be long
   // enough so that blocked tests have time to wait for all previous test files
   // to run to completion.
-  testsStartTimeout: 60000 * 10, // default 120000
-  testsFinishTimeout: 120000, // default 20000
+  testsStartTimeout: 600000, // default 120000
+  testsFinishTimeout: 600000, // default 20000
   testFramework: {
     // https://mochajs.org/api/mocha
     config: {
       ui: 'tdd',
-      timeout: '60000', // default 2000
+      timeout: '120000', // default 2000
     },
   },
 };
