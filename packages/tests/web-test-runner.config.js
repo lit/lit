2370ecc44c8aa9acf3d449cad790707c78853088
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-04-01 20:30:42 -0700

    batch tests in groups

--- packages/tests/web-test-runner.config.js
+++ packages/tests/web-test-runner.config.js
@@ -165,17 +165,26 @@ export default {
   // Note this file list can be overridden by wtr command-line arguments.
   // Tests run by lerna should override command-line arguments.
   // Sauce tests are sent in one go to reduce proxy attempts.
-  files: [
-    '../lit-html/development/**/*_test.(js|html)',
-    '../labs/observers/development/**/*_test.(js|html)',
-    '../labs/react/development/**/*_test.(js|html)',
-    '../labs/router/development/**/*_test.js',
-    '../labs/scoped-registry-mixin/development/**/*_test.(js|html)',
-    '../labs/ssr/development/**/*_test.(js|html)',
-    '../labs/task/development/**/*_test.(js|html)',
-    '../lit-element/development/**/*_test.(js|html)',
-    '../lit-html/development/**/*_test.(js|html)',
-    '../reactive-element/development/**/*_test.(js|html)',
+  groups: [
+    {
+      name: 'core-libraries',
+      files: [
+        '../lit-html/development/**/*_test.(js|html)',
+        '../lit-element/development/**/*_test.(js|html)',
+        '../reactive-element/development/**/*_test.(js|html)',
+      ],
+    },
+    {
+      name: 'labs',
+      files: [
+        '../labs/observers/development/**/*_test.(js|html)',
+        '../labs/react/development/**/*_test.(js|html)',
+        '../labs/router/development/**/*_test.js',
+        '../labs/scoped-registry-mixin/development/**/*_test.(js|html)',
+        '../labs/ssr/development/**/*_test.(js|html)',
+        '../labs/task/development/**/*_test.(js|html)',
+      ],
+    },
   ],
   nodeResolve: true,
   concurrency: Number(process.env.CONCURRENT_FRAMES || 6), // default cores / 2
