ff08b85e422f980cbd1d1f68f7ece2dbe4cf895a
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-04-14 15:46:21 -0700

    increase timeouts for ie and trinity browsers

--- packages/tests/web-test-runner.saucelabs.config.js
+++ packages/tests/web-test-runner.saucelabs.config.js
@@ -123,6 +123,6 @@ export default {
   // For ie11 where tests run more slowly, this timeout needs to be long
   // enough so that blocked tests have time to wait for all previous test files
   // to run to completion.
-  testsStartTimeout: 12000 * 10, // default 120000
-  testsFinishTimeout: 180000, // default 20000
+  testsStartTimeout: 180000, // default 120000
+  testsFinishTimeout: 240000, // default 20000
 };
