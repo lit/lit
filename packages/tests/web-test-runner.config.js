8f05ba0b2fb0a0667bde5774c4c6bd06340bb5ab
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-04-19 15:50:04 -0700

    remove ssr tests from tests/

--- packages/tests/web-test-runner.config.js
+++ packages/tests/web-test-runner.config.js
@@ -168,7 +168,6 @@ export default {
     '../labs/react/development/**/*_test.(js|html)',
     '../labs/router/development/**/*_test.js',
     '../labs/scoped-registry-mixin/development/**/*_test.(js|html)',
-    '../labs/ssr/development/**/*_test.(js|html)',
     '../labs/task/development/**/*_test.(js|html)',
     '../lit-element/development/**/*_test.(js|html)',
     '../lit-html/development/**/*_test.(js|html)',
