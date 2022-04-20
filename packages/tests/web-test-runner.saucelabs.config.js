d9501d8342814cd2b978b936365f30860d520e49
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-04-20 01:29:39 -0700

    remove lit-element tests

--- packages/tests/web-test-runner.saucelabs.config.js
+++ packages/tests/web-test-runner.saucelabs.config.js
@@ -38,7 +38,7 @@ const files = [
   '../labs/router/development/**/*_test.js',
   '../labs/scoped-registry-mixin/development/**/*_test.(js|html)',
   '../labs/task/development/**/*_test.(js|html)',
-  '../lit-element/development/**/*_test.(js|html)',
+  // '../lit-element/development/**/*_test.(js|html)',
   '../lit-html/development/**/*_test.(js|html)',
   '../reactive-element/development/**/*_test.(js|html)',
 ];
