4bc76e5a38158515086773e65080b1a3d1494ad2
Kevin Schaaf <kschaaf@google.com>
2022-05-03 01:21:43 -0700

    Fix concat

--- packages/labs/cli/src/lib/generate/utils.ts
+++ packages/labs/cli/src/lib/generate/utils.ts
@@ -41,7 +41,7 @@ const concat = (strings: TemplateStringsArray, ...values: unknown[]) => {
   return strings.slice(1).reduce((prev, next, i) => {
     let v = values[i];
     if (Array.isArray(v)) {
-      v = v.flat();
+      v = v.flat().join('');
     }
     return prev + v + next;
   }, strings[0]);
