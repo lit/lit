f9a1c99f5f70732b6cd74b78a8e1d8a778e284b2
Andrew Jakubowicz <ajakubowicz@google.com>
2022-05-09 11:12:17 -0700

    Add `render` and `TemplateResult` documentation.

--- packages/lit-html/src/lit-html.ts
+++ packages/lit-html/src/lit-html.ts
@@ -443,7 +443,12 @@ const ELEMENT_PART = 6;
 const COMMENT_PART = 7;
 
 /**
- * The return type of the template tag functions.
+ * The return type of the template tag functions, {@linkcode html} and
+ * {@linkcode svg}.
+ *
+ * This object captures the arguments passed into the tag function. A special
+ * array of strings that retain their identity between multiple evaluations of
+ * the tagged literal, and the values from the interpolated expressions.
  */
 export type TemplateResult<T extends ResultType = ResultType> = {
   // This property needs to remain unminified.
@@ -609,9 +614,24 @@ export interface RenderOptions {
 
 /**
  * Renders a value, usually a lit-html TemplateResult, to the container.
- * @param value
- * @param container
- * @param options
+ *
+ * For example, to render "Hello, Zoe!" to the container `document.body`:
+ *
+ * ```js
+ * import {html, render} from 'lit';
+ *
+ * const name = "Zoe";
+ * render(html`<p>Hello, ${name}!</p>`, document.body);
+ * ```
+ *
+ * @param value Any renderable value, typically a {@linkcode TemplateResult}
+ *   created by evaluating a template tag like {@linkcode html} or
+ *   {@linkcode svg}.
+ * @param container A DOM container to render to. The first render will append
+ *   the rendered value to the container, and subsequent renders will
+ *   efficiently update the rendered value if the same result type was
+ *   previously rendered there.
+ * @param options See {@linkcode RenderOptions} for options documentation.
  */
 export const render = (
   value: unknown,
