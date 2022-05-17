15ed003206e38c9d666990ef0f0b7ea75137d137
Kevin Schaaf <kschaaf@google.com>
2022-05-17 11:06:06 -0700

    Add license, file description

--- packages/labs/gen-wrapper-react/test-output/rollup.config.js
+++ packages/labs/gen-wrapper-react/test-output/rollup.config.js
@@ -1,3 +1,15 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+// This rollup config performs the minimum transforms needed to build
+// a package that imports React from esm module source:
+// - Handles imports to commonjs modules, since react is only distributed
+//   as commonjs
+// - Replaces process.env.NODE_ENV
+
 import {nodeResolve} from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
