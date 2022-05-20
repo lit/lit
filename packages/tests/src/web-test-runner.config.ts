b6877be6386da2cc967991ab0c6df387a03d2b5b
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-05-20 09:11:34 -0700

    increase timeouts

--- packages/tests/src/web-test-runner.config.ts
+++ packages/tests/src/web-test-runner.config.ts
@@ -225,12 +225,12 @@ const config: TestRunnerConfig = {
   ],
   // Only actually log errors. This helps make test output less spammy.
   filterBrowserLogs: ({type}) => type === 'error',
-  browserStartTimeout: 60000, // default 30000
+  browserStartTimeout: 120000, // default 30000
   // For ie11 where tests run more slowly, this timeout needs to be long
   // enough so that blocked tests have time to wait for all previous test files
   // to run to completion.
-  testsStartTimeout: 60000 * 10, // default 120000
-  testsFinishTimeout: 120000, // default 20000
+  testsStartTimeout: 120000 * 10, // default 120000
+  testsFinishTimeout: 240000, // default 20000
   testFramework: {
     // https://mochajs.org/api/mocha
     config: {
