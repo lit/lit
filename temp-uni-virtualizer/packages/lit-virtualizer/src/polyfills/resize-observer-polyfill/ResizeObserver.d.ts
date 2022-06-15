fe342dde82f1532245d0ce74d490ce8908761602
Brendan Baldwin <brendan@usergenic.com>
2022-06-15 00:50:22 -0600

    Move lit-virtualizer into packages/labs, remove all other uni-virtualizer files and convert build scripts to use wireit and shared web-test-runner.config.js.

--- temp-uni-virtualizer/packages/lit-virtualizer/src/polyfills/resize-observer-polyfill/ResizeObserver.d.ts
+++
@@ -1,8 +0,0 @@
-export type ResizeObserverConstructor = {
-    prototype: ResizeObserver;
-    new(callback: ResizeObserverCallback): ResizeObserver;
-};
-
-declare let ctor: ResizeObserverConstructor;
-
-export default ctor;
