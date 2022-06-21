b1975b019bacc11978ddb120f9fce6dda5137a0f
Brian Taylor Vann <brian.t.vann@gmail.com>
2022-06-21 15:20:06 -0700

    flatter faster types for UserProps

--- packages/labs/react/src/create-component.ts
+++ packages/labs/react/src/create-component.ts
@@ -141,12 +141,11 @@ export const createComponent = <I extends HTMLElement, E extends Events>(
   // ref, as well as special event and element properties.
   // TODO: we might need to omit more properties from HTMLElement than just
   // 'children', but 'children' is special to JSX, so we must at least do that.
-  type UserProps = React.PropsWithChildren<
-    React.PropsWithRef<
-      Partial<Omit<I, 'children'>> &
-        Partial<EventProps<E>> &
-        Omit<React.HTMLAttributes<HTMLElement>, keyof E>
-    >
+
+  type ReactProps = React.PropsWithChildren<React.HTMLAttributes<I>>;
+  type ElementWithoutChildrenOrEvents = Omit<I, keyof E | keyof ReactProps>;
+  type UserProps = Partial<
+    ElementWithoutChildrenOrEvents & ReactProps & EventProps<E>
   >;
 
   // Props used by this component wrapper. This is the UserProps and the
