a2491c347817fc0c16738630ed8b3980570273d4
Augustine Kim <augustinekim@google.com>
2022-06-28 18:05:11 -0700

    [labs/testing] Add testing package with SSR testing utilities (#2957)

---
+++ packages/labs/testing/src/fixtures.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import 'lit/experimental-hydrate-support.js';
+
+export * from './lib/fixtures/csr-fixture.js';
+export * from './lib/fixtures/ssr-fixture.js';
+export {cleanupFixtures} from './lib/fixtures/fixture-wrapper.js';
