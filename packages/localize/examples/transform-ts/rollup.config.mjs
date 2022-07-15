3241c62491e5f2e9f23f5b4af57805a5a44bb6be
Alexander Marks <aomarks@google.com>
2022-07-15 13:42:34 -0700

    Fix import style for rollup-plugin-summary in localize examples

--- packages/localize/examples/transform-ts/rollup.config.mjs
+++ packages/localize/examples/transform-ts/rollup.config.mjs
@@ -1,7 +1,7 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
-import summary from 'rollup-plugin-summary';
+import {summary} from 'rollup-plugin-summary';
 import {localeTransformers} from '@lit/localize-tools/lib/rollup.js';
 
 const locales = localeTransformers();
