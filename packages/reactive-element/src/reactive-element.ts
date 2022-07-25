bf629713013293e5f1838ed99f011b592306e16a
Alexander Marks <aomarks@google.com>
2022-07-25 15:18:58 -0700

    Update version numbers

--- packages/reactive-element/src/reactive-element.ts
+++ packages/reactive-element/src/reactive-element.ts
@@ -1539,7 +1539,7 @@ if (DEV_MODE) {
 
 // IMPORTANT: do not change the property name or the assignment expression.
 // This line will be used in regexes to search for ReactiveElement usage.
-(global.reactiveElementVersions ??= []).push('1.4.0-next.0');
+(global.reactiveElementVersions ??= []).push('1.4.0-next.1');
 if (DEV_MODE && global.reactiveElementVersions.length > 1) {
   issueWarning!(
     'multiple-versions',
