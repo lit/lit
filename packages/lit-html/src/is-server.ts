6c12775e9bbd02cc515427cf4cd2b65718b5ce20
Augustine Kim <augustinekim@google.com>
2022-09-28 17:03:56 -0700

    Add jsdoc comment

--- packages/lit-html/src/is-server.ts
+++ packages/lit-html/src/is-server.ts
@@ -4,6 +4,17 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+/**
+ * @fileoverview
+ *
+ * This file exports a boolean const whose value will depend on what environment
+ * the module is being imported from.
+ */
+
 const NODE_MODE = false;
 
+/**
+ * Boolean value that will `true` when imported via the "node" export condition
+ * and `false` otherwise.
+ */
 export const isServer = NODE_MODE;
