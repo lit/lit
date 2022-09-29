0df77a3c0e8210bea6d832202a29d9627df136e0
Augustine Kim <augustinekim@google.com>
2022-09-28 17:42:26 -0700

    Add to jsdoc when `isServer` might be used

--- packages/lit-html/src/is-server.ts
+++ packages/lit-html/src/is-server.ts
@@ -14,7 +14,11 @@
 const NODE_MODE = false;
 
 /**
- * A boolean that will be `true` in server environments like Node, and `false` in browser environments. Note that your server environment or toolchain must support the `"node"` export condition for this to be `true`.
- * and `false` otherwise.
+ * A boolean that will be `true` in server environments like Node, and `false`
+ * in browser environments. Note that your server environment or toolchain must
+ * support the `"node"` export condition for this to be `true`.
+ *
+ * This can be used when authoring components to change behavior based on
+ * whether or not the component is executing in an SSR context.
  */
 export const isServer = NODE_MODE;
