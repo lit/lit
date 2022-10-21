cc1a183f2c49d80c7b53addcd3a51852b6ba44b9
Andrew Jakubowicz <ajakubowicz@google.com>
2022-10-21 15:51:35 -0700

    add labs/observers base class to remove duplicated logic
    Focus of this change was to tackle the simple cases and remove all
    the duplicated logic.

--- packages/labs/observers/src/intersection_controller.ts
+++ packages/labs/observers/src/intersection_controller.ts
@@ -7,7 +7,7 @@ import {
   ReactiveController,
   ReactiveControllerHost,
 } from '@lit/reactive-element/reactive-controller.js';
-
+import {BaseController, type BaseControllerConfig} from './base_controller.js';
 /**
  * The callback function for a IntersectionController.
  */
@@ -18,30 +18,17 @@ export type IntersectionValueCallback<T = unknown> = (
 /**
  * The config options for a IntersectionController.
  */
-export interface IntersectionControllerConfig<T = unknown> {
+export interface IntersectionControllerConfig<T = unknown>
+  extends BaseControllerConfig {
   /**
    * Configuration object for the IntersectionObserver.
    */
   config?: IntersectionObserverInit;
   /**
-   * The element to observe. In addition to configuring the target here,
-   * the `observe` method can be called to observe additional targets. When not
-   * specified, the target defaults to the `host`. If set to `null`, no target
-   * is automatically observed. Only the configured target will be re-observed
-   * if the host connects again after unobserving via disconnection.
-   */
-  target?: Element | null;
-  /**
    * The callback used to process detected changes into a value stored
    * in the controller's `value` property.
    */
   callback?: IntersectionValueCallback<T>;
-  /**
-   * An IntersectionObserver reports the initial intersection state
-   * when observe is called. Setting this flag to true skips processing this
-   * initial state for cases when this is unnecessary.
-   */
-  skipInitial?: boolean;
 }
 
 /**
@@ -60,40 +47,16 @@ export interface IntersectionControllerConfig<T = unknown> {
  * used to process the result into a value which is stored on the controller.
  * The controller's `value` is usable during the host's update cycle.
  */
-export class IntersectionController<T = unknown> implements ReactiveController {
-  private _host: ReactiveControllerHost;
-  private _targets: Set<Element> = new Set();
-  private _observer!: IntersectionObserver;
-  private _skipInitial = false;
-  /**
-   * Flag used to help manage calling the `callback` when observe is called
-   * and `skipInitial` is set to true. Note that unlike the other observers
-   * IntersectionObserver *does* report its initial state (e.g. whether or not
-   * there is an intersection). This flag is used to avoid handling this
-   * state if `skipInitial` is true.
-   */
-  private _unobservedUpdate = false;
-  /**
-   * The result of processing the observer's changes via the `callback`
-   * function.
-   */
-  value?: T;
-  /**
-   * Function that returns a value processed from the observer's changes.
-   * The result is stored in the `value` property.
-   */
-  callback?: IntersectionValueCallback<T>;
+export class IntersectionController<T = unknown>
+  extends BaseController<IntersectionObserverInit, IntersectionValueCallback<T>>
+  implements ReactiveController
+{
+  protected override _observer!: IntersectionObserver;
   constructor(
     host: ReactiveControllerHost & Element,
-    {target, config, callback, skipInitial}: IntersectionControllerConfig<T>
+    opts: IntersectionControllerConfig<T>
   ) {
-    this._host = host;
-    // Target defaults to `host` unless explicitly `null`.
-    if (target !== null) {
-      this._targets.add(target ?? host);
-    }
-    this._skipInitial = skipInitial ?? this._skipInitial;
-    this.callback = callback;
+    super(host, opts);
     // Check browser support.
     if (!window.IntersectionObserver) {
       console.warn(
@@ -111,27 +74,9 @@ export class IntersectionController<T = unknown> implements ReactiveController {
         this.handleChanges(entries);
         this._host.requestUpdate();
       },
-      config
+      opts.config
     );
     host.addController(this);
-  }
-
-  /**
-   * Process the observer's changes with the controller's `callback`
-   * function to produce a result stored in the `value` property.
-   */
-  protected handleChanges(entries: IntersectionObserverEntry[]) {
-    this.value = this.callback?.(entries, this._observer);
-  }
-
-  hostConnected() {
-    for (const target of this._targets) {
-      this.observe(target);
-    }
-  }
-
-  hostDisconnected() {
-    this.disconnect();
   }
 
   async hostUpdated() {
@@ -143,32 +88,11 @@ export class IntersectionController<T = unknown> implements ReactiveController {
   }
 
   /**
-   * Observe the target element. The controller's `target` is automatically
-   * observed when the host connects.
-   * @param target Element to observe
-   */
-  observe(target: Element) {
-    this._targets.add(target);
-    // Note, this will always trigger the callback since the initial
-    // intersection state is reported.
-    this._observer.observe(target);
-    this._unobservedUpdate = true;
-  }
-
-  /**
    * Unobserve the target element.
    * @param target Element to unobserve
    */
   unobserve(target: Element) {
     this._targets.delete(target);
     this._observer.unobserve(target);
-  }
-
-  /**
-   * Disconnects the observer. This is done automatically when the host
-   * disconnects.
-   */
-  protected disconnect() {
-    this._observer.disconnect();
   }
 }
--- packages/labs/observers/src/resize_controller.ts
+++ packages/labs/observers/src/resize_controller.ts
@@ -8,6 +8,7 @@ import {
   ReactiveControllerHost,
 } from '@lit/reactive-element/reactive-controller.js';
 
+import {BaseController, type BaseControllerConfig} from './base_controller.js';
 /**
  * The callback function for a ResizeController.
  */
@@ -18,30 +19,17 @@ export type ResizeValueCallback<T = unknown> = (
 /**
  * The config options for a ResizeController.
  */
-export interface ResizeControllerConfig<T = unknown> {
+export interface ResizeControllerConfig<T = unknown>
+  extends BaseControllerConfig {
   /**
    * Configuration object for the ResizeController.
    */
   config?: ResizeObserverOptions;
   /**
-   * The element to observe. In addition to configuring the target here,
-   * the `observe` method can be called to observe additional targets. When not
-   * specified, the target defaults to the `host`. If set to `null`, no target
-   * is automatically observed. Only the configured target will be re-observed
-   * if the host connects again after unobserving via disconnection.
-   */
-  target?: Element | null;
-  /**
    * The callback used to process detected changes into a value stored
    * in the controller's `value` property.
    */
   callback?: ResizeValueCallback<T>;
-  /**
-   * By default the `callback` is called without changes when a target is
-   * observed. This is done to help manage initial state, but this
-   * setup step can be skipped by setting this to true.
-   */
-  skipInitial?: boolean;
 }
 
 /**
@@ -58,41 +46,17 @@ export interface ResizeControllerConfig<T = unknown> {
  * used to process the result into a value which is stored on the controller.
  * The controller's `value` is usable during the host's update cycle.
  */
-export class ResizeController<T = unknown> implements ReactiveController {
-  private _host: ReactiveControllerHost;
-  private _targets: Set<Element> = new Set();
-  private _config?: ResizeObserverOptions;
-  private _observer!: ResizeObserver;
-  private _skipInitial = false;
-  /**
-   * Flag used to help manage calling the `callback` when observe is called
-   * in addition to when a mutation occurs. This is done to help setup initial
-   * state and is performed async by requesting a host update and calling
-   * `handleChanges` once by checking and then resetting this flag.
-   */
-  private _unobservedUpdate = false;
-  /**
-   * The result of processing the observer's changes via the `callback`
-   * function.
-   */
-  value?: T;
-  /**
-   * Function that returns a value processed from the observer's changes.
-   * The result is stored in the `value` property.
-   */
-  callback?: ResizeValueCallback<T>;
+export class ResizeController<T = unknown>
+  extends BaseController<ResizeObserverOptions, ResizeValueCallback<T>>
+  implements ReactiveController
+{
+  protected override _observer!: ResizeObserver;
   constructor(
     host: ReactiveControllerHost & Element,
-    {target, config, callback, skipInitial}: ResizeControllerConfig<T>
+    opts: ResizeControllerConfig<T>
   ) {
-    this._host = host;
-    // Target defaults to `host` unless explicitly `null`.
-    if (target !== null) {
-      this._targets.add(target ?? host);
-    }
-    this._config = config;
-    this._skipInitial = skipInitial ?? this._skipInitial;
-    this.callback = callback;
+    super(host, opts);
+
     // Check browser support.
     if (!window.ResizeObserver) {
       console.warn(
@@ -107,24 +71,6 @@ export class ResizeController<T = unknown> implements ReactiveController {
     host.addController(this);
   }
 
-  /**
-   * Process the observer's changes with the controller's `callback`
-   * function to produce a result stored in the `value` property.
-   */
-  protected handleChanges(entries: ResizeObserverEntry[]) {
-    this.value = this.callback?.(entries, this._observer);
-  }
-
-  hostConnected() {
-    for (const target of this._targets) {
-      this.observe(target);
-    }
-  }
-
-  hostDisconnected() {
-    this.disconnect();
-  }
-
   async hostUpdated() {
     // Handle initial state as a set of 0 changes. This helps setup initial
     // state and promotes UI = f(state) since ideally the callback does not
@@ -136,31 +82,11 @@ export class ResizeController<T = unknown> implements ReactiveController {
   }
 
   /**
-   * Observe the target element. The controller's `target` is automatically
-   * observed when the host connects.
-   * @param target Element to observe
-   */
-  observe(target: Element) {
-    this._targets.add(target);
-    this._observer.observe(target, this._config);
-    this._unobservedUpdate = true;
-    this._host.requestUpdate();
-  }
-
-  /**
    * Unobserve the target element.
    * @param target Element to unobserve
    */
   unobserve(target: Element) {
     this._targets.delete(target);
     this._observer.unobserve(target);
-  }
-
-  /**
-   * Disconnects the observer. This is done automatically when the host
-   * disconnects.
-   */
-  protected disconnect() {
-    this._observer.disconnect();
   }
 }
