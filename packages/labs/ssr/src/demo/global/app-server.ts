45a545486606b0852fcedfa0cc25462aeb7d2b6c
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-10-23 21:46:44 -0400

    spelling: preamble
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/labs/ssr/src/demo/global/app-server.ts
+++ packages/labs/ssr/src/demo/global/app-server.ts
@@ -17,7 +17,7 @@ export function renderAppWithInitialData() {
 
 // This module runs in the app context with the client-side code, but is a
 // server-only module. It doesn't use lit-html so that it can render the HTML
-// shell in unbalanced fragments. By yielding the HTML preamable immediately
+// shell in unbalanced fragments. By yielding the HTML preamble immediately
 // with no lit-html template preparation or rendering needed, we minimize TTFB,
 // And can get the browser to start prefetch as soon as possible.
 export function* renderApp(data: typeof initialData) {
