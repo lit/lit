c989e1ff0484c8631ddf3f6dabe9f1d05900d429
Steven Orvell <sorvell@google.com>
2022-10-25 12:18:51 -0700

    Attempt to fix windows build.

--- packages/labs/gen-wrapper-vue/test-output/vite.config.ts
+++ packages/labs/gen-wrapper-vue/test-output/vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default {
   build: {
+    rollupOptions: {
+      preserveModules: false,
+    },
     lib: {
       entry: './src/tests/test-element-a_test.ts',
       fileName: () => `test-element-a_test.js`,
