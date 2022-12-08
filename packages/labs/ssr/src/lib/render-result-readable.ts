c209664648865baf58760d9800298796b893493f
Justin Fagnani <justinfagnani@google.com>
2022-12-08 15:13:17 -0800

    Simplify RenderResultReadable

--- packages/labs/ssr/src/lib/render-result-readable.ts
+++ packages/labs/ssr/src/lib/render-result-readable.ts
@@ -24,19 +24,13 @@ export class RenderResultReadable extends Readable {
    */
   private _iterators: Array<RenderResultIterator>;
 
-  /**
-   * Used to detect race conditions where we might be awaiting for a Promise,
-   * but _read() is called separately before the Promise is resolved.
-   */
-  private _waiting = false;
-
   constructor(result: RenderResult) {
     super();
     this._result = result;
     this._iterators = [this._result[Symbol.iterator]()];
   }
 
-  override _read(size: number) {
+  override async _read(_size: number) {
     // This implementation reads values from the RenderResult and pushes them
     // into the base class's Readable implementation. It tries to be as
     // efficient as possible, which means:
@@ -62,63 +56,32 @@ export class RenderResultReadable extends Readable {
     // result, because we must be able to return in the middle of the loop
     // and resume on the next call to _read().
 
-    if (this._waiting) {
-      throw new Error(
-        'Race condition in RenderResultReadable: unexpected _read() call'
-      );
-    }
-
     // Get the current iterator
     let iterator = this._iterators.pop();
 
     while (iterator !== undefined) {
       const next = iterator.next();
       if (next.done === true) {
-        if (this._iterators.length === 0) {
-          // Pushing `null` ends the stream
-          this.push(null);
-          return;
-        } else {
-          // Restore the outer iterator
-          iterator = this._iterators.pop()!;
-          continue;
-        }
+        // Restore the outer iterator
+        iterator = this._iterators.pop();
+        continue;
       }
+
       const value = next.value;
 
       if (typeof value === 'string') {
-        const continue_ = this.push(value);
-        if (continue_ === false) {
+        if (this.push(value) === false) {
+          // The consumer doesn't want any more values. Return for now and
+          // we may get a new call to _read()
           return;
         }
       } else {
         // Must be a Promise
-        // Save our current iterator so we can resume on a recursive call to
-        // _read() when the Promise resolves.
         this._iterators.push(iterator);
-        this._waiting = true;
-
-        value.then(
-          (value) => {
-            if (this._waiting !== true) {
-              throw new Error('Expected RenderResultReadable to be waiting');
-            }
-            this._waiting = false;
-            // Stash the new iterator so we can retreive it when we recurse.
-            this._iterators.push(
-              value[Symbol.iterator]() as RenderResultIterator
-            );
-            // Continue reading by recursing
-            this._read(size);
-          },
-          (e) => {
-            this.emit('error', e);
-          }
-        );
-        // Return now so we don't try to handle new values before the Promise
-        // has resovled.
-        return;
+        iterator = (await value)[Symbol.iterator]() as RenderResultIterator;
       }
     }
+    // Pushing `null` ends the stream
+    this.push(null);
   }
 }
