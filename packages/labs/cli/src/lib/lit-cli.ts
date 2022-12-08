12dbe5d608a7ed212753a867d64fdc1b1daee9cf
Kevin Schaaf <kschaaf@google.com>
2022-12-08 06:55:04 -0500

    Print diagnostic errors more gracefully to CLI.

--- packages/labs/cli/src/lib/lit-cli.ts
+++ packages/labs/cli/src/lib/lit-cli.ts
@@ -133,7 +133,13 @@ export class LitCli {
       }
 
       this.console.debug('Running command...');
-      return await command.run(commandOptions, this.console);
+      try {
+        return await command.run(commandOptions, this.console);
+      } catch (e) {
+        this.console.error('Error executing command:');
+        this.console.error((e as Error).message ?? e);
+        return 1;
+      }
     }
   }
 
