lit/lit

commit 78811714eeb00f979e2074a7dd639e8d65903a0f

Update existing XLIFF files (#3514)
`lit-localize` updates existing XLIFF translations files, if available, instead of re-generating these files from scratch.

Remarks about this change:

All `note` elements that result from a `lit-localize` run, will receive a `from="lit-localize"` attribute (see [from](http://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#from). This is necessary to identify the note we are allowed to update in a subsequent run. Other tools might have added further `note` elements. However, this also means, that any existing XLIFF files will be changed once this version of `lit-localize` is run.

We could invest a lot more time into trying to preserve indentation. Other tools might reformat the whole file using tabs or five spaces or... Guessing the most probable level of indentation for new elements, based on surrounding elements, might be doable, but I hope that such an effort is not called for yet.

I adjusted the existing test case, but please feel free to suggest more scenarios we need to cover.

Fixes #1879
Fixes #3439

packages/localize-tools/src/formatters/xliff.ts
@@ -57,20 +57,9 @@ export class XliffFormatter implements Formatter {
   readTranslations(): Bundle[] {
     const bundles: Array<Bundle> = [];
     for (const locale of this.config.targetLocales) {
-      const path = pathLib.join(
-        this.config.resolve(this.xliffConfig.xliffDir),
-        locale + '.xlf'
-      );
-      let xmlStr;
-      try {
-        xmlStr = fsExtra.readFileSync(path, 'utf8');
-      } catch (err) {
-        if ((err as Error & {code: string}).code === 'ENOENT') {
-          // It's ok if the file doesn't exist, it's probably just the first
-          // time we're running for this locale.
-          continue;
-        }
-        throw err;
+      const xmlStr = this.readTranslationFile(locale);
+      if (!xmlStr) {
+        continue;
       }
       bundles.push(this.parseXliff(xmlStr));
     }
@@ -170,10 +159,14 @@ export class XliffFormatter implements Formatter {
     }
     const writes: Array<Promise<void>> = [];
     for (const targetLocale of this.config.targetLocales) {
+      const existingTargetXmlStr = this.readTranslationFile(targetLocale);
       const xmlStr = this.encodeLocale(
         sourceMessages,
         targetLocale,
-        translations.get(targetLocale) || []
+        translations.get(targetLocale) || [],
+        existingTargetXmlStr
+          ? new xmldom.DOMParser().parseFromString(existingTargetXmlStr)
+          : undefined
       );
       const path = pathLib.join(xliffDir, `${targetLocale}.xlf`);
       writes.push(
@@ -195,83 +188,86 @@ export class XliffFormatter implements Formatter {
   private encodeLocale(
     sourceMessages: ProgramMessage[],
     targetLocale: Locale,
-    targetMessages: Message[]
+    targetMessages: Message[],
+    targetDocument?: XMLDocument
   ): string {
     const translationsByName = makeMessageIdMap(targetMessages);
-
-    const doc = new xmldom.DOMImplementation().createDocument('', '', null);
-    const indent = (node: Element | Document, level = 0) =>
-      node.appendChild(doc.createTextNode('\n' + Array(level + 1).join('  ')));
-    doc.appendChild(
-      doc.createProcessingInstruction('xml', 'version="1.0" encoding="UTF-8"')
-    );
-    indent(doc);
-
-    // https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#xliff
-    const xliff = doc.createElement('xliff');
-    xliff.setAttribute('version', '1.2');
-    xliff.setAttribute('xmlns', 'urn:oasis:names:tc:xliff:document:1.2');
-    doc.appendChild(xliff);
-    indent(xliff);
-
-    // https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#file
-    const file = doc.createElement('file');
-    xliff.appendChild(file);
-    file.setAttribute('target-language', targetLocale);
-    file.setAttribute('source-language', this.config.sourceLocale);
-    // TODO The spec requires the source filename in the "original" attribute,
-    // but we don't currently track filenames.
-    file.setAttribute('original', 'lit-localize-inputs');
-    // Plaintext seems right, as opposed to HTML, since our translatable message
-    // text is just text, and all HTML markup is encoded into <x> or <ph>
-    // elements.
-    file.setAttribute('datatype', 'plaintext');
-    indent(file);
-
-    // https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#body
-    const body = doc.createElement('body');
-    file.appendChild(body);
-    indent(body);
-
+    const doc = targetDocument || this.initNewTargetDocument(targetLocale);
+    const indent = this.createIndentFunction(doc);
+    const existingTranslationsByName = this.makeExistingTranslationsMap(doc);
+    const body = getOneElementByTagNameOrThrow(doc, 'body');
+    const names = new Set();
     for (const {name, contents: sourceContents, desc} of sourceMessages) {
       // https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#trans-unit
-      const transUnit = doc.createElement('trans-unit');
-      body.appendChild(transUnit);
-      indent(transUnit, 1);
-      transUnit.setAttribute('id', name);
+      let transUnit = existingTranslationsByName.get(name);
+      if (!transUnit) {
+        transUnit = doc.createElement('trans-unit');
+        transUnit.setAttribute('id', name);
+        this.appendChild(body, transUnit, 0, indent);
+      }
 
       // https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#source
-      const source = doc.createElement('source');
+      let source = this.getElementByTagName(transUnit, 'source');
+      if (!source) {
+        source = doc.createElement('source');
+        this.appendChild(transUnit, source, 1, indent);
+      } else {
+        this.clearElement(source);
+      }
       for (const child of this.encodeContents(doc, sourceContents)) {
         source.appendChild(child);
       }
-      transUnit.appendChild(source);
 
       const translation = translationsByName.get(name);
-      if (translation !== undefined) {
+      if (translation) {
         // https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#target
-        const target = doc.createElement('target');
+        let target = this.getElementByTagName(transUnit, 'target');
+        if (!target) {
+          target = doc.createElement('target');
+          this.appendChild(transUnit, target, 1, indent);
+        } else {
+          this.clearElement(target);
+        }
         for (const child of this.encodeContents(doc, translation.contents)) {
           target.appendChild(child);
         }
-        indent(transUnit, 1);
-        transUnit.appendChild(target);
       }
 
       if (desc) {
         // https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#note
-        const note = doc.createElement('note');
+        let note = this.getNoteElement(transUnit, desc);
+        if (!note) {
+          note = doc.createElement('note');
+          note.setAttribute('from', 'lit-localize');
+          this.appendChild(transUnit, note, 1, indent);
+        } else {
+          // migrate existing notes generated by lit-localize
+          if (!note.hasAttribute('from')) {
+            note.setAttribute('from', 'lit-localize');
+          }
+          this.clearElement(note);
+        }
         note.appendChild(doc.createTextNode(desc));
-        indent(transUnit, 1);
-        transUnit.appendChild(note);
       }
 
-      indent(transUnit);
-      indent(body);
+      names.add(name);
     }
-    indent(file);
-    indent(xliff);
+
+    // clean up obsolete translations
+    existingTranslationsByName.forEach((existingTransUnit, existingName) => {
+      if (!names.has(existingName)) {
+        this.removeChildAndPrecedingText(body, existingTransUnit);
+      }
+    });
+
+    if (!targetDocument) {
+      indent(getOneElementByTagNameOrThrow(doc, 'file'));
+      indent(getOneElementByTagNameOrThrow(doc, 'xliff'));
+    }
+
+    // for existing files this is a guess
     indent(doc);
+
     const serializer = new xmldom.XMLSerializer();
     const xmlStr = serializer.serializeToString(doc);
     return xmlStr;
@@ -318,5 +314,139 @@ export class XliffFormatter implements Formatter {
         `Internal error: unknown xliff placeholderStyle: ${unreachable(style)}`
       );
     }
+  }
+
+  private initNewTargetDocument(targetLocale: Locale): XMLDocument {
+    const doc = new xmldom.DOMImplementation().createDocument('', '', null);
+    const indent = this.createIndentFunction(doc);
+    doc.appendChild(
+      doc.createProcessingInstruction('xml', 'version="1.0" encoding="UTF-8"')
+    );
+    indent(doc);
+
+    // https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#xliff
+    const xliff = doc.createElement('xliff');
+    xliff.setAttribute('version', '1.2');
+    xliff.setAttribute('xmlns', 'urn:oasis:names:tc:xliff:document:1.2');
+    doc.appendChild(xliff);
+    indent(xliff);
+
+    // https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#file
+    const file = doc.createElement('file');
+    xliff.appendChild(file);
+    file.setAttribute('target-language', targetLocale);
+    file.setAttribute('source-language', this.config.sourceLocale);
+    // TODO The spec requires the source filename in the "original" attribute,
+    // but we don't currently track filenames.
+    file.setAttribute('original', 'lit-localize-inputs');
+    // Plaintext seems right, as opposed to HTML, since our translatable message
+    // text is just text, and all HTML markup is encoded into <x> or <ph>
+    // elements.
+    file.setAttribute('datatype', 'plaintext');
+    indent(file);
+
+    // https://docs.oasis-open.org/xliff/v1.2/os/xliff-core.html#body
+    const body = doc.createElement('body');
+    file.appendChild(body);
+
+    return doc;
+  }
+
+  private readTranslationFile(locale: Locale): string | undefined {
+    const path = pathLib.join(
+      this.config.resolve(this.xliffConfig.xliffDir),
+      locale + '.xlf'
+    );
+    try {
+      return fsExtra.readFileSync(path, 'utf8');
+    } catch (err) {
+      if ((err as Error & {code: string}).code === 'ENOENT') {
+        // It's ok if the file doesn't exist, it's probably just the first
+        // time we're running for this locale.
+        return undefined;
+      }
+      throw err;
+    }
+  }
+
+  private makeExistingTranslationsMap(
+    document: XMLDocument
+  ): Map<string, Element> {
+    const map = new Map<string, Element>();
+    const transUnits = document.getElementsByTagName('trans-unit');
+    for (let t = 0; t < transUnits.length; t++) {
+      const transUnit = transUnits[t];
+      map.set(getNonEmptyAttributeOrThrow(transUnit, 'id'), transUnit);
+    }
+    return map;
+  }
+
+  private getElementByTagName(
+    element: Element,
+    tagName: string
+  ): Element | undefined {
+    const matches = element.getElementsByTagName(tagName);
+    return matches.length === 1 ? matches[0] : undefined;
+  }
+
+  private getNoteElement(element: Element, note: string): Element | undefined {
+    const matches = element.getElementsByTagName('note');
+    if (matches.length < 1) {
+      return undefined;
+    }
+    for (let i = 0; i < matches.length; i++) {
+      const el = matches.item(i);
+      if (el?.getAttribute('from') === 'lit-localize') {
+        return el;
+      }
+      // generated by previous version of lit-localize
+      if (el?.textContent?.trim() === note) {
+        return el;
+      }
+    }
+    return undefined;
+  }
+
+  /**
+   * xmldom does not implement replaceChildren.
+   */
+  private clearElement(element: Element) {
+    const children = element.childNodes;
+    for (let i = 0; i < children.length; i++) {
+      element.removeChild(children.item(i));
+    }
+  }
+
+  /**
+   * Append childNode to parentNode, try to preserve some
+   * whitespace / indentation for common scenarios.
+   */
+  private appendChild(
+    parentNode: Node,
+    childNode: Node,
+    level: number,
+    indent: Function
+  ) {
+    const lastChild = parentNode.lastChild;
+    if (lastChild) {
+      parentNode.insertBefore(indent(parentNode, level), lastChild);
+      parentNode.insertBefore(childNode, lastChild);
+    } else {
+      parentNode.appendChild(indent(parentNode, level));
+      parentNode.appendChild(childNode);
+      parentNode.appendChild(indent(parentNode, level - 1));
+    }
+  }
+
+  private removeChildAndPrecedingText(parentNode: Node, childNode: Node) {
+    if (childNode.previousSibling?.nodeType === childNode.TEXT_NODE) {
+      parentNode.removeChild(childNode.previousSibling);
+    }
+    parentNode.removeChild(childNode);
+  }
+
+  private createIndentFunction(doc: Document): Function {
+    return (node: Element | Document, level = 0) =>
+      node.appendChild(doc.createTextNode('\n' + Array(level + 1).join('  ')));
   }
 }

packages/localize-tools/testdata/extract-xliff-merge/goldens/foo.ts
@@ -8,3 +8,5 @@ import {msg} from '@lit/localize';
 
 msg('I am translated', {desc: 'Description of translated'});
 msg('I am not translated', {desc: 'Description of not translated'});
+msg('I am translated with a note', {desc: 'Happy note'});
+msg('My note needs to be migrated', {desc: 'Existing note'});

packages/localize-tools/testdata/extract-xliff-merge/goldens/xliff/es-419.xlf
@@ -1,16 +1,26 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
-<file target-language="es-419" source-language="en" original="lit-localize-inputs" datatype="plaintext">
-<body>
+<xliff xmlns="urn:oasis:names:tc:xliff:document:1.2" xmlns:foo="http://foo.bar" version="1.2">
+  <!-- Comment by translation tool -->
+  <file target-language="es-419" source-language="en" original="other-original" datatype="plaintext" date="2002-01-25T21:06:00Z">
+  <body>
+    <trans-unit id="s677cb50a446c46f4" approved="yes">
+      <source>I am translated</source>
+      <target state="translated">Estoy traducido</target>
+  <note from="lit-localize">Description of translated</note>
+    </trans-unit>
+    <trans-unit id="s18fa8e4d6470399d">
+      <source foo:attribute="bar">I am translated with a note</source>
+      <target state="translated">Estoy traducido con una nota</target>
+      <note>Note by the translator</note>
+      <note from="lit-localize">Happy note</note>
+    </trans-unit>
+    <trans-unit id="s6cfd2eb4786af3dd">
+      <source>My note needs to be migrated</source>
+      <note from="lit-localize">Existing note</note>
+    </trans-unit>
 <trans-unit id="se86f4e822256d113">
   <source>I am not translated</source>
-  <note>Description of not translated</note>
+  <note from="lit-localize">Description of not translated</note>
 </trans-unit>
-<trans-unit id="s677cb50a446c46f4">
-  <source>I am translated</source>
-  <target>Estoy traducido</target>
-  <note>Description of translated</note>
-</trans-unit>
-</body>
-</file>
-</xliff>
+</body><!-- no format changes --></file>
+    </xliff>

packages/localize-tools/testdata/extract-xliff-merge/input/foo.ts
@@ -8,3 +8,5 @@ import {msg} from '@lit/localize';
 
 msg('I am translated', {desc: 'Description of translated'});
 msg('I am not translated', {desc: 'Description of not translated'});
+msg('I am translated with a note', {desc: 'Happy note'});
+msg('My note needs to be migrated', {desc: 'Existing note'});

packages/localize-tools/testdata/extract-xliff-merge/input/xliff/es-419.xlf
@@ -1,11 +1,25 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<xliff xmlns="urn:oasis:names:tc:xliff:document:1.2" version="1.2">
-  <file target-language="es-419" source-language="en" original="lit-localize-inputs" datatype="plaintext">
-    <body>
-      <trans-unit id="s677cb50a446c46f4">
-        <source>I am translated</source>
-        <target state="translated">Estoy traducido</target>
-      </trans-unit>
-    </body>
-  </file>
-</xliff>
+<xliff xmlns="urn:oasis:names:tc:xliff:document:1.2" xmlns:foo="http://foo.bar" version="1.2">
+  <!-- Comment by translation tool -->
+  <file target-language="es-419" source-language="en" original="other-original" datatype="plaintext" date="2002-01-25T21:06:00Z">
+  <body>
+    <trans-unit id="s677cb50a446c46f4" approved="yes">
+      <source>I was translated</source>
+      <target state="translated">Estoy traducido</target>
+    </trans-unit>
+    <trans-unit id="s18fa8e4d6470399d">
+      <source foo:attribute="bar">I am translated with a note</source>
+      <target state="translated">Estoy traducido con una nota</target>
+      <note>Note by the translator</note>
+      <note from="lit-localize">Old generated note</note>
+    </trans-unit>
+    <trans-unit id="foo">
+      <source>I am a stale message</source>
+      <target state="translated">Soy una traducción obsoleta</target>
+    </trans-unit>
+    <trans-unit id="s6cfd2eb4786af3dd">
+      <source>My note needs to be migrated</source>
+      <note>Existing note</note>
+    </trans-unit>
+</body><!-- no format changes --></file>
+    </xliff>

packages/localize-tools/testdata/extract-xliff-fresh-js/goldens/data/xliff/es-419.xlf
@@ -4,7 +4,7 @@
 <body>
 <trans-unit id="he7b474847636149b">
   <source>Hello <ph id="0">&lt;b&gt;${name}</ph>!<ph id="1">&lt;/b&gt;</ph></source>
-  <note>Friendly greeting</note>
+  <note from="lit-localize">Friendly greeting</note>
 </trans-unit>
 </body>
 </file>

packages/localize-tools/testdata/extract-xliff-fresh-ph/goldens/data/xliff/es-419.xlf
@@ -7,11 +7,11 @@
 </trans-unit>
 <trans-unit id="he7b474847636149b">
   <source>Hello <ph id="0">&lt;b&gt;${name}</ph>!<ph id="1">&lt;/b&gt;</ph></source>
-  <note>Description of Hello $(name)</note>
+  <note from="lit-localize">Description of Hello $(name)</note>
 </trans-unit>
 <trans-unit id="s8c0ec8d1fb9e6e32">
   <source>Hello World!</source>
-  <note>Description of Hello World</note>
+  <note from="lit-localize">Description of Hello World</note>
 </trans-unit>
 </body>
 </file>

packages/localize-tools/testdata/extract-xliff-fresh/goldens/data/xliff/es-419.xlf
@@ -7,11 +7,11 @@
 </trans-unit>
 <trans-unit id="he7b474847636149b">
   <source>Hello <x id="0" equiv-text="&lt;b&gt;${name}"/>!<x id="1" equiv-text="&lt;/b&gt;"/></source>
-  <note>Description of Hello $(name)</note>
+  <note from="lit-localize">Description of Hello $(name)</note>
 </trans-unit>
 <trans-unit id="s8c0ec8d1fb9e6e32">
   <source>Hello World!</source>
-  <note>Description of Hello World</note>
+  <note from="lit-localize">Description of Hello World</note>
 </trans-unit>
 </body>
 </file>

commit 6be30739694dd6f09b6cf28c9146db3ee66d1cf1

[localize] Fix XLIFF serialization regression (#3576)
Fixes a bug introduced in https://github.com/lit/lit/pull/3514 where we were not correctly updating XLIFF messages that contained placeholders, because of an index-invalidation bug in the way we were deleting the contents of existing nodes.

Fixes https://github.com/lit/lit/issues/3569

packages/localize-tools/src/formatters/xliff.ts
@@ -412,7 +412,9 @@ export class XliffFormatter implements Formatter {
    */
   private clearElement(element: Element) {
     const children = element.childNodes;
-    for (let i = 0; i < children.length; i++) {
+    // Iterate backwards so we don't have to worry about the index changing each
+    // time we remove.
+    for (let i = children.length - 1; i >= 0; i--) {
       element.removeChild(children.item(i));
     }
   }

packages/localize-tools/testdata/extract-xliff-merge/goldens/foo.ts
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {msg} from '@lit/localize';
+import {msg, str} from '@lit/localize';
 
-msg('I am translated', {desc: 'Description of translated'});
+const who = 'World';
+msg(str`I am translated. Hello ${who}.`, {desc: 'Description of translated'});
 msg('I am not translated', {desc: 'Description of not translated'});
 msg('I am translated with a note', {desc: 'Happy note'});
 msg('My note needs to be migrated', {desc: 'Existing note'});

packages/localize-tools/testdata/extract-xliff-merge/goldens/xliff/es-419.xlf
@@ -3,9 +3,9 @@
   <!-- Comment by translation tool -->
   <file target-language="es-419" source-language="en" original="other-original" datatype="plaintext" date="2002-01-25T21:06:00Z">
   <body>
-    <trans-unit id="s677cb50a446c46f4" approved="yes">
-      <source>I am translated</source>
-      <target state="translated">Estoy traducido</target>
+    <trans-unit id="sa6552c557bbace72" approved="yes">
+      <source>I am translated. Hello <ph id="0">${who}</ph>.</source>
+      <target state="translated">Estoy traducido. Hola <ph id="0">${who}</ph>.</target>
   <note from="lit-localize">Description of translated</note>
     </trans-unit>
     <trans-unit id="s18fa8e4d6470399d">

packages/localize-tools/testdata/extract-xliff-merge/input/foo.ts
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {msg} from '@lit/localize';
+import {msg, str} from '@lit/localize';
 
-msg('I am translated', {desc: 'Description of translated'});
+const who = 'World';
+msg(str`I am translated. Hello ${who}.`, {desc: 'Description of translated'});
 msg('I am not translated', {desc: 'Description of not translated'});
 msg('I am translated with a note', {desc: 'Happy note'});
 msg('My note needs to be migrated', {desc: 'Existing note'});

packages/localize-tools/testdata/extract-xliff-merge/input/xliff/es-419.xlf
@@ -3,9 +3,9 @@
   <!-- Comment by translation tool -->
   <file target-language="es-419" source-language="en" original="other-original" datatype="plaintext" date="2002-01-25T21:06:00Z">
   <body>
-    <trans-unit id="s677cb50a446c46f4" approved="yes">
-      <source>I was translated</source>
-      <target state="translated">Estoy traducido</target>
+    <trans-unit id="sa6552c557bbace72" approved="yes">
+      <source>I was translated. Hello <ph id="0">${who}</ph>.</source>
+      <target state="translated">Estoy traducido. Hola <ph id="0">${who}</ph>.</target>
     </trans-unit>
     <trans-unit id="s18fa8e4d6470399d">
       <source foo:attribute="bar">I am translated with a note</source>