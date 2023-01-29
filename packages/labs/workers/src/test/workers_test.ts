7305e336a49361ffcec31558a79a2ba6566bffe9
Justin Fagnani <justinfagnani@google.com>
2023-01-28 17:00:53 -0800

    Add no-op test

---
+++ packages/labs/workers/src/test/workers_test.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import {assert} from '@esm-bundle/chai';
+
+suite('workers', () => {
+  setup(async () => {});
+
+  test(`test`, async () => {
+    assert.ok(true);
+  });
+});
