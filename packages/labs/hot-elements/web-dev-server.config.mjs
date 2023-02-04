e9e828e4bb2a88e58e065b0c9f14b4553fbded4c
Peter Burns <rictic@google.com>
2023-02-03 21:53:35 -0600

    Prototype of an in-memory file system for WDS

--- packages/labs/hot-elements/web-dev-server.config.mjs
+++ packages/labs/hot-elements/web-dev-server.config.mjs
@@ -1,8 +1,31 @@
 import {esbuildPlugin} from '@web/dev-server-esbuild';
 import {hmrPlugin} from '@web/dev-server-hmr';
+import {getRequestFilePath} from '@web/dev-server-core';
+import * as path from 'path';
 
 export default {
-  plugins: [esbuildPlugin({ts: true}), hmrPlugin()],
+  plugins: [
+    {
+      serverStart({config, fileWatcher}) {
+        this.config = config;
+        console.log(path.resolve('./src/test/demo.ts'));
+        setInterval(() => {
+          fileWatcher.emit('change', path.resolve('./src/test/demo.ts'));
+        }, 1000);
+      },
+      count: 0,
+      transform(context) {
+        const path = getRequestFilePath(context.url, this.config.rootDir);
+        if (!path.endsWith('demo.ts')) {
+          return;
+        }
+        console.log('boop!');
+        // return `document.body.textContent = 'Hello World ${++this.count}'`;
+      },
+    },
+    esbuildPlugin({ts: true}),
+    hmrPlugin(),
+  ],
   nodeResolve: {
     exportConditions: ['development'],
   },
