71944af953f609e8ae2b67806590013730173a15
Augustine Kim <augustinekim@google.com>
2023-02-07 17:59:30 -0800

    Update readme and comments based on code review

--- packages/labs/nextjs/README.md
+++ packages/labs/nextjs/README.md
@@ -26,11 +26,11 @@ module.exports = withLitSSR(nextConfig);
 
 ## Considerations
 
-The plugin has been tested with Next.js versions 12 and 13. It currently does not support usage with the experimental `app` directory introduced with version 13.
+The plugin has been tested with Next.js versions 12 and 13. It currently does not support usage with the beta `app` directory introduced with version 13. Follow this [issue](https://github.com/lit/lit/issues/3657) for updates on supporting this feature.
 
 The plugin may not work properly if you are providing a custom webpack configuration that modifies `config.externals`. Please file an issue if you find a particular configuration is not working.
 
-The server rendered output contains HTML with declarative shadow DOM which has limited browser support. See [`@lit-labs/ssr-react`](../ssr-react/README.md#other-considerations) for more information.
+The server rendered output contains HTML with declarative shadow DOM which may require a polyfill for some browsers. See [Enabling Declarative Shadow DOM from `@lit-labs/ssr-react`](../ssr-react/README.md#enabling-declarative-shadow-dom) for more information.
 
 ## Contributing
 
--- packages/labs/nextjs/src/index.ts
+++ packages/labs/nextjs/src/index.ts
@@ -19,13 +19,16 @@ export = (_pluginOptions: LitSsrPluginOptions = {}) =>
         config.module.rules.unshift({
           // Grab entry points for all pages
           // TODO(augustjk) This may not work for the new "app" directory
-          // introduced in Next.js v13
+          // https://github.com/lit/lit/issues/3657
           test: /\/pages\//,
           // Exclude Next's own distributed files as they're commonjs and won't
           // play nice with `imports-loader`
           exclude: /next\/dist\//,
           loader: 'imports-loader',
           options: {
+            // This adds a side-effectful import which monkey patches
+            // `React.createElement` in the server and imports
+            // `lit/experimental-hydrate-support.js` in the client.
             imports: ['side-effects @lit-labs/ssr-react/enable-lit-ssr.js'],
           },
         });
@@ -34,6 +37,7 @@ export = (_pluginOptions: LitSsrPluginOptions = {}) =>
           // Next.js uses this externals setting to skip webpack bundling
           // external code for server environments but we specifically want to
           // catch react/jsx-runtime imports for external packages as well.
+          // This might change in a future update to Next.js.
           // This is what we're grabbing:
           // https://github.com/vercel/next.js/blob/412dfc52cc5e378e4f74e44af698dad25031a938/packages/next/src/build/webpack-config.ts#L1378-L1429
           const nextHandleExternals = config.externals[0];
@@ -52,19 +56,25 @@ export = (_pluginOptions: LitSsrPluginOptions = {}) =>
         }
 
         config.plugins.push(
+          // Replace all module requests of the automatic JSX runtime sources to
+          // `@lit-lab/ssr-react`'s except the ones within it.
           new webpack.NormalModuleReplacementPlugin(
             /react/,
             function (resource: {request: string; context: string}) {
               if (
                 resource.request === 'react/jsx-runtime' &&
-                // Don't replace our own imports
+                // Don't replace our own imports.
+                // Regex starts at "labs" instead of "lit-labs" to be able to
+                // match the path of the package in the Lit monorepo.
                 !/labs\/ssr-react/.test(resource.context)
               ) {
                 resource.request = '@lit-labs/ssr-react/jsx-runtime';
               }
               if (
                 resource.request === 'react/jsx-dev-runtime' &&
-                // Don't replace our own imports
+                // Don't replace our own imports.
+                // Regex starts at "labs" instead of "lit-labs" to be able to
+                // match the path of the package in the Lit monorepo.
                 !/labs\/ssr-react/.test(resource.context)
               ) {
                 resource.request = '@lit-labs/ssr-react/jsx-dev-runtime';
@@ -73,8 +83,9 @@ export = (_pluginOptions: LitSsrPluginOptions = {}) =>
           )
         );
 
+        // Apply user provided custom webpack config function if it exists.
         // There's potential for conflict here if existing `webpack` config also
-        // overwrites externals
+        // overwrites externals.
         if (typeof nextConfig.webpack === 'function') {
           return nextConfig.webpack(config, options);
         }
