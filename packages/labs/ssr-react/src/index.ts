b731bb5e6d07af2e0ca2de911b781fa3794231cd
Augustine Kim <augustinekim@google.com>
2023-02-07 16:39:14 -0800

    [labs/ssr-react] Add package for integrating Lit SSR to React (#3605)
    Co-authored-by: Kevin Schaaf <kschaaf@google.com>

---
+++ packages/labs/ssr-react/src/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+export {wrapCreateElement} from './lib/wrap-create-element.js';
+export {createElement} from './lib/create-element.js';
---
+++ packages/labs/ssr-react/src/lib/create-element.ts
@@ -0,0 +1,12 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/**
+ * React's `createElement` function enhanced to deeply render Lit components in
+ * the server. It is simply a passthrough of `React.createElement` in the
+ * browser.
+ */
+export {createElement} from 'react';
