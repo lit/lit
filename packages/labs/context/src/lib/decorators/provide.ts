7133afa77d37b8f6f1ee488580ac1d2d88a6e74e
Peter Burns <rictic@google.com>
2023-05-30 19:18:25 -0700

    consume -> provide

--- packages/labs/context/src/lib/decorators/provide.ts
+++ packages/labs/context/src/lib/decorators/provide.ts
@@ -98,7 +98,7 @@ type FieldMustMatchContextType<Obj, Key extends PropertyKey, ContextType> =
     : // Next we check whether the object has the property as an optional field
     Obj extends Partial<Record<Key, infer Providing>>
     ? // Check assignability again. Note that we have to include undefined
-      // here on the consuming type because it's optional.
+      // here on the providing type because it's optional.
       [Providing | undefined] extends [ContextType]
       ? DecoratorReturn
       : {
@@ -106,7 +106,7 @@ type FieldMustMatchContextType<Obj, Key extends PropertyKey, ContextType> =
           context: ContextType;
           consuming: Providing | undefined;
         }
-    : // Ok, the field isn't present, so either someone's using consume
+    : // Ok, the field isn't present, so either someone's using provide
       // manually, i.e. not as a decorator (maybe don't do that! but if you do,
       // you're on your own for your type checking, sorry), or the field is
       // private, in which case we can't check it.
