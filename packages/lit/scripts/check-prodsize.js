66a82131c29a46030a75b19a101dcbf424833bab
Peter Burns <rictic@google.com>
2023-06-09 17:23:46 -0700

    Also add a check prodsize script for the main lit binary too

--- packages/lit/scripts/check-prodsize.js
+++ packages/lit/scripts/check-prodsize.js
@@ -1,8 +1,8 @@
 import * as fs from 'fs';
 
-const expectedMaxSize = 7220;
+const expectedMaxSize = 14750;
 
-const litHtmlSrc = fs.readFileSync('lit-html.js', 'utf8');
+const litHtmlSrc = fs.readFileSync('lit-core.min.js', 'utf8');
 
 if (/end render|template instantiated/.test(litHtmlSrc)) {
   // eslint-disable-next-line no-undef
@@ -10,13 +10,13 @@ if (/end render|template instantiated/.test(litHtmlSrc)) {
   console.log(`debugLogEvent isn't being dead code eliminated`);
 }
 
-const numBytes = fs.readFileSync('lit-html.js').byteLength;
+const numBytes = fs.readFileSync('lit-core.min.js').byteLength;
 
 if (numBytes !== expectedMaxSize) {
   // eslint-disable-next-line no-undef
   process.exitCode = 1;
   console.log(
-    `unexpected size change in prod build of lit-html.js. size was ${numBytes}B but expected it to be ${expectedMaxSize}B
+    `unexpected size change in prod build of lit-core.min.js. size was ${numBytes}B but expected it to be ${expectedMaxSize}B
 
 update packages/lit/scripts/check-prodsize.js if this change should change the size`
   );
