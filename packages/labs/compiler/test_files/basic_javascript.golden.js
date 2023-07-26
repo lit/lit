1a86b10488d64f43d4c7faf10e1146424f83ee72
Andrew Jakubowicz <spyr1014@gmail.com>
2023-07-26 11:26:27 -0700

    add test_files to prettierignore so goldens are not formatted

--- packages/labs/compiler/test_files/basic_javascript.golden.js
+++ packages/labs/compiler/test_files/basic_javascript.golden.js
@@ -1,10 +1,4 @@
-import {html} from 'lit';
-const b_1 = (i) => i;
-const lit_template_1 = {
-  h: b_1`<h1>Hello <?>!</h1>`,
-  parts: [{type: 2, index: 1}],
-};
-export const sayHello = (name) => ({
-  ['_$litType$']: lit_template_1,
-  values: [name],
-});
+import { html } from 'lit';
+const b_1 = i => i;
+const lit_template_1 = { h: b_1 `<h1>Hello <?>!</h1>`, parts: [{ type: 2, index: 1 }] };
+export const sayHello = (name) => ({ ["_$litType$"]: lit_template_1, values: [name] });
--- packages/labs/compiler/test_files/compiled_templates_pulled_out_of_nested_scopes.golden.js
+++ packages/labs/compiler/test_files/compiled_templates_pulled_out_of_nested_scopes.golden.js
@@ -1,12 +1,12 @@
-import {html, nothing} from 'lit-html';
-const b_1 = (i) => i;
-const lit_template_1 = {h: b_1`<p>Hi</p>`, parts: []};
+import { html, nothing } from 'lit-html';
+const b_1 = i => i;
+const lit_template_1 = { h: b_1 `<p>Hi</p>`, parts: [] };
 function outside() {
-  function inner() {
-    if (true) {
-      return {['_$litType$']: lit_template_1, values: []};
+    function inner() {
+        if (true) {
+            return { ["_$litType$"]: lit_template_1, values: [] };
+        }
+        return nothing;
     }
-    return nothing;
-  }
-  return inner();
+    return inner();
 }
