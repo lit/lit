07134954e8b77705251e4e91ec616412cb5d36b3
Justin Fagnani <justinfagnani@google.com>
2023-08-15 21:30:50 -0700

    Add decorator re-export files to lit and lit-element

---
+++ packages/lit/src/std-decorators.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2023 Google LLC
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+export * from '@lit/reactive-element/std-decorators/custom-element.js';
+export * from '@lit/reactive-element/std-decorators/property.js';
+export * from '@lit/reactive-element/std-decorators/state.js';
+export * from '@lit/reactive-element/std-decorators/event-options.js';
+export * from '@lit/reactive-element/std-decorators/query.js';
+export * from '@lit/reactive-element/std-decorators/query-all.js';
+export * from '@lit/reactive-element/std-decorators/query-async.js';
+export * from '@lit/reactive-element/std-decorators/query-assigned-elements.js';
+export * from '@lit/reactive-element/std-decorators/query-assigned-nodes.js';
