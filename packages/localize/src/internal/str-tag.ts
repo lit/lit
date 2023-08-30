d1e126c4a75967ad2cf0c0155b3b2e415e0bc906
Justin Fagnani <justinfagnani@google.com>
2023-08-30 12:46:30 -0700

    [localize] Port localize decorator to hybrid (#4159)

--- packages/localize/src/internal/str-tag.ts
+++ packages/localize/src/internal/str-tag.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import type {TemplateLike} from './types';
+import type {TemplateLike} from './types.js';
 
 export interface StrResult {
   strTag: true;
