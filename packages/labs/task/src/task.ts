b2733c29fafdd23ea1c78e5a1e9dc2a80f94192e
Peter Burns <rictic@google.com>
2023-09-01 16:35:00 -0700

    Address review comments

--- packages/labs/task/src/task.ts
+++ packages/labs/task/src/task.ts
@@ -4,15 +4,59 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+/**
+ * You can import directly from @lit/task now.
+ * @deprecated
+ */
 export {
+  /**
+   * You can import directly from @lit/task now.
+   * @deprecated
+   */
   ArgsFunction,
+  /**
+   * You can import directly from @lit/task now.
+   * @deprecated
+   */
   DepsFunction,
+  /**
+   * You can import directly from @lit/task now.
+   * @deprecated
+   */
   StatusRenderer,
+  /**
+   * You can import directly from @lit/task now.
+   * @deprecated
+   */
   Task,
+  /**
+   * You can import directly from @lit/task now.
+   * @deprecated
+   */
   TaskConfig,
+  /**
+   * You can import directly from @lit/task now.
+   * @deprecated
+   */
   TaskFunction,
+  /**
+   * You can import directly from @lit/task now.
+   * @deprecated
+   */
   TaskFunctionOptions,
+  /**
+   * You can import directly from @lit/task now.
+   * @deprecated
+   */
   TaskStatus,
+  /**
+   * You can import directly from @lit/task now.
+   * @deprecated
+   */
   initialState,
+  /**
+   * You can import directly from @lit/task now.
+   * @deprecated
+   */
   shallowArrayEquals,
 } from '@lit/task/task.js';
