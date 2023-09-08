f1b75c87f7cef8ed6c2db7e6ac44fe3facbb2c04
Keith Henry <keith.henry@evolutionjobs.co.uk>
2023-09-08 11:11:54 +0100

    docs: describe transition prop style: "npm run format" in root

--- packages/labs/motion/src/view-transition-update.ts
+++ packages/labs/motion/src/view-transition-update.ts
@@ -3,7 +3,7 @@
  * Copyright 2023 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-import {LitElement} from 'lit';
+import { LitElement } from 'lit';
 
 type Constructor<T> = new (...args: any[]) => T;
 
@@ -11,38 +11,41 @@ type Constructor<T> = new (...args: any[]) => T;
  *  DOM updates will wait for transition animations to finish before running. */
 export const ViewTransitionUpdate =
     <T extends Constructor<LitElement>>(superClass: T) => {
-    class LitElementTransition extends superClass {
-        private lifecycleRenderTransition: ViewTransition;
-  
-        async performUpdate() {
-            if (!(document as DocumentWithTransition).startViewTransition)
-                return super.performUpdate();
-    
-            if (this.lifecycleRenderTransition) {
-                try {
-                    // Wait for the previous _animation_ to finish before starting the DOM update
-                    await this.lifecycleRenderTransition.finished;
+        class LitElementTransition extends superClass {
+
+            /** Holds the previous transition initiated by this component.
+             *  The next DOM update won't be performed until this finishes (animation complete and interactive) */
+            declare private lifecycleRenderTransition: ViewTransition;
+
+            async performUpdate() {
+                if (!(document as DocumentWithTransition).startViewTransition)
+                    return super.performUpdate();
+
+                if (this.lifecycleRenderTransition) {
+                    try {
+                        // Wait for the previous _animation_ to finish before starting the DOM update
+                        await this.lifecycleRenderTransition.finished;
+                    }
+                    catch (e) {
+                        // The promise can reject, but the DOM update should still happen
+                        // see https://github.com/WICG/view-transitions/blob/main/explainer.md#error-handling
+                        console.error('Error in transition animation', e);
+                    }
                 }
-                catch (e) { 
-                    // The promise can reject, but the DOM update should still happen
-                    // see https://github.com/WICG/view-transitions/blob/main/explainer.md#error-handling
-                    console.error('Error in transition animation', e); 
-                }
+
+                this.lifecycleRenderTransition = (document as DocumentWithTransition).startViewTransition(
+                    () => {
+                        super.performUpdate();
+                        return super.updateComplete; // Return a promise from the callback that resolves once the DOM update is done
+                    });
             }
-          
-            this.lifecycleRenderTransition = (document as DocumentWithTransition).startViewTransition(
-                () => {
-                    super.performUpdate();
-                    return this.updateComplete; // Return a promise from the callback that resolves once the DOM update is done
-                });
         }
-    }
-      
-      return LitElementTransition as Constructor<LitElement> & T;
-  };
+
+        return LitElementTransition as Constructor<LitElement> & T;
+    };
 
 
-interface DocumentWithTransition 
+interface DocumentWithTransition
     extends Document {
     /** The startViewTransition() method of the View Transitions API starts a new view transition and returns a ViewTransition object to represent it.  
      When startViewTransition() is invoked, a sequence of steps is followed as explained in The view transition process. 
@@ -50,7 +53,7 @@ interface DocumentWithTransition
     startViewTransition(callback: () => Promise<unknown>): ViewTransition;
 }
 
-interface ViewTransition { 
+interface ViewTransition {
     /** A Promise that fulfills once the transition animation is finished, and the new page view is visible and interactive to the user. */
     finished: Promise<unknown>;
 
