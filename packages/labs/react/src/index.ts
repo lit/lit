9c45c7bc935d20ab7def899c56bd5798afa568a9
Peter Burns <rictic@google.com>
2023-09-26 14:44:32 -0700

    Update lit-labs/react to redirect to just lit/react

--- packages/labs/react/src/index.ts
+++ packages/labs/react/src/index.ts
@@ -3,4 +3,30 @@
  * Copyright 2017 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
  */
-export * from './create-component.js';
+
+/**
+ * You can import directly from @lit/react now.
+ * @deprecated
+ */
+export {
+  /**
+   * You can import directly from @lit/react now.
+   * @deprecated
+   */
+  createComponent,
+  /**
+   * You can import directly from @lit/react now.
+   * @deprecated
+   */
+  type EventName,
+  /**
+   * You can import directly from @lit/react now.
+   * @deprecated
+   */
+  type ReactWebComponent,
+  /**
+   * You can import directly from @lit/react now.
+   * @deprecated
+   */
+  type WebComponentProps,
+} from '@lit/react';
