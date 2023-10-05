21a62dd73dbf1eb74ce267d838abaf97f7c32466
Justin Fagnani <justin@fagnani.com>
2023-10-05 08:18:17 -0700

    Add template parser to analyzer

--- packages/labs/analyzer/src/lib/lit-html/template.ts
+++ packages/labs/analyzer/src/lib/lit-html/template.ts
@@ -11,8 +11,24 @@
  */
 
 import type ts from 'typescript';
+import {_$LH} from 'lit-html/private-ssr-support.js';
+import {parseFragment} from 'parse5';
+// import type {Attribute} from 'parse5/dist/common/token.js';
+import {
+  Node,
+  CommentNode,
+  Element,
+  DocumentFragment,
+  isCommentNode,
+  isElementNode,
+  traverse,
+} from '@parse5/tools';
+
+const {getTemplateHtml, marker, markerMatch, boundAttributeSuffix} = _$LH;
 
 type TypeScript = typeof ts;
+// TODO (justinfagnani): use the real type from parse5?
+type Attribute = Element['attrs'][number];
 
 /**
  * Returns true if the specifier is know to export the Lit html template tag.
@@ -163,3 +179,201 @@ const isResolvedIdentifierLitHtmlTemplate = (
   }
   return isKnownLitModuleSpecifier(specifier.text);
 };
+
+export const PartType = {
+  ATTRIBUTE: 1,
+  CHILD: 2,
+  PROPERTY: 3,
+  BOOLEAN_ATTRIBUTE: 4,
+  EVENT: 5,
+  ELEMENT: 6,
+} as const;
+
+export type PartType = (typeof PartType)[keyof typeof PartType];
+
+export interface PartInfo {
+  type: PartType;
+  valueIndex: number;
+}
+
+export interface SinglePartInfo extends PartInfo {
+  type: typeof PartType.CHILD | typeof PartType.ELEMENT;
+  expression: ts.Expression;
+}
+
+export interface AttributePartInfo extends PartInfo {
+  type:
+    | typeof PartType.ATTRIBUTE
+    | typeof PartType.BOOLEAN_ATTRIBUTE
+    | typeof PartType.PROPERTY
+    | typeof PartType.EVENT;
+  prefix: string | undefined;
+  name: string | undefined;
+  strings: string[];
+  expressions: Array<ts.Expression>;
+}
+
+export const hasChildPart = (
+  node: CommentNode
+): node is LitTemplateCommentNode => {
+  return (node as LitTemplateCommentNode).litPart?.type === PartType.CHILD;
+};
+
+export const hasAttributePart = (
+  node: Attribute
+): node is LitTemplateAttribute => {
+  return (node as LitTemplateAttribute).litPart !== undefined;
+};
+
+export type LitTemplateNode = Node & {
+  litNodeIndex: number;
+};
+
+export interface LitTemplate extends DocumentFragment {
+  strings: TemplateStringsArray;
+  parts: Array<PartInfo>;
+}
+
+export interface LitTemplateCommentNode extends CommentNode {
+  litPart?: SinglePartInfo;
+  litNodeIndex: number;
+}
+
+export interface LitTemplateElement extends Element {
+  litNodeIndex: number;
+}
+
+export interface LitTemplateAttribute extends Attribute {
+  litPart: PartInfo;
+}
+
+export const parseLitTemplate = (
+  node: ts.TaggedTemplateExpression,
+  ts: TypeScript,
+  _checker: ts.TypeChecker
+): LitTemplate => {
+  const strings = getTemplateStrings(node, ts);
+  const values = ts.isNoSubstitutionTemplateLiteral(node.template)
+    ? []
+    : node.template.templateSpans.map((s) => s.expression);
+  const parts: Array<PartInfo> = [];
+  const [html, boundAttributeNames] = getTemplateHtml(strings, 1);
+
+  let valueIndex = 0;
+
+  // Index of the next bound attribute in attrNames
+  let boundAttributeIndex = 0;
+
+  // Depth-first node index
+  let nodeIndex = 0;
+
+  const ast = parseFragment(html.toString(), {
+    sourceCodeLocationInfo: true,
+  });
+
+  traverse(ast, {
+    ['pre:node'](node, _parent) {
+      if (isCommentNode(node)) {
+        if (node.data === markerMatch) {
+          // An child binding, like <div>${}</div>
+          const expression = values[valueIndex++];
+          parts.push(
+            ((node as LitTemplateCommentNode).litPart = {
+              type: PartType.CHILD,
+              valueIndex,
+              expression,
+            } as SinglePartInfo)
+          );
+        }
+        (node as LitTemplateCommentNode).litNodeIndex = nodeIndex++;
+        // TODO (justinfagnani): handle <!--${}--> (comment binding)
+      } else if (isElementNode(node)) {
+        if (node.attrs.length > 0) {
+          for (const attr of node.attrs) {
+            if (attr.name.startsWith(marker)) {
+              // An element binding, like <div ${}>
+              const expression = values[valueIndex++];
+              parts.push(
+                ((attr as LitTemplateAttribute).litPart = {
+                  type: PartType.ELEMENT,
+                  valueIndex,
+                  expression,
+                } as SinglePartInfo)
+              );
+              boundAttributeIndex++;
+              // TODO (justinfagnani): handle <div ${}="...">
+            } else if (attr.name.endsWith(boundAttributeSuffix)) {
+              // An attribute binding, like <div foo=${}>
+              const [, prefix, caseSensitiveName] = /([.?@])?(.*)/.exec(
+                boundAttributeNames[boundAttributeIndex++]!
+              )!;
+              const strings = attr.value.split(marker);
+              const expressions = values.slice(
+                valueIndex,
+                valueIndex + strings.length - 1
+              );
+              parts.push(
+                ((attr as LitTemplateAttribute).litPart = {
+                  prefix,
+                  name: caseSensitiveName,
+                  type:
+                    prefix === '.'
+                      ? PartType.PROPERTY
+                      : prefix === '?'
+                      ? PartType.BOOLEAN_ATTRIBUTE
+                      : prefix === '@'
+                      ? PartType.EVENT
+                      : PartType.ATTRIBUTE,
+                  strings,
+                  valueIndex,
+                  expressions,
+                } as AttributePartInfo)
+              );
+              valueIndex += strings.length - 1;
+            }
+          }
+        }
+        (node as LitTemplateElement).litNodeIndex = nodeIndex++;
+        // TODO (justinfagnani): handle <${}>
+      }
+    },
+  });
+  (ast as LitTemplate).parts = parts;
+  (ast as LitTemplate).strings = strings;
+  return ast as LitTemplate;
+};
+
+// TODO (justinfagnani): export a traverse function that takes a visitor that
+// gets passed our extended Lit interfaces. Also possibly export a unified
+// traverse that can traverse TypeScript and parse5 nodes. This would allow us
+// to do analysis of nexted templates, for rules like "A <li> element must be a
+// child of a <ul> or <ol> element", even if the <li> is in a nested template.
+
+export const getTemplateStrings = (
+  node: ts.TaggedTemplateExpression,
+  ts: TypeScript
+) => {
+  let strings: TemplateStringsArray;
+  if (ts.isNoSubstitutionTemplateLiteral(node.template)) {
+    strings = [node.template.text] as unknown as TemplateStringsArray;
+  } else {
+    strings = [
+      node.template.head.text,
+      ...node.template.templateSpans.map((s) => s.literal.text),
+    ] as unknown as TemplateStringsArray;
+  }
+  (strings as Mutable<TemplateStringsArray, 'raw'>).raw = strings;
+  return strings;
+};
+
+/**
+ * Removes the `readonly` modifier from properties in the union K.
+ */
+type Mutable<T, K extends keyof T> = Omit<T, K> & {
+  -readonly [P in keyof Pick<T, K>]: P extends K ? T[P] : never;
+};
+
+export interface LitTaggedTemplateExpression
+  extends ts.TaggedTemplateExpression {
+  litTemplate: LitTemplate;
+}
--- packages/labs/analyzer/src/test/server/lit-html/template_test.ts
+++ packages/labs/analyzer/src/test/server/lit-html/template_test.ts
@@ -15,7 +15,12 @@ import {
   setupAnalyzerForTest,
 } from '../utils.js';
 import {ClassDeclaration} from '../../../lib/model.js';
-import {isLitTaggedTemplateExpression} from '../../../lib/lit-html/template.js';
+import {
+  LitTemplateCommentNode,
+  isLitTaggedTemplateExpression,
+  parseLitTemplate,
+} from '../../../lib/lit-html/template.js';
+import {Element} from '@parse5/tools';
 
 for (const lang of languages) {
   const test = suite<AnalyzerTestContext>(`lit-html tests (${lang})`);
@@ -26,7 +31,6 @@ for (const lang of languages) {
 
   test('isLitHtmlTemplateTag', ({getModule, analyzer, typescript}) => {
     const elementAModule = getModule('element-a')!;
-    console.log('elementAModule.jsPath', elementAModule.sourcePath);
     const decl = elementAModule.declarations[0];
 
     // get to the lit-html template tag
@@ -51,8 +55,42 @@ for (const lang of languages) {
       ),
       true
     );
+  });
 
-    // console.log('template', returnStatement.expression);
+  test('parseLitTemplate', ({getModule, analyzer, typescript}) => {
+    const elementAModule = getModule('element-a')!;
+    const decl = elementAModule.declarations[0];
+    const renderMethod = (decl as ClassDeclaration).getMethod('render')!;
+    const statement = renderMethod.node.body!
+      .statements[0] as ts.ReturnStatement;
+    const expression = statement.expression as ts.TaggedTemplateExpression;
+    assert.is(
+      isLitTaggedTemplateExpression(
+        expression,
+        analyzer.typescript,
+        analyzer.program.getTypeChecker()
+      ),
+      true
+    );
+    const litTemplate = parseLitTemplate(
+      expression,
+      typescript,
+      analyzer.program.getTypeChecker()
+    );
+    assert.ok(litTemplate);
+    assert.is(litTemplate.parts.length, 1);
+    assert.is(litTemplate.strings.length, 2);
+
+    const h1 = litTemplate.childNodes[0] as Element;
+    assert.is(h1.nodeName, 'h1');
+    const binding = h1.childNodes[0];
+    assert.is(binding.nodeName, '#comment');
+    const part = (binding as LitTemplateCommentNode).litPart;
+    assert.ok(part);
+    const bindingExpression = (binding as LitTemplateCommentNode).litPart!
+      .expression;
+    assert.is(bindingExpression.getText(), 'this.a');
+    assert.is(litTemplate.parts[0], part);
   });
 
   test.run();
