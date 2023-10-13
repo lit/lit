86bef944065c1e1198d2aa638b195eb0191c3df9
Augustine Kim <augustinekim@google.com>
2023-10-12 17:58:01 -0700

    Better place to add template-shadowroot ponyfill

--- examples/nextjs/pages/_app.tsx
+++ examples/nextjs/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import {isServer} from 'lit';
+// Might be possible to dynamically import this inside the condition below but
+// will need top-level await
+import {hydrateShadowRoots} from '@webcomponents/template-shadowroot';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+if (!isServer) {
+  if (!HTMLTemplateElement.prototype.hasOwnProperty('shadowRootMote')) {
+    hydrateShadowRoots(document.body);
+  }
+}
+
+export default function App({Component, pageProps}: AppProps) {
+  return <Component {...pageProps} />;
 }
--- examples/nextjs/pages/_document.tsx
+++ examples/nextjs/pages/_document.tsx
@@ -1,5 +1,4 @@
 import {Html, Head, Main, NextScript} from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,16 +6,6 @@ export default function Document() {
       <Head />
       <body>
         <Main />
-        <Script
-          id="template-shadowroot"
-          type="module"
-          strategy="beforeInteractive"
-        >{`
-          if (!HTMLTemplateElement.prototype.hasOwnProperty('shadowRootMode')) {
-            const {hydrateShadowRoots} = await import('https://unpkg.com/@webcomponents/template-shadowroot@0.2.1/template-shadowroot.js');
-            hydrateShadowRoots(document.body);
-          }
-        `}</Script>
         <NextScript />
       </body>
     </Html>
