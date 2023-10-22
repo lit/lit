bf20ecfd34984216bfa9bdf89432f711987a732f
43081j <43081j@users.noreply.github.com>
2023-10-22 10:53:53 +0100

    test: update react goldens

--- packages/labs/gen-wrapper-react/goldens/test-element-a/src/element-mixins.ts
+++ packages/labs/gen-wrapper-react/goldens/test-element-a/src/element-mixins.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {createComponent} from '@lit-labs/react';
+import {createComponent} from '@lit/react';
 
 import {ElementMixins as ElementMixinsElement} from '@lit-internal/test-element-a/element-mixins.js';
 
