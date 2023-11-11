a9a756c39668061c414bfadd21486dd51c178a33
Andrew Jakubowicz <spyr1014@gmail.com>
2023-11-10 17:08:35 -0800

    delete files that were accidentally committed

--- packages/labs/ssr-client/controllers/server-controller.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright 2023 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */
-export interface ServerController {
-    serverUpdateComplete: Promise<unknown>;
-}
-//# sourceMappingURL=server-controller.d.ts.map
