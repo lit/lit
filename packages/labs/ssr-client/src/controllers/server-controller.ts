20de34e9403909ad474db9b21af847069bea8a53
Andrew Jakubowicz <spyr1014@gmail.com>
2023-11-13 16:29:09 -0800

    document ServerController

--- packages/labs/ssr-client/src/controllers/server-controller.ts
+++ packages/labs/ssr-client/src/controllers/server-controller.ts
@@ -4,6 +4,17 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-export interface ServerController {
+import type {ReactiveController} from 'lit';
+
+/**
+ * ServerController extends the Reactive Controller interface to allow
+ * asynchronous work to occur during SSR on the server.
+ */
+export interface ServerController extends ReactiveController {
+  /**
+   * `serverUpdateComplete` is accessed on the server during SSR. When
+   * implemented, SSR rendering will pause until all the controllers on this
+   * ReactiveController's host have settled.
+   */
   serverUpdateComplete: Promise<unknown>;
 }
