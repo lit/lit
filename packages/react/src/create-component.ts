638145a4df67680c3a0b5984756aff4fbc1fc178
ky09iH <coen.de.waal@ing.com>
2023-12-06 16:23:35 +0100

    fix: Set lit component properties before rendering to the DOM when using @lit/react createComponent

--- packages/react/src/create-component.ts
+++ packages/react/src/create-component.ts
@@ -314,6 +314,7 @@ export const createComponent = <
     }
 
     return React.createElement(tagName, {
+      ...elementProps,
       ...reactProps,
       ref: React.useCallback(
         (node: I) => {
