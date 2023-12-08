4d229f29ee61efb7ab51a31699edab3208782072
43081j <43081j@users.noreply.github.com>
2023-12-08 23:05:20 +0000

    fix: remove props entirely when none exist

--- packages/labs/gen-wrapper-vue/goldens/test-element-a/src/ElementWithoutProps.vue
+++ packages/labs/gen-wrapper-vue/goldens/test-element-a/src/ElementWithoutProps.vue
@@ -1,31 +1,14 @@
 <script setup lang="ts">
-import {h, useSlots, reactive} from 'vue';
+import {h, useSlots} from 'vue';
 import {assignSlotNodes, Slots} from '@lit-labs/vue-utils/wrapper-utils.js';
 import '@lit-internal/test-element-a/element-without-props.js';
 
 export interface Props {}
 
-const vueProps = defineProps<Props>();
-
-const defaults = reactive({} as Props);
-const vDefaults = {};
-
-let hasRendered = false;
-
 const slots = useSlots();
 
 const render = () => {
-  const eventProps = {};
-
-  const props = eventProps as typeof eventProps & Props;
-  for (const p in vueProps) {
-    const v = vueProps[p as keyof Props];
-    if (v !== undefined || hasRendered) {
-      (props[p as keyof Props] as unknown) = v ?? defaults[p as keyof Props];
-    }
-  }
-
-  hasRendered = true;
+  const props = {} as Props;
 
   return h('element-without-props', props, assignSlotNodes(slots as Slots));
 };
