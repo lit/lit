b4f591ee99fb242d03402a3ad9cbaa1eb6b89481
Justin Fagnani <justinfagnani@google.com>
2024-01-23 16:01:28 -0800

    Remove the fake eslint rules

--- packages/labs/eslint-plugin/src/index.ts
+++ packages/labs/eslint-plugin/src/index.ts
@@ -4,19 +4,14 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import type {Rule, ESLint} from 'eslint';
-import {noKevin} from './rules/no-kevin.js';
+import type {ESLint} from 'eslint';
 
-export const rules = {
-  'no-kevin': noKevin as unknown as Rule.RuleModule,
-};
+export const rules = {};
 
 export const configs: ESLint.Plugin['configs'] = {
   all: {
     plugins: ['@lit-labs/eslint-plugin'],
-    rules: {
-      '@lit-labs/no-kevin': 'error',
-    },
+    rules: {},
   },
   recommended: {
     plugins: ['@lit-labs/eslint-plugin'],
