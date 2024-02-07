59d5ba7649f2957d01996e3115d0d79bdfbf34ac
Justin Fagnani <justinfagnani@google.com>
2024-02-07 15:03:56 -0800

    Convert analyzer template test to node:test (#4537)

--- packages/labs/analyzer/src/test/server/lit-html/template_test.ts
+++ packages/labs/analyzer/src/test/server/lit-html/template_test.ts
@@ -4,53 +4,47 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {suite} from 'uvu';
-// eslint-disable-next-line import/extensions
-import * as assert from 'uvu/assert';
+import {test, describe as suite} from 'node:test';
+import * as assert from 'node:assert';
 import type ts from 'typescript';
 
-import {
-  AnalyzerTestContext,
-  languages,
-  setupAnalyzerForTest,
-} from '../utils.js';
+import {languages, setupAnalyzerForNodeTest} from '../utils.js';
 import {ClassDeclaration} from '../../../lib/model.js';
 import {isLitTaggedTemplateExpression} from '../../../lib/lit-html/template.js';
 
 for (const lang of languages) {
-  const test = suite<AnalyzerTestContext>(`lit-html tests (${lang})`);
-
-  test.before((ctx) => {
-    setupAnalyzerForTest(ctx, lang, 'basic-elements');
-  });
-
-  test('isLitHtmlTemplateTag', ({getModule, analyzer, typescript}) => {
-    const elementAModule = getModule('element-a')!;
-    const decl = elementAModule.declarations[0];
-
-    // get to the lit-html template tag
-    const renderMethod = (decl as ClassDeclaration).getMethod('render')!;
-    const statement = renderMethod.node.body!.statements[0];
-
-    assert.is(typescript.isReturnStatement(statement), true);
-    const returnStatement = statement as ts.ReturnStatement;
-    assert.ok(returnStatement.expression);
-    assert.is(
-      typescript.isTaggedTemplateExpression(returnStatement.expression),
-      true
+  suite(`lit-html template utility tests (${lang})`, () => {
+    const {getModule, analyzer, typescript} = setupAnalyzerForNodeTest(
+      lang,
+      'basic-elements'
     );
-    const expression =
-      returnStatement.expression as ts.TaggedTemplateExpression;
-    assert.is(typescript.isIdentifier(expression.tag), true);
-    assert.is(
-      isLitTaggedTemplateExpression(
-        expression,
-        analyzer.typescript,
-        analyzer.program.getTypeChecker()
-      ),
-      true
-    );
-  });
 
-  test.run();
+    test('isLitHtmlTemplateTag', () => {
+      const elementAModule = getModule('element-a')!;
+      const decl = elementAModule.declarations[0];
+
+      // get to the lit-html template tag
+      const renderMethod = (decl as ClassDeclaration).getMethod('render')!;
+      const statement = renderMethod.node.body!.statements[0];
+
+      assert.equal(typescript.isReturnStatement(statement), true);
+      const returnStatement = statement as ts.ReturnStatement;
+      assert.ok(returnStatement.expression);
+      assert.equal(
+        typescript.isTaggedTemplateExpression(returnStatement.expression),
+        true
+      );
+      const expression =
+        returnStatement.expression as ts.TaggedTemplateExpression;
+      assert.equal(typescript.isIdentifier(expression.tag), true);
+      assert.equal(
+        isLitTaggedTemplateExpression(
+          expression,
+          analyzer.typescript,
+          analyzer.program.getTypeChecker()
+        ),
+        true
+      );
+    });
+  });
 }
