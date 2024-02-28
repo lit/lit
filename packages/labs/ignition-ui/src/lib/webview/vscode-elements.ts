0c82b01fa6554547c8317269cf61fafe5aa8d2a7
Peter Burns <rictic@google.com>
2024-02-27 16:15:05 -0800

    Give <vscode-button> a type.

--- packages/labs/ignition-ui/src/lib/webview/vscode-elements.ts
+++ packages/labs/ignition-ui/src/lib/webview/vscode-elements.ts
@@ -7,7 +7,16 @@
 // Since vscode elements aren't self-registering, we need to register the ones
 // that we use like this:
 
-import {vsCodeButton} from '@vscode/webview-ui-toolkit/dist/button/index.js';
+import {
+  vsCodeButton,
+  type Button,
+} from '@vscode/webview-ui-toolkit/dist/button/index.js';
 import {provideVSCodeDesignSystem} from '@vscode/webview-ui-toolkit/dist/vscode-design-system.js';
 
 provideVSCodeDesignSystem().register(vsCodeButton());
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'vscode-button': Button;
+  }
+}
