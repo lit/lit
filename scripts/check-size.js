61496f115ba0ad0d42839dde4a1e906d48345c5c
Justin Fagnani <justinfagnani@google.com>
2024-03-26 10:56:19 -0700

    Add changeset file and update library sizes

--- scripts/check-size.js
+++ scripts/check-size.js
@@ -9,8 +9,8 @@ import * as fs from 'fs';
 // it's likely that we'll ask you to investigate ways to reduce the size.
 //
 // In either case, update the size here and push a new commit to your PR.
-const expectedLitCoreSize = 15438;
-const expectedLitHtmlSize = 7253;
+const expectedLitCoreSize = 15443;
+const expectedLitHtmlSize = 7258;
 
 const litCoreSrc = fs.readFileSync('packages/lit/lit-core.min.js', 'utf8');
 const litCoreSize = fs.readFileSync('packages/lit/lit-core.min.js').byteLength;
