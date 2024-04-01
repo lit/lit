6041a4baa9f75a0d0bd0fa1a5292034b0abc606f
Peter Siska <peschehimself@gmail.com>
2024-04-01 18:07:04 +0200

    Fix 404 link
    The original link seems to point to a 404 page. I've changed the link to point to the explainer markdown in the orginal repo.

--- packages/reactive-element/src/css-tag.ts
+++ packages/reactive-element/src/css-tag.ts
@@ -161,7 +161,7 @@ export const css = (
 /**
  * Applies the given styles to a `shadowRoot`. When Shadow DOM is
  * available but `adoptedStyleSheets` is not, styles are appended to the
- * `shadowRoot` to [mimic spec behavior](https://wicg.github.io/construct-stylesheets/#using-constructed-stylesheets).
+ * `shadowRoot` to [mimic spec behavior](https://github.com/WICG/construct-stylesheets/blob/main/explainer.md#example-usage).
  * Note, when shimming is used, any styles that are subsequently placed into
  * the shadowRoot should be placed *before* any shimmed adopted styles. This
  * will match spec behavior that gives adopted sheets precedence over styles in
