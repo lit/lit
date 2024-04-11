3f54f265ed77e06cb6ce32a4e746c66ea6a2d4e6
Augustine Kim <augustinekim@google.com>
2024-04-10 18:26:56 -0700

    Remove .cmd workaround for windows in cli tests

--- packages/labs/cli/src/lib/lit-cli.ts
+++ packages/labs/cli/src/lib/lit-cli.ts
@@ -288,12 +288,12 @@ export class LitCli {
     const installFrom = reference.installFrom ?? reference.importSpecifier;
     this.console.log(`Installing ${installFrom}...`);
     const child = childProcess.spawn(
-      // https://stackoverflow.com/questions/43230346/error-spawn-npm-enoent
-      /^win/.test(process.platform) ? 'npm.cmd' : 'npm',
+      'npm',
       ['install', '--save-dev', installFrom],
       {
         cwd: this.cwd,
         stdio: [process.stdin, 'pipe', 'pipe'],
+        shell: true,
       }
     );
     (async () => {
