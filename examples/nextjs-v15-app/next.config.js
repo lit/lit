df5a16da41882a77a51e527cf36f0f42ba47aa13
James Ives <iam@jamesiv.es>
2024-11-20 09:36:52 -0500

    build: remove swcMinify from nextjs-v15-app
    Co-authored-by: Augustine Kim <augustjkim@gmail.com>

--- examples/nextjs-v15-app/next.config.js
+++ examples/nextjs-v15-app/next.config.js
@@ -5,7 +5,6 @@ const withLitSSR = require('@lit-labs/nextjs')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
 };
 
 module.exports = withLitSSR(nextConfig);
