lit/lit

commit 4824c4ce094fabf9d96c92f0d986827b046b2705

Adds `useDefault` property option (#4934)
* Adds skipping initial attribute reflection for default values.

packages/reactive-element/src/reactive-element.ts
@@ -274,6 +274,25 @@ export interface PropertyDeclaration<Type = unknown, TypeHint = unknown> {
    * @internal
    */
   wrapped?: boolean;
+
+  /**
+   * When `true`, uses the initial value of the property as the default value,
+   * which changes how attributes are handled:
+   *  - The initial value does *not* reflect, even if the `reflect` option is `true`.
+   *    Subsequent changes to the property will reflect, even if they are equal to the
+   *     default value.
+   *  - When the attribute is removed, the property is set to the default value
+   *  - The initial value will not trigger an old value in the `changedProperties` map
+   *    argument to update lifecycle methods.
+   *
+   * When set, properties must be initialized, either with a field initializer, or an
+   * assignment in the constructor. Not initializing the property may lead to
+   * improper handling of subsequent property assignments.
+   *
+   * While this behavior is opt-in, most properties that reflect to attributes should
+   * use `useDefault: true` so that their initial values do not reflect.
+   */
+  useDefault?: boolean;
 }
 
 /**
@@ -381,6 +400,7 @@ const defaultPropertyDeclaration: PropertyDeclaration = {
   type: String,
   converter: defaultConverter,
   reflect: false,
+  useDefault: false,
   hasChanged: notEqual,
 };
 
@@ -676,6 +696,12 @@ export abstract class ReactiveElement
       (options as Mutable<PropertyDeclaration, 'attribute'>).attribute = false;
     }
     this.__prepare();
+    // Whether this property is wrapping accessors.
+    // Helps control the initial value change and reflection logic.
+    if (this.prototype.hasOwnProperty(name)) {
+      options = Object.create(options);
+      options.wrapped = true;
+    }
     this.elementProperties.set(name, options);
     if (!options.noAccessor) {
       const key = DEV_MODE
@@ -748,12 +774,10 @@ export abstract class ReactiveElement
       );
     }
     return {
-      get(this: ReactiveElement) {
-        return get?.call(this);
-      },
+      get,
       set(this: ReactiveElement, value: unknown) {
         const oldValue = get?.call(this);
-        set!.call(this, value);
+        set?.call(this, value);
         this.requestUpdate(name, oldValue, options);
       },
       configurable: true,
@@ -979,6 +1003,12 @@ export abstract class ReactiveElement
    * @internal
    */
   _$changedProperties!: PropertyValues;
+
+  /**
+   * Records property default values when the
+   * `useDefault` option is used.
+   */
+  private __defaultValues?: Map<PropertyKey, unknown>;
 
   /**
    * Properties that should be reflected when updated.
@@ -1209,11 +1239,11 @@ export abstract class ReactiveElement
             : defaultConverter;
       // mark state reflecting
       this.__reflectingProperty = propName;
-      this[propName as keyof this] = converter.fromAttribute!(
-        value,
-        options.type
+      this[propName as keyof this] =
+        converter.fromAttribute!(value, options.type) ??
+        this.__defaultValues?.get(propName) ??
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      ) as any;
+        (null as any);
       // mark state not reflecting
       this.__reflectingProperty = null;
     }
@@ -1246,12 +1276,22 @@ export abstract class ReactiveElement
           `The requestUpdate() method was called with an Event as the property name. This is probably a mistake caused by binding this.requestUpdate as an event listener. Instead bind a function that will call it with no arguments: () => this.requestUpdate()`
         );
       }
-      options ??= (
-        this.constructor as typeof ReactiveElement
-      ).getPropertyOptions(name);
-      const hasChanged = options.hasChanged ?? notEqual;
+      const ctor = this.constructor as typeof ReactiveElement;
       const newValue = this[name as keyof this];
-      if (hasChanged(newValue, oldValue)) {
+      options ??= ctor.getPropertyOptions(name);
+      const changed =
+        (options.hasChanged ?? notEqual)(newValue, oldValue) ||
+        // When there is no change, check a corner case that can occur when
+        // 1. there's a initial value which was not reflected
+        // 2. the property is subsequently set to this value.
+        // For example, `prop: {useDefault: true, reflect: true}`
+        // and el.prop = 'foo'. This should be considered a change if the
+        // attribute is not set because we will now reflect the property to the attribute.
+        (options.useDefault &&
+          options.reflect &&
+          newValue === this.__defaultValues?.get(name) &&
+          !this.hasAttribute(ctor.__attributeNameForProperty(name, options)!));
+      if (changed) {
         this._$changeProperty(name, oldValue, options);
       } else {
         // Abort the request if the property should not be considered changed.
@@ -1269,8 +1309,22 @@ export abstract class ReactiveElement
   _$changeProperty(
     name: PropertyKey,
     oldValue: unknown,
-    options: PropertyDeclaration
+    options: PropertyDeclaration,
+    initializeValue?: unknown
   ) {
+    // Record default value when useDefault is used. This allows us to
+    // restore this value when the attribute is removed.
+    if (options.useDefault && !(this.__defaultValues ??= new Map()).has(name)) {
+      this.__defaultValues.set(
+        name,
+        initializeValue ?? oldValue ?? this[name as keyof this]
+      );
+      // if this is not wrapping an accessor, it must be an initial setting
+      // and in this case we do not want to record the change or reflect.
+      if (options.wrapped !== true || initializeValue !== undefined) {
+        return;
+      }
+    }
     // TODO (justinfagnani): Create a benchmark of Map.has() + Map.set(
     // vs just Map.set()
     if (!this._$changedProperties.has(name)) {
@@ -1402,25 +1456,26 @@ export abstract class ReactiveElement
         this.__instanceProperties = undefined;
       }
       // Trigger initial value reflection and populate the initial
-      // changedProperties map, but only for the case of experimental
-      // decorators on accessors, which will not have already populated the
-      // changedProperties map. We can't know if these accessors had
-      // initializers, so we just set them anyway - a difference from
-      // experimental decorators on fields and standard decorators on
-      // auto-accessors.
-      // For context why experimentalDecorators with auto accessors are handled
-      // specifically also see:
+      // `changedProperties` map, but only for the case of properties created
+      // via `createProperty` on accessors, which will not have already
+      // populated the `changedProperties` map since they are not set.
+      // We can't know if these accessors had initializers, so we just set
+      // them anyway - a difference from experimental decorators on fields and
+      // standard decorators on auto-accessors.
+      // For context see:
       // https://github.com/lit/lit/pull/4183#issuecomment-1711959635
       const elementProperties = (this.constructor as typeof ReactiveElement)
         .elementProperties;
       if (elementProperties.size > 0) {
         for (const [p, options] of elementProperties) {
+          const {wrapped} = options;
+          const value = this[p as keyof this];
           if (
-            options.wrapped === true &&
+            wrapped === true &&
             !this._$changedProperties.has(p) &&
-            this[p as keyof this] !== undefined
+            value !== undefined
           ) {
-            this._$changeProperty(p, this[p as keyof this], options);
+            this._$changeProperty(p, undefined, options, value);
           }
         }
       }

packages/reactive-element/src/test/reactive-element_test.ts
@@ -159,6 +159,61 @@ suite('ReactiveElement', () => {
     assert.equal(el.updatedCount, 3);
   });
 
+  test('changedProperties', async () => {
+    class E extends ReactiveElement {
+      static override get properties() {
+        return {
+          prop: {},
+          acc: {},
+          gs: {},
+        };
+      }
+
+      prop!: string;
+
+      accessor acc = 'acc';
+
+      #gs = 'gs';
+      get gs() {
+        return this.#gs;
+      }
+
+      set gs(v: string) {
+        this.#gs = v;
+      }
+
+      changes = new Map<PropertyKey, any>();
+
+      constructor() {
+        super();
+        this.prop = 'prop';
+        this.changes = new Map<PropertyKey, any>();
+      }
+
+      override updated(changed: PropertyValues) {
+        this.changes = new Map(changed);
+      }
+    }
+    customElements.define(generateElementName(), E);
+    const el = new E();
+    container.appendChild(el);
+    await el.updateComplete;
+    assert.deepEqual(Array.from(el.changes), [
+      ['prop', undefined],
+      ['acc', undefined],
+      ['gs', undefined],
+    ]);
+    el.prop = '1';
+    el.acc = '2';
+    el.gs = '3';
+    await el.updateComplete;
+    assert.deepEqual(Array.from(el.changes), [
+      ['prop', 'prop'],
+      ['acc', 'acc'],
+      ['gs', 'gs'],
+    ]);
+  });
+
   test('property options', async () => {
     const hasChanged = (value: any, old: any) =>
       old === undefined || value > old;
@@ -257,6 +312,165 @@ suite('ReactiveElement', () => {
     await el.updateComplete;
     assert.equal(el.all, 15);
     assert.equal(el.updateCount, 6);
+  });
+
+  test('property options useDefault', async () => {
+    class E extends ReactiveElement {
+      static override get properties() {
+        return {
+          prop: {useDefault: true},
+          acc: {useDefault: true},
+          gs: {useDefault: true},
+        };
+      }
+
+      prop!: string;
+
+      accessor acc = 'acc';
+
+      #gs = 'gs';
+      get gs() {
+        return this.#gs;
+      }
+
+      set gs(v: string) {
+        this.#gs = v;
+      }
+
+      constructor() {
+        super();
+        this.prop = 'prop';
+      }
+
+      changes = new Map<PropertyKey, any>();
+
+      override updated(changed: PropertyValues) {
+        this.changes = new Map(changed);
+      }
+    }
+    customElements.define(generateElementName(), E);
+    const el = new E();
+    container.appendChild(el);
+    assert.equal(el.prop, 'prop');
+    assert.equal(el.acc, 'acc');
+    assert.equal(el.gs, 'gs');
+    await el.updateComplete;
+    assert.equal(el.prop, 'prop');
+    assert.equal(el.acc, 'acc');
+    assert.equal(el.gs, 'gs');
+    assert.deepEqual(Array.from(el.changes), []);
+    el.prop = '1';
+    el.acc = '2';
+    el.gs = '3';
+    await el.updateComplete;
+    assert.equal(el.prop, '1');
+    assert.equal(el.acc, '2');
+    assert.equal(el.gs, '3');
+    assert.deepEqual(Array.from(el.changes), [
+      ['prop', 'prop'],
+      ['acc', 'acc'],
+      ['gs', 'gs'],
+    ]);
+    const el2 = new E();
+    container.appendChild(el2);
+    el2.prop = '1';
+    el2.acc = '2';
+    el2.gs = '3';
+    await el2.updateComplete;
+    assert.equal(el2.prop, '1');
+    assert.equal(el2.acc, '2');
+    assert.equal(el2.gs, '3');
+    assert.deepEqual(Array.from(el2.changes), [
+      ['prop', 'prop'],
+      ['acc', 'acc'],
+      ['gs', 'gs'],
+    ]);
+    const late = generateElementName();
+    const el3 = document.createElement(late) as any;
+    container.append(el3);
+    el3.prop = '1';
+    el3.acc = '2';
+    el3.gs = '3';
+    customElements.define(late, class extends E {});
+    await el3.updateComplete;
+    assert.equal(el3.prop, '1');
+    assert.equal(el3.acc, '2');
+    assert.equal(el3.gs, '3');
+    assert.deepEqual(Array.from(el3.changes), [
+      ['prop', 'prop'],
+      ['acc', 'acc'],
+      ['gs', 'gs'],
+    ]);
+  });
+
+  test('property options useDefault does not reflect', async () => {
+    class E extends ReactiveElement {
+      static override get properties() {
+        return {
+          prop: {reflect: true, useDefault: true},
+          acc: {reflect: true, useDefault: true},
+          gs: {reflect: true, useDefault: true},
+        };
+      }
+
+      prop!: string;
+
+      accessor acc = 'acc';
+
+      #gs = 'gs';
+      get gs() {
+        return this.#gs;
+      }
+
+      set gs(v: string) {
+        this.#gs = v;
+      }
+
+      constructor() {
+        super();
+        this.prop = 'prop';
+      }
+    }
+    customElements.define(generateElementName(), E);
+    const el = new E();
+    container.appendChild(el);
+    await el.updateComplete;
+    assert.isFalse(el.hasAttributes());
+    el.prop = 'prop';
+    el.acc = 'acc';
+    el.gs = 'gs';
+    await el.updateComplete;
+    assert.equal(el.getAttribute('prop'), 'prop');
+    assert.equal(el.getAttribute('acc'), 'acc');
+    assert.equal(el.getAttribute('gs'), 'gs');
+    el.prop = '1';
+    el.acc = '2';
+    el.gs = '3';
+    await el.updateComplete;
+    assert.equal(el.getAttribute('prop'), '1');
+    assert.equal(el.getAttribute('acc'), '2');
+    assert.equal(el.getAttribute('gs'), '3');
+
+    const el2 = new E();
+    container.appendChild(el2);
+    el2.prop = '1';
+    el2.acc = '2';
+    el2.gs = '3';
+    await el2.updateComplete;
+    assert.equal(el2.getAttribute('prop'), '1');
+    assert.equal(el2.getAttribute('acc'), '2');
+    assert.equal(el2.getAttribute('gs'), '3');
+    const late = generateElementName();
+    const el3 = document.createElement(late) as any;
+    container.append(el3);
+    el3.prop = '1';
+    el3.acc = '2';
+    el3.gs = '3';
+    customElements.define(late, class extends E {});
+    await el3.updateComplete;
+    assert.equal(el3.getAttribute('prop'), '1');
+    assert.equal(el3.getAttribute('acc'), '2');
+    assert.equal(el3.getAttribute('gs'), '3');
   });
 
   test('PropertyDeclaration field `hasChanged` can be passed concrete types', () => {
@@ -525,6 +739,274 @@ suite('ReactiveElement', () => {
     assert.equal(el.defaultReflectStr, null);
     assert.deepEqual(el.defaultReflectObj, null);
     assert.deepEqual(el.defaultReflectArr, null);
+  });
+
+  test('property/attribute values when attributes removed and useDefault is set', async () => {
+    class E extends ReactiveElement {
+      static override get properties() {
+        return {
+          bool: {type: Boolean, useDefault: true},
+          num: {type: Number, useDefault: true},
+          str: {type: String, useDefault: true},
+          obj: {type: Object, useDefault: true},
+          arr: {type: Array, useDefault: true},
+          reflectBool: {type: Boolean, reflect: true, useDefault: true},
+          reflectNum: {type: Number, reflect: true, useDefault: true},
+          reflectStr: {type: String, reflect: true, useDefault: true},
+          reflectObj: {type: Object, reflect: true, useDefault: true},
+          reflectArr: {type: Array, reflect: true, useDefault: true},
+          accBool: {type: Boolean, useDefault: true},
+          accNum: {type: Number, useDefault: true},
+          accStr: {type: String, useDefault: true},
+          accObj: {type: Object, useDefault: true},
+          accArr: {type: Array, useDefault: true},
+          accReflectBool: {type: Boolean, reflect: true, useDefault: true},
+          accReflectNum: {type: Number, reflect: true, useDefault: true},
+          accReflectStr: {type: String, reflect: true, useDefault: true},
+          accReflectObj: {type: Object, reflect: true, useDefault: true},
+          accReflectArr: {type: Array, reflect: true, useDefault: true},
+          gsBool: {type: Boolean, useDefault: true},
+          gsNum: {type: Number, useDefault: true},
+          gsStr: {type: String, useDefault: true},
+          gsObj: {type: Object, useDefault: true},
+          gsArr: {type: Array, useDefault: true},
+          gsReflectBool: {type: Boolean, reflect: true, useDefault: true},
+          gsReflectNum: {type: Number, reflect: true, useDefault: true},
+          gsReflectStr: {type: String, reflect: true, useDefault: true},
+          gsReflectObj: {type: Object, reflect: true, useDefault: true},
+          gsReflectArr: {type: Array, reflect: true, useDefault: true},
+        };
+      }
+
+      bool = false;
+      num = 0;
+      str = 'str';
+      obj: Record<string, unknown> = {obj: true};
+      arr = [0];
+      reflectBool = false;
+      reflectNum = 0;
+      reflectStr = 'str';
+      reflectObj: Record<string, unknown> = {obj: true};
+      reflectArr = [0];
+      accessor accBool = false;
+      accessor accNum = 0;
+      accessor accStr = 'str';
+      accessor accObj: Record<string, unknown> = {obj: true};
+      accessor accArr = [0];
+      accessor accReflectBool = false;
+      accessor accReflectNum = 0;
+      accessor accReflectStr = 'str';
+      accessor accReflectObj: Record<string, unknown> = {obj: true};
+      accessor accReflectArr = [0];
+
+      #gsBool = false;
+      get gsBool() {
+        return this.#gsBool;
+      }
+      set gsBool(value: boolean) {
+        this.#gsBool = value;
+      }
+      #gsNum = 0;
+      get gsNum() {
+        return this.#gsNum;
+      }
+      set gsNum(value: number) {
+        this.#gsNum = value;
+      }
+      #gsStr = 'str';
+      get gsStr() {
+        return this.#gsStr;
+      }
+      set gsStr(value: string) {
+        this.#gsStr = value;
+      }
+      #gsObj = {obj: true} as Record<string, unknown>;
+      get gsObj() {
+        return this.#gsObj;
+      }
+      set gsObj(value: Record<string, unknown>) {
+        this.#gsObj = value;
+      }
+      #gsArr = [0] as unknown[];
+      get gsArr() {
+        return this.#gsArr;
+      }
+      set gsArr(value: unknown[]) {
+        this.#gsArr = value;
+      }
+      #gsReflectBool = false;
+      get gsReflectBool() {
+        return this.#gsReflectBool;
+      }
+      set gsReflectBool(value: boolean) {
+        this.#gsReflectBool = value;
+      }
+      #gsReflectNum = 0;
+      get gsReflectNum() {
+        return this.#gsReflectNum;
+      }
+      set gsReflectNum(value: number) {
+        this.#gsReflectNum = value;
+      }
+      #gsReflectStr = 'str';
+      get gsReflectStr() {
+        return this.#gsReflectStr;
+      }
+      set gsReflectStr(value: string) {
+        this.#gsReflectStr = value;
+      }
+      #gsReflectObj = {obj: true} as Record<string, unknown>;
+      get gsReflectObj() {
+        return this.#gsReflectObj;
+      }
+      set gsReflectObj(value: Record<string, unknown>) {
+        this.#gsReflectObj = value;
+      }
+      #gsReflectArr = [0] as unknown[];
+      get gsReflectArr() {
+        return this.#gsReflectArr;
+      }
+      set gsReflectArr(value: unknown[]) {
+        this.#gsReflectArr = value;
+      }
+    }
+    const name = generateElementName();
+    customElements.define(name, E);
+    container.innerHTML = `<${name} bool num="1" str="str1" obj='{"obj1": true}'
+      arr='[1]' reflectBool reflectNum="1" reflectStr="str1"
+      reflectObj ='{"obj1": true}' reflectArr="[1]"
+      accbool accnum="1" accstr="str1" accobj='{"obj1": true}'
+      accarr='[1]' accreflectbool accreflectnum="1" accreflectstr="str1"
+      accreflectobj ='{"obj1": true}' accreflectarr="[1]"
+      gsbool gsnum="1" gsstr="str1" gsobj='{"obj1": true}'
+      gsarr='[1]' gsreflectbool gsreflectnum="1" gsreflectstr="str1"
+      gsreflectobj ='{"obj1": true}' gsreflectarr="[1]">
+      </${name}>`;
+    const el = container.firstChild as E;
+    await el.updateComplete;
+    assert.equal(el.bool, true);
+    assert.equal(el.num, 1);
+    assert.equal(el.str, 'str1');
+    assert.deepEqual(el.obj, {obj1: true});
+    assert.deepEqual(el.arr, [1]);
+    assert.equal(el.reflectBool, true);
+    assert.equal(el.reflectNum, 1);
+    assert.equal(el.reflectStr, 'str1');
+    assert.deepEqual(el.reflectObj, {obj1: true});
+    assert.deepEqual(el.reflectArr, [1]);
+    assert.equal(el.accBool, true);
+    assert.equal(el.accNum, 1);
+    assert.equal(el.accStr, 'str1');
+    assert.deepEqual(el.accObj, {obj1: true});
+    assert.deepEqual(el.accArr, [1]);
+    assert.equal(el.accReflectBool, true);
+    assert.equal(el.accReflectNum, 1);
+    assert.equal(el.accReflectStr, 'str1');
+    assert.deepEqual(el.accReflectObj, {obj1: true});
+    assert.deepEqual(el.accReflectArr, [1]);
+    assert.equal(el.gsBool, true);
+    assert.equal(el.gsNum, 1);
+    assert.equal(el.gsStr, 'str1');
+    assert.deepEqual(el.gsObj, {obj1: true});
+    assert.deepEqual(el.gsArr, [1]);
+    assert.equal(el.gsReflectBool, true);
+    assert.equal(el.gsReflectNum, 1);
+    assert.equal(el.gsReflectStr, 'str1');
+    assert.deepEqual(el.gsReflectObj, {obj1: true});
+    assert.deepEqual(el.gsReflectArr, [1]);
+    //
+    el.removeAttribute('bool');
+    el.removeAttribute('num');
+    el.removeAttribute('str');
+    el.removeAttribute('obj');
+    el.removeAttribute('arr');
+    el.removeAttribute('reflectbool');
+    el.removeAttribute('reflectnum');
+    el.removeAttribute('reflectstr');
+    el.removeAttribute('reflectobj');
+    el.removeAttribute('reflectarr');
+    el.removeAttribute('accbool');
+    el.removeAttribute('accnum');
+    el.removeAttribute('accstr');
+    el.removeAttribute('accobj');
+    el.removeAttribute('accarr');
+    el.removeAttribute('accreflectbool');
+    el.removeAttribute('accreflectnum');
+    el.removeAttribute('accreflectstr');
+    el.removeAttribute('accreflectobj');
+    el.removeAttribute('accreflectarr');
+    el.removeAttribute('gsbool');
+    el.removeAttribute('gsnum');
+    el.removeAttribute('gsstr');
+    el.removeAttribute('gsobj');
+    el.removeAttribute('gsarr');
+    el.removeAttribute('gsreflectbool');
+    el.removeAttribute('gsreflectnum');
+    el.removeAttribute('gsreflectstr');
+    el.removeAttribute('gsreflectobj');
+    el.removeAttribute('gsreflectarr');
+    assert.equal(el.bool, false);
+    assert.equal(el.num, 0);
+    assert.equal(el.str, 'str');
+    assert.deepEqual(el.obj, {obj: true});
+    assert.deepEqual(el.arr, [0]);
+    assert.equal(el.reflectBool, false);
+    assert.equal(el.reflectNum, 0);
+    assert.equal(el.reflectStr, 'str');
+    assert.deepEqual(el.reflectObj, {obj: true});
+    assert.deepEqual(el.reflectArr, [0]);
+    assert.equal(el.accBool, false);
+    assert.equal(el.accNum, 0);
+    assert.equal(el.accStr, 'str');
+    assert.deepEqual(el.accObj, {obj: true});
+    assert.deepEqual(el.accArr, [0]);
+    assert.equal(el.accReflectBool, false);
+    assert.equal(el.accReflectNum, 0);
+    assert.equal(el.accReflectStr, 'str');
+    assert.deepEqual(el.accReflectObj, {obj: true});
+    assert.deepEqual(el.accReflectArr, [0]);
+    assert.equal(el.gsBool, false);
+    assert.equal(el.gsNum, 0);
+    assert.equal(el.gsStr, 'str');
+    assert.deepEqual(el.gsObj, {obj: true});
+    assert.deepEqual(el.gsArr, [0]);
+    assert.equal(el.gsReflectBool, false);
+    assert.equal(el.gsReflectNum, 0);
+    assert.equal(el.gsReflectStr, 'str');
+    assert.deepEqual(el.gsReflectObj, {obj: true});
+    assert.deepEqual(el.gsReflectArr, [0]);
+    await el.updateComplete;
+    assert.isFalse(el.hasAttributes());
+    assert.equal(el.bool, false);
+    assert.equal(el.num, 0);
+    assert.equal(el.str, 'str');
+    assert.deepEqual(el.obj, {obj: true});
+    assert.deepEqual(el.arr, [0]);
+    assert.equal(el.reflectBool, false);
+    assert.equal(el.reflectNum, 0);
+    assert.equal(el.reflectStr, 'str');
+    assert.deepEqual(el.reflectObj, {obj: true});
+    assert.deepEqual(el.reflectArr, [0]);
+    assert.equal(el.accBool, false);
+    assert.equal(el.accNum, 0);
+    assert.equal(el.accStr, 'str');
+    assert.deepEqual(el.accObj, {obj: true});
+    assert.deepEqual(el.accArr, [0]);
+    assert.equal(el.accReflectBool, false);
+    assert.equal(el.accReflectNum, 0);
+    assert.equal(el.accReflectStr, 'str');
+    assert.deepEqual(el.accReflectObj, {obj: true});
+    assert.deepEqual(el.accReflectArr, [0]);
+    assert.equal(el.gsBool, false);
+    assert.equal(el.gsNum, 0);
+    assert.equal(el.gsStr, 'str');
+    assert.deepEqual(el.gsObj, {obj: true});
+    assert.deepEqual(el.gsArr, [0]);
+    assert.equal(el.gsReflectBool, false);
+    assert.equal(el.gsReflectNum, 0);
+    assert.equal(el.gsReflectStr, 'str');
+    assert.deepEqual(el.gsReflectObj, {obj: true});
+    assert.deepEqual(el.gsReflectArr, [0]);
   });
 
   test("attributes removed when a reflecting property's value becomes null", async () => {

packages/reactive-element/src/decorators/property.ts
@@ -70,10 +70,7 @@ const legacyProperty = (
   name: PropertyKey
 ) => {
   const hasOwnProperty = proto.hasOwnProperty(name);
-  (proto.constructor as typeof ReactiveElement).createProperty(
-    name,
-    hasOwnProperty ? {...options, wrapped: true} : options
-  );
+  (proto.constructor as typeof ReactiveElement).createProperty(name, options);
   // For accessors (which have a descriptor on the prototype) we need to
   // return a descriptor, otherwise TypeScript overwrites the descriptor we
   // define in createProperty() with the original descriptor. We don't do this
@@ -127,6 +124,10 @@ export const standardProperty = <C extends Interface<ReactiveElement>, V>(
   if (properties === undefined) {
     globalThis.litPropertyMetadata.set(metadata, (properties = new Map()));
   }
+  if (kind === 'setter') {
+    options = Object.create(options);
+    options.wrapped = true;
+  }
   properties.set(context.name, options);
 
   if (kind === 'accessor') {
@@ -147,7 +148,7 @@ export const standardProperty = <C extends Interface<ReactiveElement>, V>(
       },
       init(this: ReactiveElement, v: V): V {
         if (v !== undefined) {
-          this._$changeProperty(name, undefined, options);
+          this._$changeProperty(name, undefined, options, v);
         }
         return v;
       },

commit 94ccb279c25c7dc78fe015347faf503c417307fd

fromAttribute can set the property to either null or undefined.
Fixes a regression from https://github.com/lit/lit/pull/4934 which collapsed all converted nullish values to null.

packages/reactive-element/src/reactive-element.ts
@@ -1247,11 +1247,12 @@ export abstract class ReactiveElement
             : defaultConverter;
       // mark state reflecting
       this.__reflectingProperty = propName;
+      const convertedValue = converter.fromAttribute!(value, options.type);
       this[propName as keyof this] =
-        converter.fromAttribute!(value, options.type) ??
+        convertedValue ??
         this.__defaultValues?.get(propName) ??
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (null as any);
+        (convertedValue as any);
       // mark state not reflecting
       this.__reflectingProperty = null;
     }

packages/reactive-element/src/test/reactive-element_test.ts
@@ -1107,6 +1107,40 @@ suite('ReactiveElement', () => {
     assert.deepEqual(el.gsReflectArr, [0]);
   });
 
+  test('fromAttribute can set prop to null or undefined', () => {
+    class A extends ReactiveElement {
+      static override get properties() {
+        return {
+          foo: {
+            converter: {
+              fromAttribute: (value: string | null) => {
+                if (value === 'undef') {
+                  return undefined;
+                }
+                if (value === 'nll') {
+                  return null;
+                }
+                return value;
+              },
+            },
+          },
+        };
+      }
+
+      foo?: string;
+    }
+    customElements.define(generateElementName(), A);
+    const el = new A();
+    container.appendChild(el);
+    assert.equal(el.foo, undefined);
+    el.setAttribute('foo', 'bar');
+    assert.equal(el.foo, 'bar');
+    el.setAttribute('foo', 'undef');
+    assert.equal(el.foo, undefined);
+    el.setAttribute('foo', 'nll');
+    assert.equal(el.foo, null);
+  });
+
   test("attributes removed when a reflecting property's value becomes null", async () => {
     class E extends ReactiveElement {
       static override get properties() {