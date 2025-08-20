05e260bfc80f1a51ff6c3bb1a7cc5a03ab3476b3
Justin Fagnani <justin@fagnani.com>
2025-08-19 21:14:15 -0700

    [labs/tsserver-plugin] Add getDefinitionAtPosition()

--- packages/labs/tsserver-plugin/src/lib/lit-language-service.ts
+++ packages/labs/tsserver-plugin/src/lib/lit-language-service.ts
@@ -1,8 +1,13 @@
 import {Analyzer} from '@lit-labs/analyzer';
+import {
+  isLitHtmlTaggedTemplateExpression,
+  parseLitTemplate,
+} from '@lit-labs/analyzer/lib/lit/template.js';
 import * as path from 'node:path';
 import type ts from 'typescript';
 import {Diagnostic, LanguageService} from 'typescript';
 import {noBindingLikeAttributeNames} from './rules/no-binding-like-attribute-names.js';
+import {type Element, traverse} from '@parse5/tools';
 
 const rules = [noBindingLikeAttributeNames];
 
@@ -67,6 +72,118 @@ export const makeLitLanguageService = (
       }
       // TODO(justinfagnani): Add in analyzer diagnostics
       return [...(prevDiagnostics ?? []), ...diagnostics];
+    }
+
+    override getDefinitionAtPosition(
+      fileName: string,
+      position: number
+    ): readonly ts.DefinitionInfo[] | undefined {
+      const program = this.getProgram()!;
+      const checker = program.getTypeChecker();
+      const sourceFile = program.getSourceFile(fileName)!;
+
+      const tsNode = this.#findNodeAtPosition(sourceFile!, position);
+      if (tsNode !== undefined && typescript.isTemplateLiteral(tsNode)) {
+        if (
+          isLitHtmlTaggedTemplateExpression(tsNode.parent, typescript, checker)
+        ) {
+          const litTemplate = parseLitTemplate(
+            tsNode.parent,
+            typescript,
+            checker
+          );
+
+          // Get the Lit template node at this position
+          const templatePosition = tsNode.getFullStart();
+          let foundDefinition: ts.DefinitionInfo | undefined;
+
+          traverse(litTemplate, {
+            element: (element: Element) => {
+              const {startTag} = element.sourceCodeLocation!;
+              if (
+                startTag !== undefined &&
+                startTag.startOffset + templatePosition < position &&
+                startTag.endOffset + templatePosition > position
+              ) {
+                const definition = this.#getElementDefinition(element.tagName);
+                if (definition !== undefined) {
+                  const tsDefinition = definition.node;
+
+                  // Get a ts.DefinitionInfo from this tsDefinition
+                  const sourceFile = tsDefinition.getSourceFile();
+                  const start = tsDefinition.getStart();
+                  const length = tsDefinition.getEnd() - start;
+
+                  foundDefinition = {
+                    fileName: sourceFile.fileName,
+                    textSpan: {
+                      start,
+                      length,
+                    },
+                    kind: typescript.ScriptElementKind.classElement,
+                    name: definition.name,
+                    containerKind: typescript.ScriptElementKind.moduleElement,
+                    containerName: '',
+                  };
+                }
+              }
+            },
+          });
+
+          if (foundDefinition) {
+            return [foundDefinition];
+          }
+        }
+      }
+
+      return super.getDefinitionAtPosition(fileName, position);
+    }
+
+    override getDefinitionAndBoundSpan(
+      fileName: string,
+      position: number
+    ): ts.DefinitionInfoAndBoundSpan | undefined {
+      console.log('getDefinitionAndBoundSpan', fileName, position);
+      return super.getDefinitionAndBoundSpan(fileName, position);
+    }
+
+    /**
+     * Find the TypeScript AST node at the given position using depth-first traversal.
+     */
+    #findNodeAtPosition(
+      sourceFile: ts.SourceFile,
+      position: number
+    ): ts.Node | undefined {
+      function find(node: ts.Node): ts.Node | undefined {
+        // Check if position is within this node's range
+        if (position >= node.getFullStart() && position < node.getEnd()) {
+          // Try to find a more specific child node first
+          let foundChild: ts.Node | undefined;
+          node.forEachChild((child) => {
+            if (!foundChild) {
+              foundChild = find(child);
+            }
+          });
+          // Return the most specific node found, or this node if no children match
+          return foundChild || node;
+        }
+        return undefined;
+      }
+
+      return find(sourceFile);
+    }
+
+    #getElementDefinition(tagname: string) {
+      const pkg = this.#analyzer.getPackage();
+      for (const module of pkg.modules) {
+        const customElementExports = module.getCustomElementExports();
+        for (const ce of customElementExports) {
+          if (ce.tagname === tagname) {
+            return ce;
+          }
+        }
+      }
+      return undefined;
     }
   }
 
