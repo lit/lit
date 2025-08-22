069bafeb06b8b4b5d96c11d16bfdae35cf99ceb4
Justin Fagnani <justinfagnani@google.com>
2025-08-22 12:29:22 -0700

    [labs/tsserver-plugin] Add getDefinitionAtPosition() (#5039)

---
+++ packages/labs/tsserver-plugin/src/test/lib/lit-language-service_test.ts
@@ -0,0 +1,67 @@
+import assert from 'node:assert';
+import * as path from 'node:path';
+import {describe as suite, test} from 'node:test';
+import {createTestProjectService} from '../project-service.js';
+import {getLitTemplateExpressions} from '@lit-labs/analyzer/lib/lit/template.js';
+import ts from 'typescript';
+
+suite('lit-language-service', () => {
+  test('test test', async () => {
+    const {projectService, loaded} = createTestProjectService();
+
+    const pathName = path.resolve(
+      'test-files/basic-templates/src/custom-element.ts'
+    );
+    const result = projectService.openClientFile(pathName);
+    assert.ok(result.configFileName);
+
+    // The plugin is loaded async, so we need to wait for it to be loaded
+    await loaded;
+
+    const info = projectService.getScriptInfo(pathName);
+    const project = info!.containingProjects[0];
+    const languageService = project.getLanguageService();
+    const program = languageService.getProgram()!;
+    const testSourceFile = program.getSourceFile(pathName);
+    assert.ok(testSourceFile);
+
+    const templates = getLitTemplateExpressions(
+      testSourceFile,
+      ts,
+      program.getTypeChecker()
+    );
+
+    assert.equal(templates.length, 2);
+    const standaloneTemplate = templates[1];
+    const xFooPosition =
+      standaloneTemplate.getFullStart() +
+      standaloneTemplate.getFullText().indexOf('x-foo') +
+      1;
+
+    const definitions = languageService.getDefinitionAtPosition(
+      pathName,
+      xFooPosition
+    );
+    const firstDefinition = definitions![0];
+
+    // get the source text for the definition
+    const definitionSourceFile = program.getSourceFile(
+      firstDefinition.fileName
+    );
+    const definitionSourceText = definitionSourceFile!
+      .getFullText()
+      .slice(
+        firstDefinition.textSpan.start,
+        firstDefinition.textSpan.start + firstDefinition.textSpan.length
+      );
+    assert.equal(
+      definitionSourceText,
+      `@customElement('x-foo')
+export class XFoo extends LitElement {
+  render() {
+    return html\`<slot></slot>\`;
+  }
+}`
+    );
+  });
+});
